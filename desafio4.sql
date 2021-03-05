dbbdeb309d5a8922ed801f1290ac0946ad6ea28a
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2021-03-05 17:59:26 -0300

    ajuste req 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,7 +1,7 @@
 CREATE VIEW top_3_artistas AS
 SELECT A.name AS 'artista',
 COUNT(F.artist_id) AS 'seguidores'
-FROM SpotifyClone.artist AS a
+FROM SpotifyClone.artist AS A
 INNER JOIN SpotifyClone.artist_followers AS F
 ON A.id = F.artist_id
 GROUP BY 1
