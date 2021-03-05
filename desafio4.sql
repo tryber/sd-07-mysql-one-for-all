259c9bd52331c115b0ae65074d6e802dab3243db
Vanessa Oliveira <vanessanaara@gmail.com>
2021-03-05 19:15:39 -0300

    desafios 2 a 4

---
+++ desafio4.sql
@@ -0,0 +1,6 @@
+CREATE VIEW top_3_artistas AS
+SELECT a.artista_nome AS artista, COUNT(a.artista_id) AS seguidores
+FROM SpotifyClone.artistas_seguidos as ars
+INNER JOIN SpotifyClone.artista AS a ON ars.artista_id = a.artista_id
+GROUP BY ars.artista_id
+ORDER BY 2 DESC LIMIT 3;
