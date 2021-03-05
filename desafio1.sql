f06e02f7b2896d5a37795d82912ef1baa8389539
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-03-05 18:13:23 -0300

    corrigindo tabelas desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -10,11 +10,6 @@ nome_plano VARCHAR(50) NOT NULL,
 valor_plano DECIMAL(5,2) NOT NULL
 );
 
-CREATE TABLE historico(
-historico_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-nome_reproducao VARCHAR(50) NOT NULL
-);
-
 CREATE TABLE usuario(
 usuario_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 nome_usuario VARCHAR(50) NOT NULL,
@@ -22,14 +17,6 @@ idade INT NOT NULL,
 plano_id INT NOT NULL,
 FOREIGN KEY (plano_id) REFERENCES plano(plano_id)
 ); 
-
-CREATE TABLE usuario_historico(
-usuario_id INT NOT NULL,
-historico_id INT NOT NULL,
-PRIMARY KEY(usuario_id, historico_id),
-FOREIGN KEY (usuario_id) REFERENCES usuario(usuario_id),
-FOREIGN KEY (historico_id) REFERENCES historico(historico_id)
-);
 
 CREATE TABLE artista(
 artista_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -59,48 +46,24 @@ album_id INT NOT NULL,
 FOREIGN KEY (album_id) REFERENCES album(album_id)
 ); 
 
+CREATE TABLE historico(
+historico_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+cancao_id INT NOT NULL,
+usuario_id INT NOT NULL,
+FOREIGN KEY(cancao_id) REFERENCES cancao(cancao_id),
+FOREIGN KEY(usuario_id) REFERENCES usuario(usuario_id)
+);
+
 INSERT INTO plano (nome_plano, valor_plano) VALUES
 ('gratuito', 0),
 ('familiar', 7.99),
 ('universitário', 5.99);
-
-INSERT INTO historico (nome_reproducao) VALUES
-('Soul For Us'),
-('Magic Circus'),
-('Diamond Power'),
-('Thang Of Thunder'),
-('Home Forever'),
-('Words Of Her Life'),
-('Reflections Of Magic'),
-("Honey, Let's Be Silly"),
-('Troubles Of My Inner Fire'),
-('Thang Of Thunder'),
-('Magic Circus'),
-('Dance With Her Own'),
-('Without My Streets'),
-('Celebration Of More');
 
 INSERT INTO usuario (nome_usuario, idade, plano_id) VALUES
 ('Thati', 23, 1),
 ('Cintia', '35', 2),
 ('Bill', '20', 3),
 ('Roger', '45', 1);
-
-INSERT INTO usuario_historico (usuario_id,historico_id) VALUES
-(1,1),
-(1,2),
-(1,3),
-(1,4),
-(2,5),
-(2,6),
-(2,7),
-(2,8),
-(3,9),
-(3,10),
-(3,11),
-(4,12),
-(4,13),
-(4,14);
 
 INSERT INTO artista (nome_artista, sobrenome_artista) VALUES
 ('Walter', 'Phoenix'),
@@ -144,3 +107,19 @@ INSERT INTO cancao (nome_cancao, album_id) VALUES
 ('Thang Of Thunder', 5),
 ('Words Of Her Life', 5),
 ('Without My Streets', 5);
+
+INSERT INTO historico (cancao_id, usuario_id) VALUES
+(1,1),
+(6,1),
+(7,1),
+(16,1),
+(13,2),
+(17,2),
+(2,2),
+(15,2),
+(4,3),
+(16,3),
+(6,3),
+(3,4),
+(18,4),
+(11,4);
