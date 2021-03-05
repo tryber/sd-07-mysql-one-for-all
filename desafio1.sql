36fff0c3c598ecca8792b77dc82e57d684117d28
Vanessa Oliveira <vanessanaara@gmail.com>
2021-03-05 17:08:49 -0300

    desafio 1 - agora vai com certeza

--- desafio1.sql
+++ desafio1.sql
@@ -2,28 +2,53 @@ DROP DATABASE IF EXISTS SpotifyClone;
 CREATE DATABASE SpotifyClone;
 USE SpotifyClone;
 
-CREATE TABLE album (
-  album_id INT AUTO_INCREMENT,
-  album_nome VARCHAR(200) NOT NULL,
-  artista_id INT NOT NULL,
-  PRIMARY KEY (album_id, artista_id),
-  FOREIGN KEY (artista_id)
-  REFERENCES artista(artista_id)
+CREATE TABLE artista (
+  artista_id INT PRIMARY KEY AUTO_INCREMENT,
+  artista_nome VARCHAR(100) NOT NULL
 ) ENGINE = InnoDB;
 
-INSERT INTO album (album_nome, artista_id)
-VALUES
-  ('Envious', 1),
-  ('Exuberant', 1),
-  ('Hallowed Steam', 2),
-  ('Incandescent', 3),
-  ('Temporary Culture', 4)
-;
+CREATE TABLE plano (
+  plano_id INT PRIMARY KEY AUTO_INCREMENT,
+  tipo_plano VARCHAR(45) NOT NULL,
+  valor_plano FLOAT NOT NULL
+) ENGINE = InnoDB;
 
-CREATE TABLE artista (
-  artista_id INT AUTO_INCREMENT,
-  artista_nome VARCHAR(100) NOT NULL,
-  PRIMARY KEY (artista_id)
+CREATE TABLE usuario (
+  usuario_id INT PRIMARY KEY AUTO_INCREMENT,
+  usuario_nome VARCHAR(100) NOT NULL,
+  idade INT,
+  plano_id INT NOT NULL,
+  FOREIGN KEY (plano_id) REFERENCES plano(plano_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE album (
+  album_id INT PRIMARY KEY AUTO_INCREMENT,
+  album_nome VARCHAR(200) NOT NULL,
+  artista_id INT NOT NULL,
+  FOREIGN KEY (artista_id) REFERENCES artista(artista_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE cancoes (
+  cancoes_id INT PRIMARY KEY AUTO_INCREMENT,
+  cancoes_nome VARCHAR(200) NOT NULL,
+  album_id INT NOT NULL,
+  FOREIGN KEY (album_id) REFERENCES album(album_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE historico_reproducoes (
+  usuario_id INT NOT NULL,
+  cancoes_id INT NOT NULL,
+  PRIMARY KEY (usuario_id, cancoes_id),
+  FOREIGN KEY (usuario_id) REFERENCES usuario(usuario_id),
+  FOREIGN KEY (cancoes_id) REFERENCES cancoes(cancoes_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE artistas_seguidos (
+  usuario_id INT NOT NULL,
+  artista_id INT NOT NULL,
+  PRIMARY KEY (usuario_id, artista_id),
+  FOREIGN KEY (usuario_id) REFERENCES usuario(usuario_id),
+  FOREIGN KEY (artista_id) REFERENCES artista(artista_id)
 ) ENGINE = InnoDB;
 
 INSERT INTO artista (artista_nome)
@@ -34,36 +59,29 @@ VALUES
   ('Freedie Shannon')
 ;
 
-CREATE TABLE artistas_seguidos (
-  artista_id INT NOT NULL,
-  usuario_id INT NOT NULL,
-  PRIMARY KEY (artista_id, usuario_id),
-  FOREIGN KEY (usuario_id)
-  REFERENCES usuario(usuario_id),
-  FOREIGN KEY (artista_id)
-  REFERENCES artista(artista_id)
-) ENGINE = InnoDB;
-
-INSERT INTO artistas_seguidos
+INSERT INTO plano (tipo_plano, valor_plano)
 VALUES
-  (1, 1),
-  (1, 4),
-  (1, 5),
-  (2, 1),
-  (2, 4),
-  (3, 1),
-  (3, 4),
-  (4, 5)
+  ('gratuito', 0.00),
+  ('universitário', 5.99),
+  ('familiar', 7.99)
 ;
 
-CREATE TABLE cancoes (
-  cancoes_id INT AUTO_INCREMENT,
-  cancoes_nome VARCHAR(200) NOT NULL,
-  album_id INT NOT NULL,
-  PRIMARY KEY (cancoes_id),
-  FOREIGN KEY (album_id)
-  REFERENCES album(album_id)
-) ENGINE = InnoDB;
+INSERT INTO usuario (usuario_nome, idade, plano_id)
+VALUES
+  ('Thati', 23, 1),
+  ('Cintia', 35, 3),
+  ('Bill', 20, 2),
+  ('Roger', 45, 1)
+;
+
+INSERT INTO album (album_nome, artista_id)
+VALUES
+  ('Envious', 1),
+  ('Exuberant', 1),
+  ('Hallowed Steam', 2),
+  ('Incandescent', 3),
+  ('Temporary Culture', 4)
+;
 
 INSERT INTO cancoes (cancoes_nome, album_id)
 VALUES
@@ -87,16 +105,6 @@ VALUES
   ('Without My Streets', 5)
 ;
 
-CREATE TABLE historico_reproducoes (
-  usuario_id INT NOT NULL,
-  cancoes_id INT NOT NULL,
-  PRIMARY KEY (usuario_id, cancoes_id),
-  FOREIGN KEY (usuario_id)
-  REFERENCES usuario(usuario_id),
-  FOREIGN KEY (cancoes_id)
-  REFERENCES cancoes(cancoes_id)
-) ENGINE = InnoDB;
-
 INSERT INTO historico_reproducoes
 VALUES
   (1, 1),
@@ -115,34 +123,14 @@ VALUES
   (4, 18)
 ;
 
-CREATE TABLE IF NOT EXISTS plano (
-  plano_id INT AUTO_INCREMENT,
-  tipo_plano VARCHAR(45) NOT NULL,
-  valor_plano FLOAT NOT NULL,
-  PRIMARY KEY (plano_id)
-) ENGINE = InnoDB;
-
-INSERT INTO plano (tipo_plano, valor_plano)
+INSERT INTO artistas_seguidos
 VALUES
-  ('gratuito', 0.00),
-  ('universitário', 5.99),
-  ('familiar', 7.99)
-;
-
-CREATE TABLE usuario (
-  usuario_id INT AUTO_INCREMENT,
-  usuario_nome VARCHAR(100) NOT NULL,
-  idade INT,
-  plano_id INT NOT NULL,
-  PRIMARY KEY (usuario_id),
-  FOREIGN KEY (plano_id)
-  REFERENCES plano(plano_id)
-) ENGINE = InnoDB;
-
-INSERT INTO usuario (usuario_nome, idade, plano_id)
-VALUES
-  ('Thati', 23, 1),
-  ('Cintia', 35, 3),
-  ('Bill', 20, 2),
-  ('Roger', 45, 1)
+  (1, 1),
+  (1, 3),
+  (1, 4),
+  (2, 1),
+  (2, 3),
+  (3, 1),
+  (3, 3),
+  (4, 4)
 ;
