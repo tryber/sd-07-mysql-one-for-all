2e8c96e0a58ef6398145f09fee367f8d599f2689
Luise Rios <luisemiranda@gmail.com>
2021-03-05 19:25:56 -0300

    resolvendo desafios

--- desafio8.sql
+++ desafio8.sql
@@ -2,12 +2,12 @@ USE SpotifyClone;
 DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
-    AFTER DELETE ON users
-    FOR EACH ROW
+AFTER DELETE ON users
+FOR EACH ROW
 BEGIN
-	DELETE FROM followers
-    WHERE user_id = OLD.user_id;
-    DELETE FROM playbacks
-    WHERE user_id = OLD.user_id;
+DELETE FROM followers
+WHERE user_id = OLD.user_id;
+DELETE FROM playbacks
+WHERE user_id = OLD.user_id;
 END $$
 DELIMITER ;
