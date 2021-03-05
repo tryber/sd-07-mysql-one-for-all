0e3c35fc157d4653f5d2ff67da983be46b51bcee
Melissa Gomes <mel.ge171201@gmail.com>
2021-03-05 19:05:42 -0300

    desafio 2 d novo

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,6 @@
 DROP VIEW estatisticas_musicais;
 CREATE VIEW estatisticas_musicais AS 
 SELECT 
-COUNT(c.cancao_name) AS `cancoes`,
+(SELECT COUNT(cancao_name) FROM SpotifyClone.Cancao) AS `cancoes`,
 (SELECT COUNT(artista_name) FROM SpotifyClone.Artista) AS `artistas`,
-(SELECT COUNT(album_name) FROM SpotifyClone.Albuns) AS `albuns`
-FROM SpotifyClone.Cancao AS `c`;
+(SELECT COUNT(album_name) FROM SpotifyClone.Albuns) AS `albuns`;
