b5d56bd2b4a5f80c3dd8b387baec5c8a140a8b57
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-03-05 19:16:22 -0300

    Desafio 8: trigger exclusao usuario

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+USE SpotifyClone;
+
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON usuario
+FOR EACH ROW
+BEGIN
+DELETE FROM historico
+WHERE usuario_id = OLD.usuario_id;
+DELETE FROM usuario_artista
+WHERE usuario_id = OLD.usuario_id;
+END $$
+DELIMITER ;
