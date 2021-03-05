191b41e5fca3060970c069c8cc5804905b8dd6b6
marcelo ivan <sagacello@hotmail.com>
2021-03-05 16:49:40 -0300

    primeiro_desafio tentativa 3

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,9 @@
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-CREATE SCHEMA IF NOT EXISTS `SpotifyClone` ;
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE SpotifyClone;
+
+USE SpotifyClone;
 
 DROP TABLE IF EXISTS `SpotifyClone`.`planos` ;
 
@@ -132,22 +133,22 @@ SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 ​
-INSERT INTO planos (plano, valor) VALUES
+INSERT INTO SpotifyClone.planos (plano, valor) VALUES
 ('gratuito', 0), ('familiar', 7.99), ('universitário', 5.99);
 ​
-INSERT INTO usuarios (usuario, idade, plano_id)
+INSERT INTO SpotifyClone.usuarios (usuario, idade, plano_id)
 VALUES ('Thati', 23, 1), ('Cintia', 35, 2), ('Bill', 20, 3), ('Roger', 45, 1);
 ​
-INSERT INTO artistas (artista)
+INSERT INTO SpotifyClone.artistas (artista)
 VALUES ('Walter Phoenix'), ('Peter Strong'), ('Lance Day'), ('Freedie Shannon');
 ​
-INSERT INTO seguidores (usuario_id, artista_id)
+INSERT INTO SpotifyClone.seguidores (usuario_id, artista_id)
 VALUES (1, 1), (1, 4), (1, 3), (2,1), (2, 3), (3, 2), (3, 1), (4, 4);
 ​
-INSERT INTO albuns (album, artista_id)
+INSERT INTO SpotifyClone.albuns (album, artista_id)
 VALUES ('Envious', 1), ('Exuberant', 1), ('Hallowed Steam', 2), ('Incandescent', 3), ('Temporary Culture', 4);
 ​
-INSERT INTO musicas (musica, album_id, artista_id)
+INSERT INTO SpotifyClone.musicas (musica, album_id, artista_id)
 VALUES ('Soul For Us', 1, 1), ('Reflections Of Magic', 1, 1), ('Dance With Her Own', 1, 1),
 ('Troubles Of My Inner Fire', 2, 1), ('Time Fireworks', 2, 1),
 ('Magic Circus', 3, 2), ('Honey, So Do I', 3, 2), ('Sweetie, Let''s Go Wild', 3, 2), ('She Knows', 3, 2),
@@ -155,7 +156,7 @@ VALUES ('Soul For Us', 1, 1), ('Reflections Of Magic', 1, 1), ('Dance With Her O
 ('Home Forever', 4, 3), ('Diamond Power', 4, 3), ('Honey, Let''s Be Silly', 4, 3),
 ('Thang Of Thunder', 5, 4), ('Words Of Her Life', 5, 4), ('Without My Streets', 5, 4);
 ​
-INSERT INTO historico_reproducoes (usuario_id, musica_id)
+INSERT INTO SpotifyClone.historico_reproducoes (usuario_id, musica_id)
 VALUES (1, 1), (1, 6), (1, 14), (1, 16),
 (2, 13), (2, 17), (2, 2), (2, 15),
 (3, 4), (3, 16),  (3, 6),
