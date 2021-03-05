d86dbd8f060a9fbd6aead45e34308504d6d2f0ca
Brune Miguel <bstfreire@gmail.com>
2021-03-05 20:17:16 -0300

    Correção requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,9 @@
   CREATE VIEW faturamento_atual AS
   SELECT 
-  MIN(p.value_plan),2) AS faturamento_minimo,
-  MAX(p.value_plan),2) AS faturamento_maximo,
+  MIN(p.value_plan) AS faturamento_minimo,
+  MAX(p.value_plan) AS faturamento_maximo,
   ROUND(SUM(p.value_plan) / COUNT(u.name), 2) AS faturamento_medio,
-  SUM(p.value_plan),2) AS faturamento_total
+  SUM(p.value_plan) AS faturamento_total
   FROM SpotifyClone.plan AS p
   INNER JOIN SpotifyClone.users AS u 
   ON u.plan_id = p.plan_id;
