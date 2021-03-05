929d11ee33a596116154ba0e059a09a1bbdeb1b6
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-03-05 18:21:58 -0300

    Refatorando Desafio 5 com novas tabelas

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,8 @@
 CREATE VIEW top_2_hits_do_momento AS (
 SELECT c.nome_cancao AS `cancao`,
-COUNT(u.historico_id) AS `reproducoes`
+COUNT(h.historico_id) AS `reproducoes`
 FROM cancao AS `c`
-INNER JOIN historico AS `h` ON c.nome_cancao = h.nome_reproducao
-INNER JOIN usuario_historico AS `u` ON u.historico_id = h.historico_id
+INNER JOIN historico AS `h` ON c.cancao_id = h.cancao_id
 GROUP BY c.nome_cancao
 ORDER BY `reproducoes` DESC, `cancao`
 LIMIT 2
