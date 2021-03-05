516a0eb814e7fffdd07eb1ab88b087c26f8fd868
vitorAmorims <vtamorims@gmail.com>
2021-03-05 18:33:31 -0300

    inserindo nome do db

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT
-  (SELECT COUNT(musical_track.id) FROM musical_track) as 'cancoes',
-  (SELECT COUNT(singers.id) FROM singers) as "artistas",
-  (SELECT COUNT(albuns.id) FROM albuns) as "albuns";
+  (SELECT COUNT(musical_track.id) FROM SpotifyClone.musical_track) as 'cancoes',
+  (SELECT COUNT(singers.id) FROM SpotifyClone.singers) as "artistas",
+  (SELECT COUNT(albuns.id) FROM SpotifyClone.albuns) as "albuns";
