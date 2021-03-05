3e4ec317b9a1204f62090146a19ec0cc5ecf5125
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2021-03-05 19:35:49 -0300

    req 5 ajuste

--- desafio5.sql
+++ desafio5.sql
@@ -4,5 +4,5 @@ COUNT(H.song_id) AS 'reproducoes'
 FROM SpotifyClone.songs AS S
 INNER JOIN SpotifyClone.music_play_history AS H
 ON S.id = H.song_id
-GROUP BY 1
-ORDER BY 2 DESC LIMIT 2;
+GROUP BY H.song_id
+ORDER BY 2 DESC, 1 LIMIT 2;
