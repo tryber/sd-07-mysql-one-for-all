dc8f60a78d53fa92ccf3540e23eb1b71784fc3f2
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-03-05 18:58:10 -0300

    Requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,3 +1,6 @@
+CREATE VIEW cancoes_premium AS
+SELECT m.nome_musica AS nome,
+COUNT(h.musica_id) AS reproducoes
 FROM SpotifyClone.historico_usuario AS h
 INNER JOIN SpotifyClone.musicas AS m
 ON m.id = h.musica_id
