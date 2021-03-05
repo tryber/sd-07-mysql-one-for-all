9b53dadfeee34bb8753be8945c49b282831d08b0
Brune Miguel <bstfreire@gmail.com>
2021-03-05 18:08:56 -0300

    Requisito 3

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+ CREATE VIEW historico_reproducao_usuarios AS
+ SELECT u.name AS usuario, s.name
+ FROM SpotifyClone.users AS u
+ INNER JOIN SpotifyClone.user_history_songs  as h
+ ON u.user_id = h.user_id
+ INNER JOIN SpotifyClone.songs as s
+ ON h.song_id = s.song_id
+ ORDER BY u.name, s.name;
