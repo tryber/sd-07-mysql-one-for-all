25843d9948d9cd4c64847d609eb084b90e9fd0d6
Brune Miguel <bstfreire@gmail.com>
2021-03-05 17:49:06 -0300

    Corrigindo CC

--- desafio2.sql
+++ desafio2.sql
@@ -3,4 +3,3 @@ SELECT
   (SELECT COUNT(song_id) FROM SpotifyClone.songs) AS cancoes,
   (SELECT COUNT(name_singer) FROM SpotifyClone.singers) AS artistas,
   (SELECT COUNT(album_id) FROM SpotifyClone.album) AS albuns;
- 
