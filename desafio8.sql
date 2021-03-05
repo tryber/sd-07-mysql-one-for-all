b1e04f5c715f4b4c6ecace9c729909ce44215521
vanderson-henrique <vhbenedito@gmail.com>
2021-03-05 15:21:51 -0300

    Desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+USE SpotifyClone;
+
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON usuarios
+FOR EACH ROW
+BEGIN
+DELETE FROM historico_reproducoes WHERE usuario_id = OLD.usuario_id;
+DELETE FROM seguindo_artistas WHERE usuario_id = OLD.usuario_id;
+END $$
+
+DELIMITER ;
