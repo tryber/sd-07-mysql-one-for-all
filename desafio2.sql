9b6f13b5b39a3ada8df49713e42c99b586cd5cbf
vanderson-henrique <vhbenedito@gmail.com>
2021-03-05 15:18:46 -0300

    Desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+CREATE VIEW estatisticas_musicais AS
+SELECT COUNT(DISTINCT(c.cancao_id)) AS 'cancoes',
+COUNT(DISTINCT(a.artista_id)) AS 'artistas',
+COUNT(DISTINCT(al.album_id)) AS 'albuns'
+FROM SpotifyClone.cancoes AS c
+INNER JOIN SpotifyClone.albuns AS al
+ON c.album_id = al.album_id
+INNER JOIN SpotifyClone.artistas AS a
+ON al.artista_id = a.artista_id;
