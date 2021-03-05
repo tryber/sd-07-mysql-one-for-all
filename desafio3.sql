9eee7c6368465562524c83ad37011fd609bd6269
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-03-05 18:18:59 -0300

    Refatorando Desafio 3 com novas tabelas

--- desafio3.sql
+++ desafio3.sql
@@ -1,10 +1,10 @@
 CREATE VIEW historico_reproducao_usuarios AS(
 SELECT u.nome_usuario AS `usuario`,
-h.nome_reproducao AS `nome`
+c.nome_cancao AS `nome`
 FROM usuario AS `u`
-INNER JOIN usuario_historico AS `uh` 
-ON u.usuario_id = uh.usuario_id
-INNER JOIN historico AS `h`
-ON uh.historico_id = h.historico_id
+INNER JOIN historico AS `h` 
+ON u.usuario_id = h.usuario_id
+INNER JOIN cancao AS `c`
+ON h.cancao_id = c.cancao_id
 ORDER BY `usuario`, `nome`
 );
