1434d7587487cf125a4152b685c666729d09114e
Anderson Santana <anderslash@gmail.com>
2021-03-05 19:15:40 -0300

    Testes

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,3 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
 DROP DATABASE IF EXISTS `SpotifyClone`;
 CREATE DATABASE IF NOT EXISTS `SpotifyClone`;
 USE `SpotifyClone`;
@@ -145,7 +141,3 @@ INSERT INTO `SpotifyClone`.`usuarios` (`usuario_id`, `usuario`, `idade`, `id_pla
 INSERT INTO `SpotifyClone`.`usuarios` (`usuario_id`, `usuario`, `idade`, `id_plano`) VALUES (DEFAULT, 'Cintia', 35, 3);
 INSERT INTO `SpotifyClone`.`usuarios` (`usuario_id`, `usuario`, `idade`, `id_plano`) VALUES (DEFAULT, 'Bill', 20, 2);
 INSERT INTO `SpotifyClone`.`usuarios` (`usuario_id`, `usuario`, `idade`, `id_plano`) VALUES (DEFAULT, 'Roger', 45, 1);
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
