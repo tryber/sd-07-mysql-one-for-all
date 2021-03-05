ca483c9dd6a3697bbf46a485211f8c252dcd8305
alvarovascon <alvarovascon2@gmail.com>
2021-03-05 17:55:57 -0300

    DESAFIO 3- ordenação

--- desafio3.sql
+++ desafio3.sql
@@ -2,4 +2,4 @@ CREATE VIEW historico_reproducao_usuarios AS
 SELECT u.usuario, m.musica AS nome FROM SpotifyClone.usuarios AS u
 INNER JOIN SpotifyClone.historicos AS h ON h.usuario_id = u.usuario_id
 INNER JOIN SpotifyClone.musicas AS m ON h.musica_id = m.musica_id
-ORDER BY u.usuario;
+ORDER BY u.usuario, m.musica;
