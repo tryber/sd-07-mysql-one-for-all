c12e2ab872c7f33692c9df25463525b7a352c6b6
nevesfreire <nando.neves@gmail.com>
2021-03-05 19:34:42 -0300

    teste

--- tests/challenges.spec.js
+++ tests/challenges.spec.js
@@ -15,12 +15,6 @@ describe('Queries de seleção', () => {
     }
     catch(error) {
       console.log('Erro ao restaurar o dump!');
-<<<<<<< HEAD
-=======
-
-      console.log(error, '** Error Info **')
-
->>>>>>> 8779ae8121efd7f25644737e05edfc44b1ae01af
     }
 
     importer.disconnect();
