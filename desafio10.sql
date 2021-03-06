601d780852adac0bf3622dac690b109ef6660686
Francisco <franciscoliratorres@gmail.com>
2021-03-06 14:27:51 -0300

    add desafio 11

---
+++ desafio10.sql
@@ -0,0 +1,19 @@
+SELECT usuario_id, COUNT(cancao_id) FROM SpotifyClone.historico
+GROUP BY usuario_id;
+
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(id_do_usuario INT)
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE musicas_historico INT;
+    SELECT usuario_id, COUNT(cancao_id) FROM SpotifyClone.historico
+	GROUP BY usuario_id
+    HAVING historico.usuario_id = id_do_usuario INTO musicas_historico;
+    RETURN musicas_historico;
+END $$
+
+DELIMITER ;
+
+SELECT quantidade_musicas_no_historico(3);
