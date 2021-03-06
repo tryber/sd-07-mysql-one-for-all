d6a2123de13a66aaeada5c9ba05d5a6ea438a3a2
hvenses <hvenses@gmail.com>
2021-03-06 15:13:29 -0300

    Commitando req4

---
+++ desafio4.sql
@@ -0,0 +1,6 @@
+CREATE VIEW top_3_artistas AS
+SELECT a.artista AS artista, COUNT(sa.artista_id) AS seguidores
+FROM SpotifyClone.artistas AS a
+INNER JOIN SpotifyClone.seguindo_artistas AS sa ON a.artista_id = sa.artista_id
+GROUP BY a.artista
+ORDER BY seguidores DESC, artista LIMIT 3;
