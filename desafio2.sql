1144e9914dfa046bbeebd4a675734e8e5ce5c8dc
Lfocosi <luccafocosi@gmail.com>
2021-03-06 15:29:16 -0300

    desafio 2 part 3

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
-use SpotifyClone;
-create view estatisticas_musicais As
-  SELECT (select count(albun_id) from SpotifyClone.musicas) as 'cancoes',
-  (select count(*) from SpotifyClone.cantores) as 'artistas',
-  (select count(*) from SpotifyClone.albuns) as 'albuns'
+CREATE view estatisticas_musicais AS
+  SELECT count(albun_id) AS 'cancoes',
+  (SELECT count(cantor_id) FROM SpotifyClone.cantores) AS 'artistas',
+  (SELECT count(albun_id) FROM SpotifyClone.albuns) AS 'albuns'
+  FROM SpotifyClone.musicas;
