67c997af3820e14ce58371a508678badb625ce03
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-03-06 09:51:45 -0300

    Adicionando o SpotifyClone no requisito 9 e 10

--- desafio10.sql
+++ desafio10.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 CREATE PROCEDURE quantidade_musicas_no_historico(IN id INT)
 BEGIN
 SELECT COUNT(*) AS quantidade_musicas_no_historico 
-from historico 
+from SpotifyClone.historico 
 WHERE usuario_id = id;
 END $$
 DELIMITER ;
--- desafio9.sql
+++ desafio9.sql
@@ -2,8 +2,8 @@ DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(IN nome VARCHAR(50))
 BEGIN
 SELECT a.artista, al.album 
-FROM artistas AS a
-INNER JOIN albuns AS al
+FROM SpotifyClone.artistas AS a
+INNER JOIN SpotifyClone.albuns AS al
 ON al.artista_id = a.artista_id
 HAVING artista = nome
 ORDER BY album;
