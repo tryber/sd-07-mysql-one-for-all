0447486a679c89f83b2fd3544c115322f917d840
dennerpl <denner.souzapl@gmail.com>
2021-03-06 20:27:54 -0300

    requisito 8 funcionando, porem nao passa no teste

---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.usuarios
+FOR EACH ROW
+BEGIN
+	DELETE FROM SpotifyClone.seguidores WHERE usuario_id = OLD.usuario_id;
+	DELETE FROM SpotifyClone.historico WHERE usuario_id = OLD.usuario_id;
+END $$
+
+DELIMITER ;
