aa32e59f68bd244a3b20cabbcb8a4af64778f40f
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-03-06 09:09:13 -0300

    Requisito 9

---
+++ desafio9.sql
@@ -0,0 +1,12 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(IN nome VARCHAR(50))
+BEGIN
+SELECT a.artista, al.album 
+FROM artistas AS a
+INNER JOIN albuns AS al
+ON al.artista_id = a.artista_id
+HAVING artista = nome
+ORDER BY album;
+END $$
+DELIMITER ;
