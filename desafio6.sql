7f4cead7bf004c8902ace8c9efcb2afceee29098
vitorAmorims <vtamorims@gmail.com>
2021-03-06 05:57:45 -0300

    convertendo o tipo de dados com comando CAST

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,7 @@
 CREATE VIEW faturamento_atual AS 
 SELECT
-(SELECT MIN(SpotifyClone.plans.price) AS "faturamento_minimo" FROM SpotifyClone.plans join SpotifyClone.users on SpotifyClone.users.plan_id = plans.id) AS "faturamento_minimo",
-(SELECT ROUND(MAX(SpotifyClone.plans.price), 2) AS "faturamento_maximo" FROM SpotifyClone.plans join SpotifyClone.users on SpotifyClone.users.plan_id = plans.id) AS "faturamento_maximo",
-(SELECT ROUND(AVG(SpotifyClone.plans.price), 2) AS "faturamento_medio" FROM SpotifyClone.plans join SpotifyClone.users on SpotifyClone.users.plan_id = plans.id) AS "faturamento_medio",
-(SELECT ROUND(SUM(SpotifyClone.plans.price), 2) AS "faturamento_total" FROM SpotifyClone.plans join SpotifyClone.users on SpotifyClone.users.plan_id = plans.id) AS "faturamento_total";
+(SELECT MIN(CAST(SpotifyClone.plans.price AS CHAR)) FROM SpotifyClone.plans join SpotifyClone.users on SpotifyClone.users.plan_id = plans.id
+) AS "faturamento_minimo",
+(SELECT ROUND(MAX(CAST(SpotifyClone.plans.price AS CHAR)), 2) FROM SpotifyClone.plans join SpotifyClone.users on SpotifyClone.users.plan_id = plans.id) AS "faturamento_maximo",
+(SELECT ROUND(AVG(CAST(SpotifyClone.plans.price AS CHAR)), 2) FROM SpotifyClone.plans join SpotifyClone.users on SpotifyClone.users.plan_id = plans.id) AS "faturamento_medio",
+(SELECT ROUND(SUM(CAST(SpotifyClone.plans.price AS CHAR)), 2) FROM SpotifyClone.plans join SpotifyClone.users on SpotifyClone.users.plan_id = plans.id) AS "faturamento_total";
