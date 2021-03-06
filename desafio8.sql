3b95c6bddfaf27dea0b7c1ee5fd0ae6a91bba661
danielmlduarte <danielduarte.tryber@gmail.com>
2021-03-05 21:18:00 -0300

    Desafio 8 - Refatorado

--- desafio8.sql
+++ desafio8.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER trigger_usuario_delete
     BEFORE DELETE ON usuario
     FOR EACH ROW
 BEGIN
-    DELETE FROM seguindo_artista sa WHERE sa.usuario_id = OLD.usuario_id;
-    DELETE FROM historico_reproducao hr WHERE hr.usuario_id = OLD.usuario_id;    
+    DELETE FROM seguindo_artista WHERE seguindo_artista.usuario_id = OLD.usuario_id;
+    DELETE FROM historico_reproducao WHERE historico_reproducao.usuario_id = OLD.usuario_id;    
 END $$
 DELIMITER ;
