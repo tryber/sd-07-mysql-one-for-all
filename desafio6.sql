0c6015b4cf7209115a42c4ef142de1f9594e1f70
vitorAmorims <vtamorims@gmail.com>
2021-03-06 07:41:59 -0300

    inserindo formataçao nos campos do desafio6.sql

--- desafio6.sql
+++ desafio6.sql
@@ -3,5 +3,5 @@ SELECT
 (SELECT MIN(CAST(SpotifyClone.plans.price AS CHAR(5))) FROM SpotifyClone.plans join SpotifyClone.users on SpotifyClone.users.plan_id = plans.id
 ) AS "faturamento_minimo",
 (SELECT ROUND(MAX(CAST(SpotifyClone.plans.price AS CHAR(5))), 2) FROM SpotifyClone.plans join SpotifyClone.users on SpotifyClone.users.plan_id = plans.id) AS "faturamento_maximo",
-(SELECT ROUND(AVG(CAST(SpotifyClone.plans.price AS CHAR(5))), 2) FROM SpotifyClone.plans join SpotifyClone.users on SpotifyClone.users.plan_id = plans.id) AS "faturamento_medio",
+(SELECT CAST(CONVERT((AVG(SpotifyClone.plans.price)), DECIMAL(6,2)) AS CHAR) AS "faturamento_medio" FROM SpotifyClone.plans join SpotifyClone.users on SpotifyClone.users.plan_id = plans.id,
 (SELECT ROUND(SUM(CAST(SpotifyClone.plans.price AS CHAR(5))), 2) FROM SpotifyClone.plans join SpotifyClone.users on SpotifyClone.users.plan_id = plans.id) AS "faturamento_total";
