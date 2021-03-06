ca63542710511eceb41d429a901bca204fc83145
CarolSi <a.carolg1@gmail.com>
2021-03-05 22:23:26 -0300

    requisito 5

--- desafio3.sql
+++ desafio3.sql
@@ -1,9 +1,9 @@
 CREATE VIEW SpotifyClone.historico_reproducao_usuarios AS
-SELECT us.usuario AS `usuario`,
+SELECT usr.usuario AS `usuario`,
 can.cancao AS `nome`
-FROM SpotifyClone.usuarios AS us
+FROM SpotifyClone.usuarios AS usr
 INNER JOIN SpotifyClone.historico_de_reproducoes AS his
-ON us.usuario_id = his.usuario_id
+ON usr.usuario_id = his.usuario_id
 INNER JOIN SpotifyClone.cancoes AS can
 ON his.cancao_id = can.cancao_id
 ORDER BY usuario, nome;
