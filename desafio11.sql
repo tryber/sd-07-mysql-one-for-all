822fdb747c1d4b2af530bf62906bd2f886888ebb
renatomak <renato.mark.silva@gmail.com>
2021-03-06 15:42:33 -0300

    Desafio 11-corrigido

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 CREATE VIEW cancoes_premium AS
-SELECT A.cancao_title nome, COUNT(*) reporducoes
+SELECT A.cancao_title nome, COUNT(*) reproducoes
 FROM SpotifyClone.cancoes A, SpotifyClone.historico_de_reproducoes B, SpotifyClone.usuarios C
 WHERE A.cancao_id=B.cancao_id AND B.usuario_id=C.usuario_id AND C.plano_id IN (2, 3)
 GROUP BY B.cancao_id
