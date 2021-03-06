440e7dc0adbee4eeebdeee5a0a511a087e312b3e
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-03-06 10:27:15 -0300

    Finalizando

--- desafio10.sql
+++ desafio10.sql
@@ -1,10 +1,13 @@
 DELIMITER $$
-CREATE PROCEDURE quantidade_musicas_no_historico(IN id int)
+CREATE FUNCTION quantidade_musicas_no_historico(id int)
+RETURNS INT READS SQL DATA
 BEGIN
+DECLARE resultado INT;
 SELECT COUNT(*) AS quantidade_musicas_no_historico
 FROM SpotifyClone.historico 
 INNER JOIN SpotifyClone.usuarios AS u
 ON u.usuario_id = historico.usuario_id
-WHERE u.usuario_id = id;
+WHERE u.usuario_id = id INTO resultado;
+RETURN resultado;
 END $$
 DELIMITER ;
