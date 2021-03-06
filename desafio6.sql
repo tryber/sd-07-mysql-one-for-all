39957cc9f259f5300d1194a81cfb9c16c100ca2d
kramer <kramerscs@gmail.com>
2021-03-06 13:48:56 -0300

    feat: 1 to 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,7 @@
 CREATE VIEW faturamento_atual AS 
- SELECT MIN(price) AS faturamento_minimo, MAX(price) AS faturamento_maximo, ROUND(AVG(price), 2) AS faturamento_medio, SUM(price) AS faturamento_total FROM(
- SELECT Users.plano_id, Plano.price
- FROM SpotifyClone.Users AS Users
- INNER JOIN SpotifyClone.Plano AS Plano
- ON Users.plano_id = Plano.plano_id) AS C;
+  SELECT MIN(price) AS faturamento_minimo, MAX(price) AS faturamento_maximo, ROUND(AVG(price), 2) AS faturamento_medio, SUM(price) AS faturamento_total FROM(
+  SELECT Users.plano_id, Plano.price
+  FROM SpotifyClone.Users AS Users
+  INNER JOIN SpotifyClone.Plano AS Plano
+  ON Users.plano_id = Plano.plano_id) AS C;
+
