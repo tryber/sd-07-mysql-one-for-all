1741e378405025df068f5ba16d227bc6d7969add
Felipe Nascimento <flpnascto@gmail.com>
2021-03-06 18:02:21 -0300

    Desafio 08

--- desafio8.sql
+++ desafio8.sql
@@ -7,9 +7,9 @@ DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON users
 FOR EACH ROW
- BEGIN
-	DELETE FROM following WHERE OLD.user_id = user_id;
-    DELETE FROM history WHERE OLD.user_id = user_id;
+BEGIN
+  DELETE FROM following WHERE OLD.user_id = user_id;
+  DELETE FROM history WHERE OLD.user_id = user_id;
 END $$
 
 DELIMITER ;
