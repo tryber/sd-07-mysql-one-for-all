52f4e05bef95a1c1b8f8528c7db323ed7fcf604b
Fernando Antunes <fernando.antunes1@gmail.com>
2021-03-06 15:37:56 -0300

    Requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,8 @@
 DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historico(nome_usuario VARCHAR(100))
+CREATE FUNCTION quantidade_musicas_no_historico(cod_usuario INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE music_total INT;
-DECLARE cod_usuario INT;
-
-SELECT usuario_id FROM SpotifyClone.usuarios
-WHERE usuario = nome_usuario INTO cod_usuario;
-
 SELECT COUNT(*)
 FROM SpotifyClone.historico_reproducao
 WHERE usuario_id = cod_usuario INTO music_total;
