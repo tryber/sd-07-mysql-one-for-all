807c8b432fcea36aba88ccb8a2a5ce69c018c943
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-03-06 19:31:57 -0300

    tentando passar req. 1

--- desafio1.sql
+++ desafio1.sql
@@ -2,6 +2,19 @@ DROP DATABASE IF EXISTS SpotifyClone;
 
 CREATE DATABASE `SpotifyClone`;
 
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema SpotifyClone
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema SpotifyClone
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `SpotifyClone` ;
+
 USE `SpotifyClone`;
 -- -----------------------------------------------------
 -- Table `SpotifyClone`.`plano`
@@ -148,3 +161,7 @@ VALUES (1, 1),
   (3, 2),
   (3, 1),
   (4, 4);
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
