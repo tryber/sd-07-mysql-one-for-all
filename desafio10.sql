d696a0d17814d4dd0f2037ef69849e0061f90f0b
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-03-06 10:07:18 -0300

    Finalizando

--- desafio10.sql
+++ desafio10.sql
@@ -1,10 +1,10 @@
 DELIMITER $$
-CREATE PROCEDURE quantidade_musicas_no_historico(IN id VARCHAR(50))
+CREATE PROCEDURE quantidade_musicas_no_historico(IN id int)
 BEGIN
 SELECT COUNT(*) AS quantidade_musicas_no_historico
 FROM SpotifyClone.historico 
 INNER JOIN SpotifyClone.usuarios AS u
 ON u.usuario_id = historico.usuario_id
-WHERE usuario = id;
+WHERE u.usuario_id = id;
 END $$
 DELIMITER ;
--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,10 @@
 CREATE VIEW cancoes_premium AS
-SELECT m.musica AS nome, COUNT(h.musicas_id) AS reproducoes FROM SpotifyClone.musicas AS m
+SELECT m.musica AS nome, 
+COUNT(h.musicas_id) AS reproducoes 
+FROM SpotifyClone.musicas AS m
 INNER JOIN SpotifyClone.historico AS h
 ON h.musicas_id = m.musicas_id
-INNER JOIN usuarios AS u
+INNER JOIN SpotifyClone.usuarios AS u
 ON u.usuario_id = h.usuario_id
 WHERE u.plano_id IN (2, 3)
 GROUP BY nome
