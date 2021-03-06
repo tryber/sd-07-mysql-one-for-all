979c38b4ba325ee9f0217c27a082a5618204886b
Felipe Nascimento <flpnascto@gmail.com>
2021-03-06 16:38:57 -0300

    Desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -4,7 +4,7 @@
 
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(search_user VARCHAR(50))
+CREATE FUNCTION quantidade_musicas_no_historico(search_user_id INT)
 RETURNS INT READS SQL DATA
   BEGIN
     DECLARE history_sum INT;
@@ -12,7 +12,7 @@ RETURNS INT READS SQL DATA
     FROM history AS h
     JOIN users AS u
       ON h.user_id = u.user_id
-    WHERE u.name = CONCAT('%', search_user, '%')
+    WHERE u.user_id = search_user_id
     INTO history_sum;
     RETURN history_sum;
   END $$
