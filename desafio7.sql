320762dcb1226e6033a76e3c6f5e67f739d57a22
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-03-06 12:14:41 -0300

    Adicionando requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+CREATE VIEW perfil_artistas AS
+SELECT AR.nome_artista AS `artista`,
+AL.nome_album AS `album`,
+COUNT(S.id_artista) AS `seguidores`
+FROM SpotifyClone.artista AS AR
+INNER JOIN SpotifyClone.seguidores AS S ON AR.id_artista = S.id_artista
+INNER JOIN SpotifyClone.album AS AL ON AL.id_artista = AR.id_artista
+GROUP BY `album`, S.id_artista
+ORDER BY `seguidores` DESC, `artista`;
