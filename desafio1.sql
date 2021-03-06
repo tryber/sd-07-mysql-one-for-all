1e3a211807df79e2c6ca8bb2764889bff74436b0
jvitorof <j.vitoroflores@gmail.com>
2021-03-06 11:18:43 -0300

    ONE FOR ALL: um no forro, todos pelo forro :dancers::dancers:

--- desafio1.sql
+++ desafio1.sql
@@ -32,12 +32,15 @@ FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
 
 CREATE TABLE songs(
 song_id INT PRIMARY KEY  AUTO_INCREMENT,
-song VARCHAR(100)
+song VARCHAR(100),
+album_id INT,
+FOREIGN KEY (album_id) REFERENCES albuns(album_id)
 ) engine = InnoDB;
 
 CREATE TABLE folowers(
 user_id INT,
 artist_id INT,
+PRIMARY KEY (user_id, artist_id),
 FOREIGN KEY (user_id) REFERENCES users(user_id),
 FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
 ) engine = InnoDB;
@@ -45,6 +48,7 @@ FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
 CREATE TABLE histories(
 user_id INT,
 song_id INT,
+PRIMARY KEY (user_id, song_id),
 FOREIGN KEY (user_id) REFERENCES users(user_id),
 FOREIGN KEY (song_id) REFERENCES songs(song_id)
 ) engine = InnoDB;
@@ -77,26 +81,26 @@ VALUES
 ('Incandescent', 3), 
 ('Temporary Culture', 4);
 
-INSERT INTO songs (song)
+INSERT INTO songs (song, album_id)
 VALUES
-('Soul For Us'), 
-('Reflections Of Magic'), 
-('Dance With Her Own'), 
-('Troubles Of My Inner Fire'), 
-('Time Fireworks'), 
-('Magic Circus'), 
-('Honey, So Do I'), 
-("Sweetie, Let's Go Wild"), 
-('She Knows'), 
-('Fantasy For Me'), 
-('Celebration Of More'), 
-('Rock His Everything'), 
-('Home Foreve'), 
-('Diamond Power'), 
-("Honey, Let's Be Silly"), 
-('Thang Of Thunder'), 
-('Words Of Her Life'), 
-('Without My Streets');
+('Soul For Us', 1), 
+('Reflections Of Magic', 1), 
+('Dance With Her Own', 1), 
+('Troubles Of My Inner Fire', 2), 
+('Time Fireworks', 2), 
+('Magic Circus', 3), 
+('Honey, So Do I', 3), 
+("Sweetie, Let's Go Wild", 3), 
+('She Knows', 3), 
+('Fantasy For Me', 4), 
+('Celebration Of More', 4), 
+('Rock His Everything', 4), 
+('Home Foreve', 4), 
+('Diamond Power', 4), 
+("Honey, Let's Be Silly", 4), 
+('Thang Of Thunder', 5), 
+('Words Of Her Life', 5), 
+('Without My Streets', 5);
 
 INSERT INTO histories (user_id, song_id)
 VALUES
