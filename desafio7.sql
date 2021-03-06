afb0d5252ab17edbc0a70ef186e58cc12464a9a8
vitorAmorims <vtamorims@gmail.com>
2021-03-06 11:13:38 -0300

    desafio7.sql

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+SELECT
+  SpotifyClone.singers.name AS "artista",
+  SpotifyClone.albuns.name AS "album",
+  COUNT(SpotifyClone.following_artists.artist_id) AS "seguidores"
+FROM SpotifyClone.singers
+JOIN SpotifyClone.albuns
+ON albuns.artist_id = singers.id
+JOIN SpotifyClone.following_artists
+ON SpotifyClone.following_artists.artist_id = SpotifyClone.singers.id
+GROUP BY albuns.id
+ORDER BY seguidores DESC, artista, album;
