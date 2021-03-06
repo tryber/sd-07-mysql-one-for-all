cecd1f713e9c3dc9354ee84750ae0e28b2300c4d
PedroMarqDev <PedroMarqDev@gmail.com>
2021-03-06 12:38:48 -0300

    Format fix desafio6

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,8 @@
-CREATE VIEW faturamento_atual AS
 SELECT 
-ROUND(MIN(p.plan_value), 2) AS faturamento_minimo,
-ROUND(MAX(p.plan_value), 2) AS faturamento_maximo,
-ROUND(AVG(p.plan_value), 2) AS faturamento_medio,
-ROUND(SUM(p.plan_value), 2)  AS faturamento_total
+FORMAT(ROUND(MIN(p.plan_value), 2), 2) AS faturamento_minimo,
+FORMAT(ROUND(MAX(p.plan_value), 2), 2)  AS faturamento_maximo,
+FORMAT(ROUND(AVG(p.plan_value), 2), 2)  AS faturamento_medio,
+FORMAT(ROUND(SUM(p.plan_value), 2), 2)   AS faturamento_total
 FROM SpotifyClone.users AS u
 INNER JOIN SpotifyClone.plans AS p ON
 u.plan_id = p.plan_id;
