be099f65110e3a880c021fe60ff976edae4ebde8
Felipe Nascimento <flpnascto@gmail.com>
2021-03-06 17:24:59 -0300

    Desafio 08

---
+++ desafio8.sql
@@ -0,0 +1,15 @@
+-- USE SpotifyClone;
+
+-- DROP TRIGGER IF EXISTS trigger_usuario_delete;
+
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON users
+FOR EACH ROW
+ BEGIN
+	DELETE FROM following WHERE OLD.user_id = user_id;
+    DELETE FROM history WHERE OLD.user_id = user_id;
+END $$
+
+DELIMITER ;
