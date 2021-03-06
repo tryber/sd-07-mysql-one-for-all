8a64340eed746e8857345da1504218726abbf574
Flavio Oliveira <flavsoliver@gmail.com>
2021-03-06 20:59:47 -0300

    desafio 1 - inserção de ID default

--- desafio1.sql
+++ desafio1.sql
@@ -2,13 +2,6 @@ DROP database if exists SpotifyClone;
 
 CREATE database SpotifyClone;
 
-SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS,
-  UNIQUE_CHECKS = 0;
-SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS,
-  FOREIGN_KEY_CHECKS = 0;
-SET @OLD_SQL_MODE = @@SQL_MODE,
-  SQL_MODE = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
 CREATE SCHEMA IF NOT EXISTS SpotifyClone;
 
 USE SpotifyClone;
@@ -96,102 +89,98 @@ CREATE TABLE IF NOT EXISTS SpotifyClone.seguindo (
 
 LOCK TABLES plano WRITE;
 
-INSERT INTO plano (`plano_nome`, `valor_plano`)
-VALUES ('gratuito', 0),
-  ('familiar', 7.99),
-  ('universitário', 5.99);
+INSERT INTO plano (`plano_id`, `plano_nome`, `valor_plano`)
+VALUES (DEFAULT, 'gratuito', 0),
+  (DEFAULT, 'familiar', 7.99),
+  (DEFAULT, 'universitário', 5.99);
 
 UNLOCK TABLES;
 
 LOCK TABLES usuario WRITE;
 
-INSERT INTO usuario (`usuario_nome`, `idade`, `plano_id`)
-VALUES ('Thati', 23, 1),
-  ('Cintia', 35, 2),
-  ('Bill', 20, 3),
-  ('Roger', 45, 1);
+INSERT INTO usuario (`usuario_id`, `usuario_nome`, `idade`, `plano_id`)
+VALUES (DEFAULT, 'Thati', 23, 1),
+  (DEFAULT, 'Cintia', 35, 2),
+  (DEFAULT, 'Bill', 20, 3),
+  (DEFAULT, 'Roger', 45, 1);
 
 UNLOCK TABLES;
 
 LOCK TABLES artista WRITE;
 
-INSERT INTO artista (`artista_nome`)
-VALUES ('Walter Phoenix'),
-  ('Peter Strong'),
-  ('Lance Day'),
-  ('Freedie Shannon');
+INSERT INTO artista (`artista_id`, `artista_nome`)
+VALUES (DEFAULT, 'Walter Phoenix'),
+  (DEFAULT, 'Peter Strong'),
+  (DEFAULT, 'Lance Day'),
+  (DEFAULT, 'Freedie Shannon');
 
 UNLOCK TABLES;
 
 LOCK TABLES album WRITE;
 
-INSERT INTO album (`album_nome`, `artista_id`)
-VALUES ('Envious', 1),
-  ('Exuberant', 1),
-  ('Hallowed Steam', 2),
-  ('Incandescent', 3),
-  ('Temporary Culture', 4);
+INSERT INTO album (`album_id`, `album_nome`, `artista_id`)
+VALUES (DEFAULT, 'Envious', 1),
+  (DEFAULT, 'Exuberant', 1),
+  (DEFAULT, 'Hallowed Steam', 2),
+  (DEFAULT, 'Incandescent', 3),
+  (DEFAULT, 'Temporary Culture', 4);
 
 UNLOCK TABLES;
 
 LOCK TABLES musica WRITE;
 
-INSERT INTO musica (`musica_nome`, `artista_id`, `album_id`)
-VALUES ("Soul For Us", 1, 1),
-  ("Reflections Of Magic", 1, 1),
-  ("Dance With Her Own", 1, 1),
-  ("Troubles Of My Inner Fire", 1, 2),
-  ("Time Fireworks", 1, 2),
-  ("Magic Circus", 2, 3),
-  ("Honey, So Do I", 2, 3),
-  ("Sweetie, Let's Go Wild", 2, 3),
-  ("She Knows", 2, 3),
-  ("Fantasy For Me", 3, 4),
-  ("Celebration Of More", 3, 4),
-  ("Rock His Everything", 3, 4),
-  ("Home Forever", 3, 4),
-  ("Diamond Power", 3, 4),
-  ("Honey, Let's Be Silly", 3, 4),
-  ("Thang Of Thunder", 4, 5),
-  ("Words Of Her Life", 4, 5),
-  ("Without My Streets", 4, 5);
+INSERT INTO musica (`musica_id`, `musica_nome`, `artista_id`, `album_id`)
+VALUES (DEFAULT, "Soul For Us", 1, 1),
+  (DEFAULT, "Reflections Of Magic", 1, 1),
+  (DEFAULT, "Dance With Her Own", 1, 1),
+  (DEFAULT, "Troubles Of My Inner Fire", 1, 2),
+  (DEFAULT, "Time Fireworks", 1, 2),
+  (DEFAULT, "Magic Circus", 2, 3),
+  (DEFAULT, "Honey, So Do I", 2, 3),
+  (DEFAULT, "Sweetie, Let's Go Wild", 2, 3),
+  (DEFAULT, "She Knows", 2, 3),
+  (DEFAULT, "Fantasy For Me", 3, 4),
+  (DEFAULT, "Celebration Of More", 3, 4),
+  (DEFAULT, "Rock His Everything", 3, 4),
+  (DEFAULT, "Home Forever", 3, 4),
+  (DEFAULT, "Diamond Power", 3, 4),
+  (DEFAULT, "Honey, Let's Be Silly", 3, 4),
+  (DEFAULT, "Thang Of Thunder", 4, 5),
+  (DEFAULT, "Words Of Her Life", 4, 5),
+  (DEFAULT, "Without My Streets", 4, 5);
 
 UNLOCK TABLES;
 
 LOCK TABLES historico WRITE;
 
-INSERT INTO historico (`usuario_id`, `musica_id`)
-VALUES (1, 1),
-  (1, 6),
-  (1, 14),
-  (1, 16),
-  (2, 13),
-  (2, 17),
-  (2, 2),
-  (2, 15),
-  (3, 4),
-  (3, 16),
-  (3, 6),
-  (4, 3),
-  (4, 18),
-  (4, 11);
+INSERT INTO historico (`historico_id`, `usuario_id`, `musica_id`)
+VALUES (DEFAULT, 1, 1),
+  (DEFAULT, 1, 6),
+  (DEFAULT, 1, 14),
+  (DEFAULT, 1, 16),
+  (DEFAULT, 2, 13),
+  (DEFAULT, 2, 17),
+  (DEFAULT, 2, 2),
+  (DEFAULT, 2, 15),
+  (DEFAULT, 3, 4),
+  (DEFAULT, 3, 16),
+  (DEFAULT, 3, 6),
+  (DEFAULT, 4, 3),
+  (DEFAULT, 4, 18),
+  (DEFAULT, 4, 11);
 
 UNLOCK TABLES;
 
 LOCK TABLES seguindo WRITE;
 
-INSERT INTO seguindo (`usuario_id`, `artista_id`)
-VALUES (1, 1),
-  (1, 4),
-  (1, 3),
-  (2, 1),
-  (2, 3),
-  (3, 2),
-  (3, 1),
-  (4, 4);
+INSERT INTO seguindo (`seguindo_id`, `usuario_id`, `artista_id`)
+VALUES (DEFAULT, 1, 1),
+  (DEFAULT, 1, 4),
+  (DEFAULT, 1, 3),
+  (DEFAULT, 2, 1),
+  (DEFAULT, 2, 3),
+  (DEFAULT, 3, 2),
+  (DEFAULT, 3, 1),
+  (DEFAULT, 4, 4);
 
 UNLOCK TABLES;
-
-SET SQL_MODE = @OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
