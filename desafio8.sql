1012601ac69f254983d2b1e4866ca7a607456bab
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-03-06 16:32:25 -0300

    refatorando desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -10,8 +10,6 @@ FROM SpotifyClone.seguindo s
 JOIN SpotifyClone.historico_de_reproducoes hr
 ON s.usuario_id = hr.usuario_id
 WHERE OLD.usuario_id = s.usuario_id;
--- DELETE FROM SpotifyClone.seguindo s WHERE OLD.usuario_id = s.usuario_id;
--- DELETE FROM SpotifyClone.historico_de_reproducoes hr WHERE OLD.usuario_id = hr.usuario_id;
 END $$
 
 DELIMITER ;
