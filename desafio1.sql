e6ee709cb6690f0380f289b46df3dfa6de20f13e
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-03-06 18:25:42 -0300

    Correçao criaçao do banco

--- desafio1.sql
+++ desafio1.sql
@@ -1,17 +1,7 @@
 DROP DATABASE IF EXISTS SpotifyClone;
-SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS,
-  UNIQUE_CHECKS = 0;
-SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS,
-  FOREIGN_KEY_CHECKS = 0;
-SET @OLD_SQL_MODE = @@SQL_MODE,
-  SQL_MODE = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
--- -----------------------------------------------------
--- Schema SpotifyClone
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema SpotifyClone
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `SpotifyClone`;
+
+CREATE DATABASE `SpotifyClone`;
+
 USE `SpotifyClone`;
 -- -----------------------------------------------------
 -- Table `SpotifyClone`.`plano`
@@ -158,6 +148,3 @@ VALUES (1, 1),
   (3, 2),
   (3, 1),
   (4, 4);
-SET SQL_MODE = @OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
