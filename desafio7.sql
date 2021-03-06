75602fa7fbdf7f9a63002465e61a4b925d8972d6
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-03-06 12:20:42 -0300

    Correção requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -6,4 +6,4 @@ FROM SpotifyClone.artista AS AR
 INNER JOIN SpotifyClone.seguidores AS S ON AR.id_artista = S.id_artista
 INNER JOIN SpotifyClone.album AS AL ON AL.id_artista = AR.id_artista
 GROUP BY `album`, S.id_artista
-ORDER BY `seguidores` DESC, `artista`;
+ORDER BY `seguidores` DESC, `artista`, `album`;
