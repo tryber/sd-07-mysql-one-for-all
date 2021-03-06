c34a93ea498bb822cdfc30cee4bc96cff26fe690
hvenses <hvenses@gmail.com>
2021-03-06 12:09:19 -0300

    Alterando  a tabela historico como primeiro FROM

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
 CREATE VIEW historico_reproducao_usuarios AS 
 SELECT u.usuario as usuario, c.cancao as nome
-FROM SpotifyClone.usuarios as u
-INNER JOIN SpotifyClone.historico_de_reproducoes as h ON u.usuario_id = h.usuario_id
+FROM SpotifyClone.historico_de_reproducoes as h 
+INNER JOIN SpotifyClone.usuarios as u ON u.usuario_id = h.usuario_id
 INNER JOIN SpotifyClone.cancoes as c ON h.cancao_id = c.cancao_id
 ORDER BY usuario, nome;
