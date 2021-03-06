c2b79eb89c071a31617b440271decdba1b451138
PedroMarqDev <PedroMarqDev@gmail.com>
2021-03-06 10:45:57 -0300

    Desafio 2-4

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+SELECT 
+CONCAT(a.first_name, ' ', a.last_name) AS 'artista',
+COUNT(f.artist_id) AS  'seguidores'
+FROM follow_list AS f
+INNER JOIN artists AS a ON
+f.artist_id = a.artist_id
+GROUP BY `artista`
+ORDER BY `seguidores`DESC, `artista`
+LIMIT 3
