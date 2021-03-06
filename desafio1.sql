83b7295668d819115660cc7d351ed1353db6ca32
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-03-06 10:54:57 -0300

    Correção exercício 4

--- desafio1.sql
+++ desafio1.sql
@@ -60,21 +60,20 @@ VALUES
 INSERT INTO usuarios (nome_usuario, idade, id_plano)
 VALUES
   ('Thati', 23, 1),
-  ('Cintia', 35, 3),
-  ('Bill', 20, 2),
+  ('Cintia', 35, 2),
+  ('Bill', 20, 3),
   ('Roger', 45, 1);
-  
 
-INSERT INTO artista(id_artista, nome_artista) 
+INSERT INTO artista(id_artista, nome_artista)
 VALUES
   (1, 'Walter Phoenix'),
-  (2, 'Freedie Shannon'),
+  (2, 'Peter Strong'),
   (3, 'Lance Day'),
-  (4, 'Peter Strong');
+  (4, 'Freedie Shannon');
 
 INSERT INTO album(id_album, nome_album, id_artista) 
-VALUES 
-  (1, 'Envious', 1),
+VALUES
+  (1,'Envious', 1),
   (2, 'Exuberant', 1),
   (3, 'Hallowed Steam', 2),
   (4, 'Incandescent', 3),
@@ -82,49 +81,49 @@ VALUES
 
 INSERT INTO musicas(nome_musica, id_album)
 VALUES
-('Soul For Us', 1), 
-('Reflections Of Magic', 1),
-('Dance With Her Own', 1),
-('Troubles Of My Inner Fire', 2),
-('Time Fireworks', 2),
-('Magic Circus', 3),
-('Honey, So Do I', 3),
-('Sweetie, Let''s Go Wild', 3),
-('She Knows', 3),
-('Fantasy For Me', 4), 
-('Celebration Of More', 4), 
-('Rock His Everything', 4),
-('Home Forever', 4), 
-('Diamond Power', 4), 
-('Honey, Let''s Be Silly', 4),
-('Thang Of Thunder', 5), 
-('Words Of Her Life', 5), 
-('Without My Streets', 5);
+ ('Soul For Us', 1),
+ ('Reflections Of Magic', 1),
+ ('Dance With Her Own', 1),
+ ('Troubles Of My Inner Fire', 2),
+ ('Time Fireworks', 2),
+ ('Magic Circus', 3),
+ ('Honey, So Do I', 3),
+ ("Sweetie, Let's Go Wild", 3),
+ ('She Knows', 3),
+ ('Fantasy For Me', 4),
+ ('Celebration Of More', 4),
+ ('Rock His Everything', 4),
+ ('Home Forever', 4),
+ ('Diamond Power', 4),
+ ("Honey, Let's Be Silly", 4),
+ ('Thang Of Thunder', 5),
+ ('Words Of Her Life', 5),
+ ('Without My Streets', 5);
 
 INSERT INTO historico(usuario_id, id_musica)
 VALUES
-  (1, 1),
-  (1, 6),
-  (1, 14),
-  (1, 16),
-  (2, 13),
-  (2, 17),
-  (2, 2),
-  (2, 15),
-  (3, 4),
-  (3, 16),
-  (3, 6),
-  (4, 3),
-  (4, 18),
-  (4, 11);
-
+ (1, 1),
+ (1, 6),
+ (1, 14),
+ (1, 16),
+ (2, 13),
+ (2, 17),
+ (2, 2),
+ (2, 15),
+ (3, 4),
+ (3, 16),
+ (3, 6),
+ (4, 3),
+ (4, 18),
+ (4, 11);
+ 
 INSERT INTO seguidores(usuario_id, id_artista)
 VALUES
-  (1, 1), 
-  (1, 4), 
-  (1, 3),
-  (2, 1),
-  (2, 3),
-  (3, 2),
-  (3, 1),
-  (4, 4);
+ (1, 1),
+ (1, 4),
+ (1, 3),
+ (2, 1),
+ (2, 3),
+ (3, 2),
+ (3, 1),
+ (4, 4);
