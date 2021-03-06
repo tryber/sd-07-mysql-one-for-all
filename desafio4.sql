030ef810a98c60692dc1189c2b78a4533da32776
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2021-03-06 10:30:09 -0300

    requisito 4v1

---
+++ desafio4.sql
@@ -0,0 +1,10 @@
+CREATE VIEW top_3_artistas AS
+SELECT
+a.artista AS artista,
+COUNT(u.usuario) AS seguidores
+FROM SpotifyClone.seguidores AS s
+INNER JOIN SpotifyClone.artistas AS a ON s.artista_id = a.artista_id
+INNER JOIN SpotifyClone.usuarios AS u ON s.usuario_id = u.usuario_id
+GROUP BY artista
+ORDER BY seguidores DESC, artista
+LIMIT 3;
