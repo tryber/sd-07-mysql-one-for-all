22349d5d5a419884bd596ab206af638040fd6cc4
luc-zago <luc.zago@hotmail.com>
2021-03-07 10:54:16 -0300

    Novo teste

--- desafio1.sql
+++ desafio1.sql
@@ -1,132 +1,161 @@
-DROP DATABASE IF EXISTS SpotifyClone;
+DROP DATABASE IF EXISTS `SpotifyClone` ;
 
-CREATE DATABASE SpotifyClone;
+CREATE DATABASE IF NOT EXISTS `SpotifyClone`;
 
-USE SpotifyClone;
+USE `SpotifyClone` ;
 
-CREATE TABLE plans(
-plan_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-plan_name VARCHAR(50) NOT NULL,
-price DECIMAL(3,2) NOT NULL DEFAULT 0.00,
-PRIMARY KEY (plan_id)
-) engine = InnoDB;
+DROP TABLE IF EXISTS `plans` ;
 
-CREATE TABLE IF NOT EXISTS users (
-user_id INT PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(100) NOT NULL,
-age INT NOT NULL,
-plan_id INT NOT NULL,
-CONSTRAINT fk_users_plans
-FOREIGN KEY (plan_id)
-REFERENCES plans (plan_id)
-ON DELETE RESTRICT
-ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS `plans` (
+  `plan_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `plan_name` VARCHAR(100) NOT NULL,
+  `price` DECIMAL(5, 2) NOT NULL)
+ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS `users` ;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `age` INT NOT NULL,
+  `plan_id` INT NOT NULL,
+  CONSTRAINT `fk_users_plans`
+    FOREIGN KEY (`plan_id`)
+    REFERENCES `plans` (`plan_id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 )ENGINE = InnoDB;
 
-CREATE TABLE artists(
-artist_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-name VARCHAR(50) NOT NULL,
-PRIMARY KEY (artist_id)
-) engine = InnoDB;
+DROP TABLE IF EXISTS `artists` ;
 
-CREATE TABLE albums(
-album_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-album_name VARCHAR(50) NOT NULL,
-artist_id INT UNSIGNED NOT NULL,
-PRIMARY KEY (album_id),
-CONSTRAINT fk_albums_artist FOREIGN KEY (artist_id)
-REFERENCES artists (artist_id) ON DELETE RESTRICT ON UPDATE CASCADE
-) engine = InnoDB;
+CREATE TABLE IF NOT EXISTS `artists` (
+  `artist_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL )
+ENGINE = InnoDB;
 
-CREATE TABLE songs(
-song_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-song_name VARCHAR(50) NOT NULL,
-artist_id INT UNSIGNED NOT NULL,
-album_id INT UNSIGNED NOT NULL,
-PRIMARY KEY (song_id),
-CONSTRAINT fk_songs_artist FOREIGN KEY (artist_id)
-REFERENCES artists (artist_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-CONSTRAINT fk_songs_album FOREIGN KEY (album_id)
-REFERENCES albums (album_id) ON DELETE RESTRICT ON UPDATE CASCADE
-) engine = InnoDB;
+DROP TABLE IF EXISTS `albums` ;
 
-CREATE TABLE reproduction_history(
-history_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-user_id INT UNSIGNED NOT NULL,
-song_id INT UNSIGNED NOT NULL,
-PRIMARY KEY (history_id),
-KEY idx_fk_user_id (user_id),
-KEY idx_fk_song_id (song_id),
-CONSTRAINT fk_reproduction_history_user FOREIGN KEY (user_id)
-REFERENCES users (user_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-CONSTRAINT fk_reproduction_history_song FOREIGN KEY (song_id)
-REFERENCES songs (song_id) ON DELETE RESTRICT ON UPDATE CASCADE
-) engine = InnoDB;
+CREATE TABLE IF NOT EXISTS `albums` (
+  `album_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `album_name` VARCHAR(100) NOT NULL,
+  `artist_id` INT NOT NULL,
+  CONSTRAINT `fk_albums_artists`
+    FOREIGN KEY (`artist_id`)
+    REFERENCES `artists` (`artist_id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+)ENGINE = InnoDB;
 
-CREATE TABLE followers(
-follower_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-artist_id INT UNSIGNED NOT NULL,
-user_id INT UNSIGNED NOT NULL,
-PRIMARY KEY (follower_id),
-KEY idx_fk_artist_id (artist_id),
-KEY idx_fk_user_id (user_id),
-CONSTRAINT fk_followers_artist FOREIGN KEY (artist_id)
-REFERENCES artists (artist_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-CONSTRAINT fk_followers_user FOREIGN KEY (user_id)
-REFERENCES users (user_id) ON DELETE RESTRICT ON UPDATE CASCADE
-) engine = InnoDB;
+DROP TABLE IF EXISTS `followers` ;
 
-INSERT INTO plans (plan_id, plan_name, price)
-VALUES
-(1, 'Gratuito', 0.00),
-(2, 'Universitário', 5.99),
-(3, 'Familiar', 7.99);
-  
-INSERT INTO users (user_id, name, age, plan_id)
-VALUES
-(1, 'Tathi', 23, 1),
-(2, 'Cintia', 35, 3),
-(3, 'Bill', 20, 2),
-(4, 'Roger', 45, 1);
+CREATE TABLE IF NOT EXISTS `followers` (
+  `user_id` INT NOT NULL,
+  `artist_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `artist_id`),
+  CONSTRAINT `fk_follow_users`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_folow_artists`
+    FOREIGN KEY (`artist_id`)
+    REFERENCES `artists` (`artist_id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+)ENGINE = InnoDB;
 
-INSERT INTO artists (artist_id, name)
-VALUES
-(1, 'Freedie Shannon'),
-(2, 'Lance Day'),
-(3, 'Peter Strong'),
-(4, 'Walter Phoenix');
-  
-INSERT INTO albums (album_id, album_name, artist_id)
-VALUES
-(1, 'Envious', 4),
-(2, 'Exuberant', 4),
-(3, 'Hallowed Steam', 3),
-(4, 'Incandescent', 2),
-(5, 'Temporary Culture', 1);
-  
-INSERT INTO songs (song_id, song_name, artist_id, album_id)
-VALUES
-(1, 'Soul For Us', 4, 1),
-(2, 'Reflections Of Magic', 4, 1),
-(3, 'Dance With Her Own', 4, 1),
-(4, 'Troubles Of My Inner Fire', 4, 2),
-(5, 'Time Fireworks', 4, 2),
-(6, 'Magic Circus', 3, 3),
-(7, 'Honey, So Do I', 3, 3),
-(8, "Sweetie, Let's Go Wild", 3, 3),
-(9, 'She Knows', 3, 3),
-(10, 'Fantasy For Me', 2, 4),
-(11, 'Celebration Of More', 2, 4),
-(12, 'Rock His Everything', 2, 4),
-(13, 'Home Forever', 2, 4),
-(14, 'Diamond Power', 2, 4),
-(15, "Honey, Let's Be Silly", 2, 4),
-(16, 'Thang Of Thunder', 1, 5),
-(17, 'Words Of Her Life', 1, 5),
-(18, 'Without My Streets', 1, 5);
-  
-INSERT INTO reproduction_history (user_id, song_id)
-VALUES
+DROP TABLE IF EXISTS `songs` ;
+
+CREATE TABLE IF NOT EXISTS `songs` (
+  `song_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `song_name` VARCHAR(100) NOT NULL,
+  `artist_id` INT NOT NULL,
+  `album_id` INT NOT NULL,
+  CONSTRAINT `fk_songs_artists`
+    FOREIGN KEY (`artist_id`)
+    REFERENCES `artists` (`artist_id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+  CONSTRAINT `fk_songs_albums`
+    FOREIGN KEY (`album_id`)
+    REFERENCES `albums` (`album_id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+)ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS `reproduction_history` ;
+
+CREATE TABLE IF NOT EXISTS `reproduction_history` (
+  `user_id` INT NOT NULL,
+  `song_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `song_id`),
+  CONSTRAINT `fk_history_users`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`user_id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_history_songs`
+    FOREIGN KEY (`song_id`)
+    REFERENCES `songs` (`song_id`)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+)ENGINE = InnoDB;
+
+INSERT INTO `plans` (`name`, `price`) VALUES 
+('gratuito', 0),
+('familiar', 7.99),
+('universitário', 5.99);
+
+INSERT INTO `users` (`name`, `age`, `plan_id`) VALUES 
+('Thati', 23, 1),
+('Cintia', 35, 2),
+('Bill', 20, 3),
+('Roger', 45, 1);
+
+INSERT INTO `artists` (`name`) VALUES 
+('Freedie Shannon'),
+('Lance Day'),
+('Peter Strong'),
+('Walter Phoenix');
+
+INSERT INTO `albums` (`album_name`, `artist_id`) VALUES
+('Envious', 4),
+('Exuberant', 4),
+('Hallowed Steam', 3),
+('Incandescent', 2),
+('Temporary Culture', 1);
+
+INSERT INTO `followers` (`user_id`, `artist_id`) VALUES
+(1, 1),
+(1, 2),
+(1, 4),
+(2, 2),
+(2, 4),
+(3, 3),
+(3, 4),
+(4, 1);
+
+INSERT INTO `songs` (`song_name`, `album_id`) VALUES
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+('Sweetie, Let\'s Go Wild', 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+('Honey, Let\'s Be Silly', 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
+
+INSERT INTO `reproduction_history` (`user_id`, `song_id`) VALUES 
 (1, 1),
 (1, 6),
 (1, 14),
@@ -141,14 +170,3 @@ VALUES
 (4, 3),
 (4, 18),
 (4, 11);
-  
-INSERT INTO followers (artist_id, user_id)
-VALUES
-(1, 1),
-(1, 4),
-(2, 1),
-(2, 2),
-(3, 3),
-(4, 1),
-(4, 2),
-(4, 3);
