442fc98b558e5d0a554070d1d39d3fac9b1c2b3f
mateus <mateuslaalmeida@gmail.com>
2021-03-06 21:46:14 -0300

    Alterados typos

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 CREATE VIEW top_2_hits_do_momento AS
 SELECT m.music_name AS 'cancao',
-COUNT(mh.fk_music_id) AS 'reproducao'
+COUNT(mh.fk_music_id) AS 'reproducoes'
 FROM SpotifyClone.musics as m
 INNER JOIN SpotifyClone.musics_history AS mh ON mh.fk_music_id = m.music_id
 GROUP BY m.music_name
