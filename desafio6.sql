bdefbda0d74e4dfe84f25de3a839dd9f3babe0fe
Lfocosi <luccafocosi@gmail.com>
2021-03-06 22:46:29 -0300

    desafio 6 parte 5

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ CREATE VIEW faturamento_atual AS
   SELECT MIN(p.preço) AS 'faturamento_minimo',
   MAX(p.preço) AS 'faturamento_maximo',
   concat(round(AVG(p.preço), 2)) AS 'faturamento_medio',
-  SUM(p.preço) AS 'faturamento_total'
+  concat(SUM(p.preço)) AS 'faturamento_total'
   FROM SpotifyClone.usuarios AS u
   INNER JOIN SpotifyClone.planos AS p
   ON p.plano_id = u.plano_id;
