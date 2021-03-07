7028b1da2b9f3fe532eb457e4b10fed761019cf6
luc-zago <luc.zago@hotmail.com>
2021-03-07 15:44:44 -0300

    Alterações no desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,7 +1,8 @@
+USE SpotifyClone;
 DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
-BEFORE DELETE ON users
+BEFORE DELETE ON SpotifyClone.users
 FOR EACH ROW
 BEGIN
 DELETE FROM followers WHERE OLD.user_id = user_id;
