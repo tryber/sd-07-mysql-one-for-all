7c9b66ca62fabe0f133fc6a04b9ca23972e4c8c2
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-03-06 23:10:35 -0300

    desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,5 @@
+CREATE VIEW SpotifyClone.estatisticas_musicais AS
+SELECT
+    ( SELECT COUNT(*) FROM SpotifyClone.Cancao ) AS cancoes,
+    ( SELECT COUNT(*) FROM SpotifyClone.Artista ) AS artistas,
+    ( SELECT COUNT(*) FROM SpotifyClone.Album ) AS albuns;
