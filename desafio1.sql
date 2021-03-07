3572fd2e52981f7417084bced1908f2642da0456
luc-zago <luc.zago@hotmail.com>
2021-03-07 12:00:53 -0300

    Teste alternativo

--- desafio1.sql
+++ desafio1.sql
@@ -1,209 +1,129 @@
--- -----------------------------------------------------
--- DATABASE SpotifyClone
--- -----------------------------------------------------
-DROP DATABASE IF EXISTS `SpotifyClone` ;
+DROP DATABASE IF EXISTS SpotifyClone;
 
--- -----------------------------------------------------
--- DATABASE SpotifyClone
--- -----------------------------------------------------
-CREATE DATABASE IF NOT EXISTS `SpotifyClone`;
+CREATE DATABASE SpotifyClone;
 
-USE `SpotifyClone` ;
+USE SpotifyClone;
 
--- -----------------------------------------------------
--- Table `plans`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `plans` ;
+CREATE TABLE plans(
+plan_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+plan_name VARCHAR(50) NOT NULL,
+price DECIMAL(3,2) NOT NULL DEFAULT 0.00,
+PRIMARY KEY (plan_id)
+) engine = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `plans` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `name` VARCHAR(100) NOT NULL,
-  `price` DECIMAL(5, 2) NOT NULL)
-ENGINE = InnoDB;
+CREATE TABLE users(
+user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(50) NOT NULL,
+age INT UNSIGNED NOT NULL,
+plan_id INT UNSIGNED NOT NULL,
+PRIMARY KEY (user_id),
+KEY idx_fk_plan_id (plan_id),
+CONSTRAINT fk_users_plan FOREIGN KEY (plan_id)
+REFERENCES plans (plan_id) ON DELETE RESTRICT ON UPDATE CASCADE
+) engine = InnoDB;
 
+CREATE TABLE artists(
+artist_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(50) NOT NULL,
+PRIMARY KEY (artist_id)
+) engine = InnoDB;
 
--- -----------------------------------------------------
--- Table `users`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `users` ;
+CREATE TABLE albums(
+album_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+album_name VARCHAR(50) NOT NULL,
+artist_id INT UNSIGNED NOT NULL,
+PRIMARY KEY (album_id),
+CONSTRAINT fk_albums_artist FOREIGN KEY (artist_id)
+REFERENCES artists (artist_id) ON DELETE RESTRICT ON UPDATE CASCADE
+) engine = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `name` VARCHAR(100) NOT NULL,
-  `age` INT NOT NULL,
-  `plan_id` INT NOT NULL,
-  CONSTRAINT `fk_users_plans`
-    FOREIGN KEY (`plan_id`)
-    REFERENCES `plans` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE songs(
+song_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+song_name VARCHAR(50) NOT NULL,
+artist_id INT UNSIGNED NOT NULL,
+album_id INT UNSIGNED NOT NULL,
+PRIMARY KEY (song_id),
+CONSTRAINT fk_songs_artist FOREIGN KEY (artist_id)
+REFERENCES artists (artist_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+CONSTRAINT fk_songs_album FOREIGN KEY (album_id)
+REFERENCES albums (album_id) ON DELETE RESTRICT ON UPDATE CASCADE
+) engine = InnoDB;
 
+CREATE TABLE reproduction_history(
+user_id INT UNSIGNED NOT NULL,
+song_id INT UNSIGNED NOT NULL,
+PRIMARY KEY (user_id, song_id),
+KEY idx_fk_user_id (user_id),
+KEY idx_fk_song_id (song_id),
+CONSTRAINT fk_reproduction_history_user FOREIGN KEY (user_id)
+REFERENCES users (user_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+CONSTRAINT fk_reproduction_history_song FOREIGN KEY (song_id)
+REFERENCES songs (song_id) ON DELETE RESTRICT ON UPDATE CASCADE
+) engine = InnoDB;
 
--- -----------------------------------------------------
--- Table `artists`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `artists` ;
+CREATE TABLE followers(
+artist_id INT UNSIGNED NOT NULL,
+user_id INT UNSIGNED NOT NULL,
+PRIMARY KEY (artist_id, user_id),
+KEY idx_fk_artist_id (artist_id),
+KEY idx_fk_user_id (user_id),
+CONSTRAINT fk_followers_artist FOREIGN KEY (artist_id)
+REFERENCES artists (artist_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+CONSTRAINT fk_followers_user FOREIGN KEY (user_id)
+REFERENCES users (user_id) ON DELETE RESTRICT ON UPDATE CASCADE
+) engine = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `artists` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `name` VARCHAR(100) NOT NULL )
-ENGINE = InnoDB;
+INSERT INTO plans (plan_name, price)
+VALUES
+('Gratuito', 0.00),
+('Universitário', 5.99),
+('Familiar', 7.99);
+  
+INSERT INTO users (name, age, plan_id)
+VALUES
+('Tathi', 23, 1),
+('Cintia', 35, 3),
+('Bill', 20, 2),
+('Roger', 45, 1);
 
-
--- -----------------------------------------------------
--- Table `albums`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `albums` ;
-
-CREATE TABLE IF NOT EXISTS `albums` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `name` VARCHAR(100) NOT NULL,
-  `artist_id` INT NOT NULL,
-  CONSTRAINT `fk_albums_artists`
-    FOREIGN KEY (`artist_id`)
-    REFERENCES `artists` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `follow`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `follow` ;
-
-CREATE TABLE IF NOT EXISTS `follow` (
-  `user_id` INT NOT NULL,
-  `artist_id` INT NOT NULL,
-  PRIMARY KEY (`user_id`, `artist_id`),
-  CONSTRAINT `fk_follow_users`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_folow_artists`
-    FOREIGN KEY (`artist_id`)
-    REFERENCES `artists` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `songs`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `songs` ;
-
-CREATE TABLE IF NOT EXISTS `songs` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `name` VARCHAR(100) NOT NULL,
-  `album_id` INT NOT NULL,
-  CONSTRAINT `fk_songs_albums`
-    FOREIGN KEY (`album_id`)
-    REFERENCES `albums` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `history`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `history` ;
-
-CREATE TABLE IF NOT EXISTS `history` (
-  `user_id` INT NOT NULL,
-  `song_id` INT NOT NULL,
-  PRIMARY KEY (`user_id`, `song_id`),
-  CONSTRAINT `fk_history_users`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_history_songs`
-    FOREIGN KEY (`song_id`)
-    REFERENCES `songs` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
--- -----------------------------------------------------
--- Data for table `plans`
--- -----------------------------------------------------
-INSERT INTO `plans` (`id`, `name`, `price`) VALUES 
-(DEFAULT, 'gratuito', 0),
-(DEFAULT, 'familiar', 7.99),
-(DEFAULT, 'universitário', 5.99);
-
--- -----------------------------------------------------
--- Data for table `users`
--- -----------------------------------------------------
-INSERT INTO `users` (`id`, `name`, `age`, `plan_id`) VALUES 
-(DEFAULT, 'Thati', 23, 1),
-(DEFAULT, 'Cintia', 35, 2),
-(DEFAULT, 'Bill', 20, 3),
-(DEFAULT, 'Roger', 45, 1);
-
--- -----------------------------------------------------
--- Data for table `artists`
--- -----------------------------------------------------
-INSERT INTO `artists` (`id`, `name`) VALUES 
-(DEFAULT, 'Walter Phoenix'),
-(DEFAULT, 'Peter Strong'),
-(DEFAULT, 'Lance Day'),
-(DEFAULT, 'Freedie Shannon');
-
--- -----------------------------------------------------
--- Data for table `albums`
--- -----------------------------------------------------
-INSERT INTO `albums` (`id`, `name`, `artist_id`) VALUES
-(DEFAULT, 'Envious', 1),
-(DEFAULT, 'Exuberant', 1),
-(DEFAULT, 'Hallowed Steam', 2),
-(DEFAULT, 'Incandescent', 3),
-(DEFAULT, 'Temporary Culture', 4);
-
--- -----------------------------------------------------
--- Data for table `follow`
--- -----------------------------------------------------
-INSERT INTO `follow` (`user_id`, `artist_id`) VALUES
-(1, 1),
-(1, 3),
-(1, 4),
-(2, 1),
-(2, 3),
-(3, 1),
-(3, 2),
-(4, 4);
-
--- -----------------------------------------------------
--- Data for table `songs`
--- -----------------------------------------------------
-INSERT INTO `songs` (`id`, `name`, `album_id`) VALUES
-(DEFAULT, 'Soul For Us', 1),
-(DEFAULT, 'Reflections Of Magic', 1),
-(DEFAULT, 'Dance With Her Own', 1),
-(DEFAULT, 'Troubles Of My Inner Fire', 2),
-(DEFAULT, 'Time Fireworks', 2),
-(DEFAULT, 'Magic Circus', 3),
-(DEFAULT, 'Honey, So Do I', 3),
-(DEFAULT, 'Sweetie, Let\'s Go Wild', 3),
-(DEFAULT, 'She Knows', 3),
-(DEFAULT, 'Fantasy For Me', 4),
-(DEFAULT, 'Celebration Of More', 4),
-(DEFAULT, 'Rock His Everything', 4),
-(DEFAULT, 'Home Forever', 4),
-(DEFAULT, 'Diamond Power', 4),
-(DEFAULT, 'Honey, Let\'s Be Silly', 4),
-(DEFAULT, 'Thang Of Thunder', 5),
-(DEFAULT, 'Words Of Her Life', 5),
-(DEFAULT, 'Without My Streets', 5);
-
--- -----------------------------------------------------
--- Data for table `history`
--- -----------------------------------------------------
-INSERT INTO `history` (`user_id`, `song_id`) VALUES 
+INSERT INTO artists (name)
+VALUES
+('Freedie Shannon'),
+('Lance Day'),
+('Peter Strong'),
+('Walter Phoenix');
+  
+INSERT INTO albums (album_name, artist_id)
+VALUES
+('Envious', 4),
+('Exuberant', 4),
+('Hallowed Steam', 3),
+('Incandescent', 2),
+('Temporary Culture', 1);
+  
+INSERT INTO songs (song_name, artist_id, album_id)
+VALUES
+('Soul For Us', 4, 1),
+('Reflections Of Magic', 4, 1),
+('Dance With Her Own', 4, 1),
+('Troubles Of My Inner Fire', 4, 2),
+('Time Fireworks', 4, 2),
+('Magic Circus', 3, 3),
+('Honey, So Do I', 3, 3),
+("Sweetie, Let's Go Wild", 3, 3),
+('She Knows', 3, 3),
+('Fantasy For Me', 2, 4),
+('Celebration Of More', 2, 4),
+('Rock His Everything', 2, 4),
+('Home Forever', 2, 4),
+('Diamond Power', 2, 4),
+("Honey, Let's Be Silly", 2, 4),
+('Thang Of Thunder', 1, 5),
+('Words Of Her Life', 1, 5),
+('Without My Streets', 1, 5);
+  
+INSERT INTO reproduction_history (user_id, song_id)
+VALUES
 (1, 1),
 (1, 6),
 (1, 14),
@@ -218,3 +138,14 @@ INSERT INTO `history` (`user_id`, `song_id`) VALUES
 (4, 3),
 (4, 18),
 (4, 11);
+  
+INSERT INTO followers (artist_id, user_id)
+VALUES
+(1, 1),
+(1, 4),
+(2, 1),
+(2, 2),
+(3, 3),
+(4, 1),
+(4, 2),
+(4, 3);
