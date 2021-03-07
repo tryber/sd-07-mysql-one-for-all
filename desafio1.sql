088ea427c1c8b27a5b2aa6041c26c72f9aeb20eb
Flavio Oliveira <flavsoliver@gmail.com>
2021-03-06 22:01:30 -0300

    desafio 1 - bora?

--- desafio1.sql
+++ desafio1.sql
@@ -1,65 +1,68 @@
 DROP database if exists SpotifyClone;
+
 CREATE database SpotifyClone;
+
 CREATE SCHEMA IF NOT EXISTS SpotifyClone;
+
 USE SpotifyClone;
-DROP TABLE IF EXISTS plano;
+
 CREATE TABLE IF NOT EXISTS plano (
   `plano_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `plano_nome` VARCHAR(45) NOT NULL,
   `valor_plano` FLOAT NOT NULL
 ) ENGINE = InnoDB;
-DROP TABLE IF EXISTS usuario;
+
 CREATE TABLE IF NOT EXISTS usuario (
   `usuario_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `usuario_nome` VARCHAR(45) NOT NULL,
   `idade` INT NOT NULL,
   `plano_id` INT NOT NULL,
-  CONSTRAINT `plano_id_usuario_fk` FOREIGN KEY (`plano_id`) REFERENCES SpotifyClone.plano (`plano_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  FOREIGN KEY (`plano_id`) REFERENCES plano (`plano_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
-DROP TABLE IF EXISTS artista;
+
 CREATE TABLE IF NOT EXISTS artista (
   `artista_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `artista_nome` VARCHAR(45) NOT NULL
 ) ENGINE = InnoDB;
-DROP TABLE IF EXISTS album;
+
 CREATE TABLE IF NOT EXISTS album (
   `album_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `album_nome` VARCHAR(45) NOT NULL,
   `artista_id` INT NOT NULL,
-  CONSTRAINT `artista_id_album_fk` FOREIGN KEY (`artista_id`) REFERENCES SpotifyClone.artista (`artista_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  FOREIGN KEY (`artista_id`) REFERENCES artista (`artista_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
-DROP TABLE IF EXISTS musica;
+
 CREATE TABLE IF NOT EXISTS musica (
   `musica_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `musica_nome` VARCHAR(45) NOT NULL,
   `artista_id` INT NOT NULL,
   `album_id` INT NOT NULL,
-  CONSTRAINT `artista_id_musica_fk` FOREIGN KEY (`artista_id`) REFERENCES SpotifyClone.artista (`artista_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `album_id_musica_fk` FOREIGN KEY (`album_id`) REFERENCES SpotifyClone.album (`album_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  FOREIGN KEY (`artista_id`) REFERENCES artista (`artista_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  FOREIGN KEY (`album_id`) REFERENCES album (`album_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
-DROP TABLE IF EXISTS historico;
+
 CREATE TABLE IF NOT EXISTS historico (
   `historico_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `usuario_id` INT NOT NULL,
   `musica_id` INT NOT NULL,
-  CONSTRAINT `usuario_id_historico_fk` FOREIGN KEY (`usuario_id`) REFERENCES SpotifyClone.usuario (`usuario_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `musica_id_historico_fk` FOREIGN KEY (`musica_id`) REFERENCES SpotifyClone.musica (`musica_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  FOREIGN KEY (`usuario_id`) REFERENCES usuario (`usuario_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  FOREIGN KEY (`musica_id`) REFERENCES musica (`musica_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
-DROP TABLE IF EXISTS seguindo;
+
 CREATE TABLE IF NOT EXISTS seguindo (
   `seguindo_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `usuario_id` INT NOT NULL,
   `artista_id` INT NOT NULL,
-  CONSTRAINT `usuario_id_seguindo_fk` FOREIGN KEY (`usuario_id`) REFERENCES SpotifyClone.usuario (`usuario_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `artista_id_seguindo_fk` FOREIGN KEY (`artista_id`) REFERENCES SpotifyClone.artista (`artista_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  FOREIGN KEY (`usuario_id`) REFERENCES usuario (`usuario_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  FOREIGN KEY (`artista_id`) REFERENCES artista (`artista_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
-LOCK TABLES plano WRITE;
+
+
 INSERT INTO plano (`plano_id`, `plano_nome`, `valor_plano`)
 VALUES (DEFAULT, 'gratuito', 0),
   (DEFAULT, 'familiar', 7.99),
   (DEFAULT, 'universitário', 5.99);
-UNLOCK TABLES;
-LOCK TABLES usuario WRITE;
+
 INSERT INTO usuario (
     `usuario_id`,
     `usuario_nome`,
@@ -70,23 +73,20 @@ VALUES (DEFAULT, 'Thati', 23, 1),
   (DEFAULT, 'Cintia', 35, 2),
   (DEFAULT, 'Bill', 20, 3),
   (DEFAULT, 'Roger', 45, 1);
-UNLOCK TABLES;
-LOCK TABLES artista WRITE;
+
 INSERT INTO artista (`artista_id`, `artista_nome`)
 VALUES (DEFAULT, 'Walter Phoenix'),
   (DEFAULT, 'Peter Strong'),
   (DEFAULT, 'Lance Day'),
   (DEFAULT, 'Freedie Shannon');
-UNLOCK TABLES;
-LOCK TABLES album WRITE;
+
 INSERT INTO album (`album_id`, `album_nome`, `artista_id`)
 VALUES (DEFAULT, 'Envious', 1),
   (DEFAULT, 'Exuberant', 1),
   (DEFAULT, 'Hallowed Steam', 2),
   (DEFAULT, 'Incandescent', 3),
   (DEFAULT, 'Temporary Culture', 4);
-UNLOCK TABLES;
-LOCK TABLES musica WRITE;
+
 INSERT INTO musica (
     `musica_id`,
     `musica_nome`,
@@ -111,8 +111,7 @@ VALUES (DEFAULT, "Soul For Us", 1, 1),
   (DEFAULT, "Thang Of Thunder", 4, 5),
   (DEFAULT, "Words Of Her Life", 4, 5),
   (DEFAULT, "Without My Streets", 4, 5);
-UNLOCK TABLES;
-LOCK TABLES historico WRITE;
+
 INSERT INTO historico (`historico_id`, `usuario_id`, `musica_id`)
 VALUES (DEFAULT, 1, 1),
   (DEFAULT, 1, 6),
@@ -128,8 +127,7 @@ VALUES (DEFAULT, 1, 1),
   (DEFAULT, 4, 3),
   (DEFAULT, 4, 18),
   (DEFAULT, 4, 11);
-UNLOCK TABLES;
-LOCK TABLES seguindo WRITE;
+
 INSERT INTO seguindo (`seguindo_id`, `usuario_id`, `artista_id`)
 VALUES (DEFAULT, 1, 1),
   (DEFAULT, 1, 4),
@@ -139,4 +137,3 @@ VALUES (DEFAULT, 1, 1),
   (DEFAULT, 3, 2),
   (DEFAULT, 3, 1),
   (DEFAULT, 4, 4);
-UNLOCK TABLES;
