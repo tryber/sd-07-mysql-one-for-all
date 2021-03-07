b75c4c7b69068bd76595908ef6e5f204ff6acdae
marcelo ivan <sagacello@hotmail.com>
2021-03-07 12:05:11 -0300

    desafio 11 + desafio 10 no local certo

--- desafio9.sql
+++ desafio9.sql
@@ -1,13 +1,1 @@
-USE SpotifyClone; 
-DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historico(ID_USUARIO INT)
-RETURNS INT READS SQL DATA
-BEGIN
-DECLARE quantidade INT;
-SELECT COUNT(musicas_id)
-FROM historico
-WHERE usuario_id = ID_USUARIO
-INTO quantidade;
-RETURN quantidade;
-END $$
-DELIMITER;
+
