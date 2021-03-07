5fdf2d88c51d2a38874aa6802d74c95dc3fa4944
Jr3564 <rodolfojr94@gmail.com>
2021-03-07 19:57:05 -0300

    Requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+USE SpotifyClone;
+
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+    AFTER DELETE ON Users
+    FOR EACH ROW
+BEGIN
+	DELETE FROM Followings
+    WHERE Followings.use_id = OLD.use_id;
+	DELETE FROM Historic
+    WHERE Historic.use_id = OLD.use_id;
+END $$
+DELIMITER ;
