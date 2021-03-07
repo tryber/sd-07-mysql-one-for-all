327ac69aca347bc3360bc6455abbf8a7c45533aa
luc-zago <luc.zago@hotmail.com>
2021-03-07 12:08:17 -0300

    Teste no desafio 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,4 +1,3 @@
-USE SpotifyClone;
 CREATE VIEW estatisticas_musicais AS
 SELECT (SELECT COUNT(song_id) FROM SpotifyClone.songs) AS cancoes,
 (SELECT COUNT(artist_id) FROM SpotifyClone.artists) AS artistas,
