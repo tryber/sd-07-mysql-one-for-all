0fe117bb2054b99a3ee435f0f7ca38471bbadfc8
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-03-07 20:57:55 -0300

    Exercise 8 - done-2

--- desafio8.sql
+++ desafio8.sql
@@ -3,6 +3,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuario
 FOR EACH ROW
 BEGIN
+DECLARE user_id INT;
 SET user_id = OLD.usuario_id;
 DELETE FROM SpotifyClone.historico_reproducao
 WHERE usuario_id = user_id;
