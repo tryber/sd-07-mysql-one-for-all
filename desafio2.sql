3cf95130b2171d0d4e003b148690ca3cf38d2b65
kramer <kramerscs@gmail.com>
2021-03-07 14:53:41 -0300

    2 and 8

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 CREATE VIEW estatisticas_musicais AS 
-  SELECT CONVERT(SUM(Number_of_songs), DECIMAL) AS cancoes, COUNT(artist_id) AS artistas, CONVERT(SUM(albuns), DECIMAL) AS albuns FROM(
+  SELECT CONVERT(SUM(Number_of_songs), DECIMAL(5, 0)) AS cancoes, COUNT(artist_id) AS artistas, CONVERT(SUM(albuns), DECIMAL(5, 0)) AS albuns FROM(
   SELECT C.Number_of_songs, C.artist_id, COUNT(Album.album_id) AS albuns FROM(
   SELECT COUNT(Songs.song_id) AS Number_of_songs, Artists.artist_id
   FROM SpotifyClone.Songs AS Songs
