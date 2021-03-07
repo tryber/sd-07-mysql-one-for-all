8d24f26406aa212d43f0f6175fcf5ef0483e3f51
Carol Bezerra <carol.arbe@gmail.com>
2021-03-07 15:13:10 -0300

    Desafio 1 com CC

--- desafio1.sql
+++ desafio1.sql
@@ -5,13 +5,13 @@ CREATE DATABASE SpotifyClone;
 USE SpotifyClone;
 
 CREATE TABLE planos(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
     tipo VARCHAR(50) NOT NULL,
     valor DECIMAL(5,2) NOT NULL
 ) engine = InnoDB;
 
 CREATE TABLE usuarios(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     idade INT NOT NULL,
     plano_id INT,
@@ -19,19 +19,19 @@ CREATE TABLE usuarios(
 ) engine = InnoDB;
 
 CREATE TABLE artistas(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL
 ) engine = InnoDB;
 
 CREATE TABLE albuns(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
     album VARCHAR(50) NOT NULL,
     artista_id INT,
     FOREIGN KEY (artista_id) REFERENCES artistas (id)
 ) engine = InnoDB;
 
 CREATE TABLE usuario_segue_artista(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
     usuario_id INT NOT NULL,
     artista_id INT,
     FOREIGN KEY (usuario_id) REFERENCES usuarios (id),
@@ -39,7 +39,7 @@ CREATE TABLE usuario_segue_artista(
 ) engine = InnoDB;
 
 CREATE TABLE cancoes(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     artista_id INT,
     album_id INT,
@@ -48,7 +48,7 @@ CREATE TABLE cancoes(
 ) engine = InnoDB;
 
 CREATE TABLE usuario_ouve_cancao(
-	id INT PRIMARY KEY AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
     usuario_id INT NOT NULL,
     cancao_id INT,
     FOREIGN KEY (usuario_id) REFERENCES usuarios (id),
