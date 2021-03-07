1de086bc5719242aa839c381bf4ee244ecd492ef
kramer <kramerscs@gmail.com>
2021-03-07 15:03:18 -0300

    2

--- desafio2.sql
+++ desafio2.sql
@@ -1,13 +1,7 @@
 CREATE VIEW estatisticas_musicais AS 
-  SELECT CONVERT(SUM(Number_of_songs), DECIMAL(5, 0)) AS cancoes, COUNT(artist_id) AS artistas, CONVERT(SUM(albuns), DECIMAL(5, 0)) AS albuns FROM(
-  SELECT C.Number_of_songs, C.artist_id, COUNT(Album.album_id) AS albuns FROM(
-  SELECT COUNT(Songs.song_id) AS Number_of_songs, Artists.artist_id
-  FROM SpotifyClone.Songs AS Songs
-  INNER JOIN SpotifyClone.Album AS Album
-  ON Songs.album_id = Album.album_id
-  INNER JOIN SpotifyClone.Artists AS Artists
-  ON Album.artist_id = Artists.artist_id
-  GROUP BY Artists.artist_id) AS C
-  INNER JOIN SpotifyClone.Album AS Album
-  ON C.artist_id = Album.artist_id
-  GROUP BY C.artist_id) AS C2;
+  SELECT COUNT(DISTINCT(S.song_id)) AS cancoes, COUNT(DISTINCT(Art.artist_id)) AS artistas, COUNT(DISTINCT(A.album_id)) AS albuns
+  FROM SpotifyClone.Songs AS S
+  INNER JOIN SpotifyClone.Album AS A
+  ON S.album_id = A.album_id
+  INNER JOIN SpotifyClone.Artists AS Art
+  ON A.artist_id = Art.artist_id;
