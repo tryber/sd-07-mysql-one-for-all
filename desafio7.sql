16cac70ca3f826376a9ffa5eedac29cf45f3deb8
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-03-07 15:46:48 -0300

    desafio 7 cc

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 CREATE VIEW SpotifyClone.perfil_artistas AS
 SELECT a.`nome_artista` AS `artista`,
-	al.`nome_album` AS `album`,
-  COUNT(s.`artista_id`) AS `seguidores`
+al.`nome_album` AS `album`,
+COUNT(s.`artista_id`) AS `seguidores`
 FROM SpotifyClone.seguidores AS s
   INNER JOIN SpotifyClone.artistas AS a ON a.`artista_id` = s.`artista_id`
   JOIN SpotifyClone.usuarios AS u ON u.`usuario_id` = s.`usuario_id`
