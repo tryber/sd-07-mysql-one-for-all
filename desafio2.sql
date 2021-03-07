5bd869da402661392e39b7a5a1397323820fd174
Flavio Oliveira <flavsoliver@gmail.com>
2021-03-07 11:28:45 -0300

    desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,14 @@
+USE SpotifyClone;
+CREATE VIEW `estatisticas_musicais` AS
+SELECT (
+    SELECT COUNT(musica_id)
+    FROM musica
+  ) AS cancoes,
+  (
+    SELECT COUNT(artista_id)
+    FROM artista
+  ) AS artistas,
+  (
+    SELECT COUNT(album_id)
+    FROM album
+  ) AS albuns;
