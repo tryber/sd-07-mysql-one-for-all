9449ca52f857f55724a1482a6629578fa2efc195
Flavio Oliveira <flavsoliver@gmail.com>
2021-03-06 23:47:48 -0300

    desafio 01 teste

--- desafio1.sql
+++ desafio1.sql
@@ -1,118 +1,91 @@
-DROP database if exists SpotifyClone;
-
-CREATE database SpotifyClone;
-
+DROP DATABASE IF EXISTS SpotifyClone;
+CREATE DATABASE SpotifyClone;
 CREATE SCHEMA IF NOT EXISTS SpotifyClone;
-
 USE SpotifyClone;
-
 CREATE TABLE IF NOT EXISTS plano (
-  `plano_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `plano_nome` VARCHAR(45) NOT NULL,
-  `valor_plano` FLOAT NOT NULL
+  `plano_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `nome_plano` VARCHAR(45) NOT NULL,
+  `valor` FLOAT NOT NULL
 ) ENGINE = InnoDB;
-
 CREATE TABLE IF NOT EXISTS usuario (
-  `usuario_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `usuario_nome` VARCHAR(45) NOT NULL,
+  `usuario_id` INT PRIMARY KEY AUTO_INCREMENT,
+  nome_usuario VARCHAR(100) NOT NULL,
   `idade` INT NOT NULL,
   `plano_id` INT NOT NULL,
   FOREIGN KEY (`plano_id`) REFERENCES plano (`plano_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
-
 CREATE TABLE IF NOT EXISTS artista (
-  `artista_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `artista_nome` VARCHAR(45) NOT NULL
+  `artista_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `nome_artista` VARCHAR(45) NOT NULL
 ) ENGINE = InnoDB;
-
 CREATE TABLE IF NOT EXISTS album (
-  `album_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `album_nome` VARCHAR(45) NOT NULL,
+  `album_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `nome_album` VARCHAR(45) NOT NULL,
   `artista_id` INT NOT NULL,
   FOREIGN KEY (`artista_id`) REFERENCES artista (`artista_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
-
 CREATE TABLE IF NOT EXISTS musica (
-  `musica_id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `musica_nome` VARCHAR(45) NOT NULL,
-  `artista_id` INT NOT NULL,
+  `musica_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `nome_musica` VARCHAR(45) NOT NULL,
   `album_id` INT NOT NULL,
-  FOREIGN KEY (`artista_id`) REFERENCES artista (`artista_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  FOREIGN KEY (`album_id`) REFERENCES album (`album_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `artista_id` INT NOT NULL,
+  FOREIGN KEY (`album_id`) REFERENCES album (`album_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  FOREIGN KEY (`artista_id`) REFERENCES artista (`artista_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS historico (
+CREATE TABLE IF NOT EXISTS historico_reproducao (
   `usuario_id` INT NOT NULL,
   `musica_id` INT NOT NULL,
-  PRIMARY KEY (usuario_id, musica_id),
+  PRIMARY KEY (`usuario_id`, `musica_id`),
   FOREIGN KEY (`usuario_id`) REFERENCES usuario (`usuario_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   FOREIGN KEY (`musica_id`) REFERENCES musica (`musica_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS seguindo (
+CREATE TABLE IF NOT EXISTS seguidores (
   `usuario_id` INT NOT NULL,
   `artista_id` INT NOT NULL,
-  PRIMARY KEY (usuario_id, artista_id),
+  PRIMARY KEY (`usuario_id`, `artista_id`),
   FOREIGN KEY (`usuario_id`) REFERENCES usuario (`usuario_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   FOREIGN KEY (`artista_id`) REFERENCES artista (`artista_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
-
-
-INSERT INTO plano (`plano_id`, `plano_nome`, `valor_plano`)
-VALUES (DEFAULT, 'gratuito', 0),
-  (DEFAULT, 'familiar', 7.99),
-  (DEFAULT, 'universitário', 5.99);
-
-INSERT INTO usuario (
-    `usuario_id`,
-    `usuario_nome`,
-    `idade`,
-    `plano_id`
-  )
-VALUES (DEFAULT, 'Thati', 23, 1),
-  (DEFAULT, 'Cintia', 35, 2),
-  (DEFAULT, 'Bill', 20, 3),
-  (DEFAULT, 'Roger', 45, 1);
-
-INSERT INTO artista (`artista_id`, `artista_nome`)
-VALUES (DEFAULT, 'Walter Phoenix'),
-  (DEFAULT, 'Peter Strong'),
-  (DEFAULT, 'Lance Day'),
-  (DEFAULT, 'Freedie Shannon');
-
-INSERT INTO album (`album_id`, `album_nome`, `artista_id`)
-VALUES (DEFAULT, 'Envious', 1),
-  (DEFAULT, 'Exuberant', 1),
-  (DEFAULT, 'Hallowed Steam', 2),
-  (DEFAULT, 'Incandescent', 3),
-  (DEFAULT, 'Temporary Culture', 4);
-
-INSERT INTO musica (
-    `musica_id`,
-    `musica_nome`,
-    `artista_id`,
-    `album_id`
-  )
-VALUES (DEFAULT, "Soul For Us", 1, 1),
-  (DEFAULT, "Reflections Of Magic", 1, 1),
-  (DEFAULT, "Dance With Her Own", 1, 1),
-  (DEFAULT, "Troubles Of My Inner Fire", 1, 2),
-  (DEFAULT, "Time Fireworks", 1, 2),
-  (DEFAULT, "Magic Circus", 2, 3),
-  (DEFAULT, "Honey, So Do I", 2, 3),
-  (DEFAULT, "Sweetie, Let's Go Wild", 2, 3),
-  (DEFAULT, "She Knows", 2, 3),
-  (DEFAULT, "Fantasy For Me", 3, 4),
-  (DEFAULT, "Celebration Of More", 3, 4),
-  (DEFAULT, "Rock His Everything", 3, 4),
-  (DEFAULT, "Home Forever", 3, 4),
-  (DEFAULT, "Diamond Power", 3, 4),
-  (DEFAULT, "Honey, Let's Be Silly", 3, 4),
-  (DEFAULT, "Thang Of Thunder", 4, 5),
-  (DEFAULT, "Words Of Her Life", 4, 5),
-  (DEFAULT, "Without My Streets", 4, 5);
-
-INSERT INTO historico (`usuario_id`, `musica_id`)
+INSERT INTO usuario (`nome_usuario`, `idade`, `plano_id`)
+VALUES ('Thati', 23, 1),
+  ('Cintia', 35, 2),
+  ('Bill', 20, 3),
+  ('Roger', 45, 1);
+INSERT INTO plano (`nome_plano`, `valor`)
+VALUES ('gratuito', 0),
+  ('familiar', 7.99),
+  ('universitário', 5.99);
+INSERT INTO artista (`nome_artista`)
+VALUES ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shannon');
+INSERT INTO album (`nome_album`, `artista_id`)
+VALUES ('Envious', 1),
+  ('Exuberant', 1),
+  ('Hallowed Steam', 2),
+  ('Incandescent', 3),
+  ('Temporary Culture', 4);
+INSERT INTO musica (`nome_musica`, `album_id`, `artista_id`)
+VALUES ('Soul For Us', 1, 1),
+  ('Reflections Of Magic', 1, 1),
+  ('Dance With Her Own', 1, 1),
+  ('Troubles Of My Inner Fire', 2, 1),
+  ('Time Fireworks', 2, 1),
+  ('Magic Circus', 3, 2),
+  ('Honey, So Do I', 3, 2),
+  ("Sweetie, Let's Go Wild", 3, 2),
+  ('She Knows', 3, 2),
+  ('Fantasy For Me', 4, 3),
+  ('Celebration Of More', 4, 3),
+  ('Rock His Everything', 4, 3),
+  ('Home Forever', 4, 3),
+  ('Diamond Power', 4, 3),
+  ("Honey, Let's Be Silly", 4, 3),
+  ('Thang Of Thunder', 5, 4),
+  ('Words Of Her Life', 5, 4),
+  ('Without My Streets', 5, 4);
+INSERT INTO historico_reproducao (`usuario_id`, `musica_id`)
 VALUES (1, 1),
   (1, 6),
   (1, 14),
@@ -127,8 +100,7 @@ VALUES (1, 1),
   (4, 3),
   (4, 18),
   (4, 11);
-
-INSERT INTO seguindo (`usuario_id`, `artista_id`)
+INSERT INTO seguidores (`usuario_id`, `artista_id`)
 VALUES (1, 1),
   (1, 4),
   (1, 3),
