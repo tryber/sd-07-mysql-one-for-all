23046df811073b48984ffdd54a9cc8a6203a4d50
Carol Bezerra <carol.arbe@gmail.com>
2021-03-07 15:52:35 -0300

    Desafio 1 com valores

--- desafio1.sql
+++ desafio1.sql
@@ -25,7 +25,7 @@ id INT PRIMARY KEY AUTO_INCREMENT,
 
 CREATE TABLE albuns(
 id INT PRIMARY KEY AUTO_INCREMENT,
-    album VARCHAR(50) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
     artista_id INT,
     FOREIGN KEY (artista_id) REFERENCES artistas (id)
 ) engine = InnoDB;
@@ -54,3 +54,28 @@ id INT PRIMARY KEY AUTO_INCREMENT,
     FOREIGN KEY (usuario_id) REFERENCES usuarios (id),
     FOREIGN KEY (cancao_id) REFERENCES cancoes (id)
 ) engine = InnoDB;
+
+INSERT INTO planos (tipo, valor)
+VALUES ('gratuito', 0), ('familiar', 7.99), ('universitario', 5.99);
+
+INSERT INTO usuarios (nome, idade, plano_id)
+VALUES ('Thati', 23, 1), ('Cintia', 35, 2), ('Bill', 20, 3), ('Roger', 45, 1);
+
+INSERT INTO artistas (nome)
+VALUES ('Walter Phoenix'), ('Peter Strong'), ('Lance Day'), ('Freedie Shannon');
+
+INSERT INTO albuns (nome, artista_id)
+VALUES ('Envious', 1), ('Exuberant', 1), ('Hallowed Steam', 2), ('Incandescent', 3), ('Temporary Culture', 4);
+
+INSERT INTO usuario_segue_artista (usuario_id, artista_id)
+VALUES (1, 1), (1, 4), (1, 3), (2, 1), (2, 3), (3, 2), (3, 1), (4, 4);
+
+INSERT INTO cancoes (nome, artista_id, album_id) VALUES
+('Soul For Us', 1, 1), ('Reflections Of Magic', 1, 1), ('Dance With Her Own', 1, 1),
+('Troubles Of My Inner Fire', 1, 2), ('Time Fireworks', 1, 2),
+('Magic Circus', 2, 3), ('Honey, So Do I', 2, 3), ("Sweetie, Let's Go Wild", 2, 3), ('She Knows', 2, 3),
+('Fantasy For Me', 3, 4), ('Celebration Of More', 3, 4), ('Rock His Everything', 3, 4), ('Home Forever', 3, 4), ('Diamond Power', 3, 4), ("Honey, Let's Be Silly", 3, 4),
+('Thang Of Thunder', 4, 5), ('Words Of Her Life', 4, 5), ('Without My Streets', 4, 5);
+
+INSERT INTO usuario_ouve_cancao (usuario_id, cancao_id) VALUES
+(1, 1), (1, 6), (1, 14), (1, 16), (2, 13), (2, 17), (2, 3), (2, 15), (3, 4), (3, 16), (3, 6), (4, 3), (4, 18), (4, 11);
