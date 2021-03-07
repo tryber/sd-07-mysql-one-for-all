9d58fe18c053e4e847a2faf7b4e6a30977f9a857
CarolSi <a.carolg1@gmail.com>
2021-03-07 15:43:41 -0300

    requisito 9

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 CREATE VIEW SpotifyClone.top_2_hits_do_momento AS
 SELECT can.cancao AS `cancao`,
 COUNT(can.cancao_id) AS `reproducoes`
-FROM SpotifyClone.cancoes as can
-INNER JOIN SpotifyClone.historico_de_reproducoes as his
+FROM SpotifyClone.cancoes AS can
+INNER JOIN SpotifyClone.historico_de_reproducoes AS his
 ON his.cancao_id = can.cancao_id
 GROUP BY can.cancao_id
 ORDER BY reproducoes DESC, cancao
--- desafio7.sql
+++ desafio7.sql
@@ -2,8 +2,8 @@ CREATE VIEW SpotifyClone.perfil_artistas AS
 SELECT art.artista AS `artista`,
 alb.album AS `album`,
 COUNT(sArt.artista) AS `seguidores`
-FROM SpotifyClone.artistas art
-INNER JOIN SpotifyClone.seguindo_artista sArt 
+FROM SpotifyClone.artistas AS art
+INNER JOIN SpotifyClone.seguindo_artista AS sArt 
 ON sArt.artista = art.artista_id
 INNER JOIN SpotifyClone.albuns alb 
 ON alb.artista_id = art.artista_id
