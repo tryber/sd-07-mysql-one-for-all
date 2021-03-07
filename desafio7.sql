67cfb7c83b9dbd5e090ff472adf4c739b6d92df4
marcelo ivan <sagacello@hotmail.com>
2021-03-07 11:53:14 -0300

    desafio 7 corrigido + desafio 9

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 CREATE VIEW perfil_artistas AS 
-SELECT a.artista, ab.album , count(s.usuario_id) seguidoresAS FROM SpotifyClone.artistas AS a
+SELECT a.artista, ab.album , count(s.usuario_id) seguidores FROM SpotifyClone.artistas AS a
 INNER JOIN SpotifyClone.albuns AS ab ON ab.artista_id = a.artista_id
 INNER JOIN SpotifyClone.seguidores AS s ON ab.artista_id = s.artista_id
 GROUP BY artista, album
