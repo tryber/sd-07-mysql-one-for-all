cb97cc3578f201b34c4b374078d92d2005df9b0b
marcelo ivan <sagacello@hotmail.com>
2021-03-07 12:12:58 -0300

    todos quase completos

--- desafio9.sql
+++ desafio9.sql
@@ -2,8 +2,8 @@ USE SpotifyClone;
 DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(IN entrada_artista VARCHAR(100))
 BEGIN
-    SELECT a.artista AS a, ab.album AS album
-    FROM artistas AS a
-    INNER JOIN albuns AS ab ON a.artista = entrada_artista AND a.artista_id = ab.artista_id;
+SELECT a.artista AS artista, ab.album AS album
+FROM artistas AS a
+INNER JOIN albuns AS ab ON a.artista = entrada_artista AND a.artista_id = ab.artista_id;
 END $$
 DELIMITER ;
