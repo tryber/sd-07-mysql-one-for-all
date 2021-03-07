9b4552b40812a1f943453b05f4ccd494018645ca
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-03-07 11:52:01 -0300

    desafio 2 correçao

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,5 @@
-USE SpotifyClone;
-CREATE VIEW `estatisticas_musicais` AS
+CREATE VIEW SpotifyClone.estatisticas_musicais AS
 SELECT
-(SELECT COUNT(`nome_musica`) FROM musicas) AS cancoes,
-(SELECT COUNT(`nome_artista`) FROM artistas) AS artistas,
-(SELECT COUNT(`nome_album`) FROM albuns) AS albuns;
+(SELECT COUNT(`nome_musica`) FROM SpotifyClone.musicas) AS cancoes,
+(SELECT COUNT(`nome_artista`) FROM SpotifyClone.artistas) AS artistas,
+(SELECT COUNT(`nome_album`) FROM SpotifyClone.albuns) AS albuns;
