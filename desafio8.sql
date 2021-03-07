0088427fe9385f627b0ce6f126f11ef60ad34671
danielmlduarte <danielduarte.tryber@gmail.com>
2021-03-07 14:40:35 -0300

    Desafio8 - Refatorado

--- desafio8.sql
+++ desafio8.sql
@@ -2,10 +2,10 @@ USE spotifyclone;
 
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-    BEFORE DELETE ON usuario
-    FOR EACH ROW
+BEFORE DELETE ON usuario
+FOR EACH ROW
 BEGIN
-    DELETE FROM seguindo_artista AS sa WHERE sa.usuario_id = OLD.usuario_id;
-    DELETE FROM historico_reproducao AS hr WHERE hr.usuario_id = OLD.usuario_id;    
+DELETE FROM seguindo_artista WHERE usuario_id = OLD.usuario_id;
+DELETE FROM historico_reproducao WHERE usuario_id = OLD.usuario_id;    
 END $$
 DELIMITER ;
