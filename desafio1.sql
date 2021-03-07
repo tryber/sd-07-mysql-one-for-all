9dd22c60466aa4cb08a044dbe044f277c0763d46
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-03-06 23:08:39 -0300

    a esperança e a ultima que morre 4

--- desafio1.sql
+++ desafio1.sql
@@ -1,84 +1,71 @@
 DROP DATABASE IF EXISTS SpotifyClone;
-
 CREATE DATABASE SpotifyClone;
-
+CREATE SCHEMA IF NOT EXISTS SpotifyClone;
 USE SpotifyClone;
-
 CREATE TABLE IF NOT EXISTS plano (
   plano_id INT PRIMARY KEY AUTO_INCREMENT,
   nome_plano VARCHAR(45) NOT NULL,
   valor FLOAT NOT NULL
-);
-
+) ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS usuario (
   usuario_id INT PRIMARY KEY AUTO_INCREMENT,
   nome_usuario VARCHAR(100) NOT NULL,
   idade INT NOT NULL,
   plano_id INT NOT NULL,
-  FOREIGN KEY (plano_id) REFERENCES plano (plano_id)
-);
-
+  FOREIGN KEY (plano_id) REFERENCES plano (plano_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS artista (
   artista_id INT PRIMARY KEY AUTO_INCREMENT,
   nome_artista VARCHAR(45) NOT NULL
-);
-
+) ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS album (
   album_id INT PRIMARY KEY AUTO_INCREMENT,
   nome_album VARCHAR(45) NOT NULL,
   artista_id INT NOT NULL,
-  FOREIGN KEY (artista_id) REFERENCES artista (artista_id)
-);
-
+  FOREIGN KEY (artista_id) REFERENCES artista (artista_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS musica (
   musica_id INT PRIMARY KEY AUTO_INCREMENT,
   nome_musica VARCHAR(45) NOT NULL,
   album_id INT NOT NULL,
   artista_id INT NOT NULL,
-  FOREIGN KEY (album_id) REFERENCES album (album_id),
-  FOREIGN KEY (artista_id) REFERENCES artista (artista_id)
-);
-
+  FOREIGN KEY (album_id) REFERENCES album (album_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  FOREIGN KEY (artista_id) REFERENCES artista (artista_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS historico_reproducao (
   usuario_id INT NOT NULL,
   musica_id INT NOT NULL,
   PRIMARY KEY (usuario_id, musica_id),
-  FOREIGN KEY (usuario_id) REFERENCES usuario (usuario_id),
-  FOREIGN KEY (musica_id) REFERENCES musica (musica_id)
-);
-
+  FOREIGN KEY (usuario_id) REFERENCES usuario (usuario_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  FOREIGN KEY (musica_id) REFERENCES musica (musica_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS seguidores (
   usuario_id INT NOT NULL,
   artista_id INT NOT NULL,
   PRIMARY KEY (usuario_id, artista_id),
-  FOREIGN KEY (usuario_id) REFERENCES usuario (usuario_id),
-  FOREIGN KEY (artista_id) REFERENCES artista (artista_id)
-);
-
+  FOREIGN KEY (usuario_id) REFERENCES usuario (usuario_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  FOREIGN KEY (artista_id) REFERENCES artista (artista_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = InnoDB;
 INSERT INTO usuario (nome_usuario, idade, plano_id)
 VALUES ('Thati', 23, 1),
   ('Cintia', 35, 2),
   ('Bill', 20, 3),
   ('Roger', 45, 1);
-
 INSERT INTO plano (nome_plano, valor)
 VALUES ('gratuito', 0),
   ('familiar', 7.99),
   ('universitário', 5.99);
-
 INSERT INTO artista (nome_artista)
 VALUES ('Walter Phoenix'),
   ('Peter Strong'),
   ('Lance Day'),
   ('Freedie Shannon');
-
 INSERT INTO album (nome_album, artista_id)
 VALUES ('Envious', 1),
   ('Exuberant', 1),
   ('Hallowed Steam', 2),
   ('Incandescent', 3),
   ('Temporary Culture', 4);
-
 INSERT INTO musica (nome_musica, album_id, artista_id)
 VALUES ('Soul For Us', 1, 1),
   ('Reflections Of Magic', 1, 1),
@@ -98,7 +85,6 @@ VALUES ('Soul For Us', 1, 1),
   ('Thang Of Thunder', 5, 4),
   ('Words Of Her Life', 5, 4),
   ('Without My Streets', 5, 4);
-
 INSERT INTO historico_reproducao (usuario_id, musica_id)
 VALUES (1, 1),
   (1, 6),
@@ -114,7 +100,6 @@ VALUES (1, 1),
   (4, 3),
   (4, 18),
   (4, 11);
-
 INSERT INTO seguidores (usuario_id, artista_id)
 VALUES (1, 1),
   (1, 4),
