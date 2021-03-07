b459420e5f4a30ffcf38615e5546a0e2b9fcff23
CarolSi <a.carolg1@gmail.com>
2021-03-07 15:29:42 -0300

    requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.usuarios 
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.seguindo_artista 
+WHERE usuario = OLD.usuario_id;
+DELETE FROM SpotifyClone.historico_de_reproducoes
+WHERE usuario_id = OLD.usuario_id;
+END; $$
+
+DELIMITER ;
