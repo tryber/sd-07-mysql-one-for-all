dc5b400dc25f15b041e4a775a6e36983c57e8000
Jr3564 <rodolfojr94@gmail.com>
2021-03-07 19:58:59 -0300

    Requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -5,9 +5,9 @@ CREATE TRIGGER trigger_usuario_delete
     AFTER DELETE ON Users
     FOR EACH ROW
 BEGIN
-	DELETE FROM Followings
+    DELETE FROM Followings
     WHERE Followings.use_id = OLD.use_id;
-	DELETE FROM Historic
+	  DELETE FROM Historic
     WHERE Historic.use_id = OLD.use_id;
 END $$
 DELIMITER ;
