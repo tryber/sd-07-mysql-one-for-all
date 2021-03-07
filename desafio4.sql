2019f59ec452bca5cf1947c3c6e9df8554ffa5d2
Flavio Oliveira <flavsoliver@gmail.com>
2021-03-07 13:08:19 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+SELECT a.artista_nome AS `artista`,
+    COUNT(*) AS `seguidores`
+FROM SpotifyClone.seguindo AS s
+    JOIN SpotifyClone.usuario AS u ON u.usuario_id = s.usuario_id
+    JOIN SpotifyClone.artista AS a ON a.artista_id = s.artista_id
+GROUP BY a.artista_id
+ORDER BY `seguidores` DESC,
+    `artista`
+LIMIT 3;
