86be915abc0e0b6e21aa3560c208ac65c11bb2b2
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-03-07 21:11:26 -0300

    Exercise 9 - done

---
+++ desafio9.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(IN nome_do_artista VARCHAR(25))
+BEGIN
+SELECT
+art.nome_artista AS artista,
+alb.nome_album AS album
+FROM SpotifyClone.artista AS art
+INNER JOIN SpotifyClone.album AS alb
+ON art.artista_id = alb.artista_id
+WHERE artista = nome_do_artista
+ORDER BY album;
+END $$
+DELIMITER ;
