d589aa1417d9fde96466398faf640a4805fa9a46
Carol Bezerra <carol.arbe@gmail.com>
2021-03-07 22:12:45 -0300

    Desafio 1 com ajustes tentativa 2

--- desafio1.sql
+++ desafio1.sql
@@ -1,69 +1,60 @@
 DROP DATABASE IF EXISTS SpotifyClone;
 
-CREATE DATABASE SpotifyClone;
+CREATE DATABASE IF NOT EXISTS SpotifyClone;
 
 USE SpotifyClone;
 
 CREATE TABLE planos(
-id INT AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
     tipo VARCHAR(50) NOT NULL,
-    valor DECIMAL(5,2) NOT NULL,
-    PRIMARY KEY (id)
+    valor DECIMAL(5,2) NOT NULL
 ) engine = InnoDB;
 
 CREATE TABLE usuarios(
-id INT AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     idade INT NOT NULL,
     plano_id INT,
-    PRIMARY KEY (id),
     FOREIGN KEY (plano_id) REFERENCES planos (id)
 ) engine = InnoDB;
 
 CREATE TABLE artistas(
-id INT AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
+id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL
 ) engine = InnoDB;
 
 CREATE TABLE albuns(
-id INT AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
-    artista_id INT,
-    PRIMARY KEY (id),
+    artista_id INT NOT NULL,
     FOREIGN KEY (artista_id) REFERENCES artistas (id)
 ) engine = InnoDB;
 
 CREATE TABLE usuario_segue_artista(
-id INT AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
     usuario_id INT NOT NULL,
     artista_id INT,
-    PRIMARY KEY (id),
     FOREIGN KEY (usuario_id) REFERENCES usuarios (id),
     FOREIGN KEY (artista_id) REFERENCES artistas (id)
 ) engine = InnoDB;
 
 CREATE TABLE cancoes(
-id INT AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
-    artista_id INT,
-    album_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (artista_id) REFERENCES artistas (id),
+    album_id INT NOT NULL,
     FOREIGN KEY (album_id) REFERENCES albuns (id)
 ) engine = InnoDB;
 
 CREATE TABLE usuario_ouve_cancao(
-id INT AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
     usuario_id INT NOT NULL,
-    cancao_id INT,
-    PRIMARY KEY (id),
+    cancao_id INT NOT NULL,
     FOREIGN KEY (usuario_id) REFERENCES usuarios (id),
     FOREIGN KEY (cancao_id) REFERENCES cancoes (id)
 ) engine = InnoDB;
 
 INSERT INTO planos (tipo, valor)
-VALUES ('gratuito', 0), ('familiar', 7.99), ('universitario', 5.99);
+VALUES ('gratuito', 0), ('familiar', 7.99), ('universitário', 5.99);
 
 INSERT INTO usuarios (nome, idade, plano_id)
 VALUES ('Thati', 23, 1), ('Cintia', 35, 2), ('Bill', 20, 3), ('Roger', 45, 1);
@@ -72,17 +63,21 @@ INSERT INTO artistas (nome)
 VALUES ('Walter Phoenix'), ('Peter Strong'), ('Lance Day'), ('Freedie Shannon');
 
 INSERT INTO albuns (nome, artista_id)
-VALUES ('Envious', 1), ('Exuberant', 1), ('Hallowed Steam', 2), ('Incandescent', 3), ('Temporary Culture', 4);
+VALUES ('Envious', 1), ('Exuberant', 1), ('Hallowed Steam', 4), ('Incandescent', 3), ('Temporary Culture', 2);
 
 INSERT INTO usuario_segue_artista (usuario_id, artista_id)
-VALUES (1, 1), (1, 4), (1, 3), (2, 1), (2, 3), (3, 2), (3, 1), (4, 4);
+VALUES (1, 1), (1, 2), (1, 3), (2, 1), (2, 3), (3, 4), (3, 1), (4, 2);
 
-INSERT INTO cancoes (nome, artista_id, album_id) VALUES
-('Soul For Us', 1, 1), ('Reflections Of Magic', 1, 1), ('Dance With Her Own', 1, 1),
-('Troubles Of My Inner Fire', 1, 2), ('Time Fireworks', 1, 2),
-('Magic Circus', 2, 3), ('Honey, So Do I', 2, 3), ("Sweetie, Let's Go Wild", 2, 3), ('She Knows', 2, 3),
-('Fantasy For Me', 3, 4), ('Celebration Of More', 3, 4), ('Rock His Everything', 3, 4), ('Home Forever', 3, 4), ('Diamond Power', 3, 4), ("Honey, Let's Be Silly", 3, 4),
-('Thang Of Thunder', 4, 5), ('Words Of Her Life', 4, 5), ('Without My Streets', 4, 5);
+INSERT INTO cancoes (nome, artista_id) VALUES
+('Soul For Us', 1), ('Reflections Of Magic', 1), ('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2), ('Time Fireworks', 2),
+('Magic Circus', 3), ('Honey, So Do I', 3), ("Sweetie, Let's Go Wild", 3), ('She Knows', 3),
+('Fantasy For Me', 4), ('Celebration Of More', 4), ('Rock His Everything', 4), ('Home Forever', 4), ('Diamond Power', 4), ("Honey, Let's Be Silly", 4),
+('Thang Of Thunder', 5), ('Words Of Her Life', 5), ('Without My Streets', 5);
 
 INSERT INTO usuario_ouve_cancao (usuario_id, cancao_id) VALUES
-(1, 1), (1, 6), (1, 14), (1, 16), (2, 13), (2, 17), (2, 2), (2, 15), (3, 4), (3, 16), (3, 6), (4, 3), (4, 18), (4, 11);
+(1, 1), (1, 6), (1
+, 14), (1, 16), (2, 13), (2, 17), (2, 2), (2, 15), (3, 4), (3, 16), (3, 6), (4, 3), (4, 18), (4, 11);
+
+-- Ajustes com referência do Fernando Antunes;
+-- Ajustes no Json com referência da Carol Andrade;
--- desafio11.sql
+++ desafio11.sql
@@ -3,5 +3,5 @@ SELECT c.nome AS nome, COUNT(uoc.cancao_id) AS reproducoes FROM SpotifyClone.can
 INNER JOIN SpotifyClone.usuario_ouve_cancao AS uoc ON c.id = uoc.cancao_id
 LEFT JOIN SpotifyClone.usuarios AS u ON uoc.usuario_id = u.id
 LEFT JOIN SpotifyClone.planos AS p ON u.plano_id = p.id
-WHERE p.tipo IN ('familiar', 'universitario')
+WHERE p.tipo IN ('familiar', 'universitário')
 GROUP BY nome ORDER BY nome;
