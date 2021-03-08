3f52997dd0d19ab292045c8592ccc45ce6962741
Camila Valentim <camilaifpb@hotmail.com>
2021-03-08 18:11:13 -0300

    resolvendo o desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -33,6 +33,7 @@ CREATE TABLE seguindo_artistas(
 seguindo_artistas_id INT PRIMARY KEY AUTO_INCREMENT,
 usuario_id INT NOT NULL,
 artista_id INT,
+PRIMARY KEY (usuario_id, artista_id),
 FOREIGN KEY (usuario_id) REFERENCES SpotifyClone.usuarios(usuario_id),
 FOREIGN KEY (artista_id) REFERENCES SpotifyClone.artistas(artista_id)
 )engine = InnoDB;
