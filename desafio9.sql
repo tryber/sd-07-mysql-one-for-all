2c89a8e047aa4ec7864c502703c7ee2d5fdb4f89
Melissa Gomes <mel.ge171201@gmail.com>
2021-03-08 17:12:23 -0300

    req 9 de novo

--- desafio9.sql
+++ desafio9.sql
@@ -4,13 +4,13 @@ DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(IN artistaName VARCHAR(100))
 BEGIN
 SELECT 
-a.artista_name AS artista,
-al.album_name AS album
+a.artista_name AS `artista`,
+al.album_name AS `album`
 FROM SpotifyClone.Artistas AS `a`
 INNER JOIN SpotifyClone.Albunss AS `al`
 ON a.artista_id = al.artista_id
 HAVING a.artista_name = artistaName
-ORDER BY album;
+ORDER BY `album`;
 END $$
 
 DELIMITER ;
