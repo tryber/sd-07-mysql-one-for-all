23dc929d1cc143bff50972533ad4fee6abb109c2
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-03-07 21:19:52 -0300

    fix: desafio 2.

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,4 @@
-USE SpotifyClone;
-CREATE VIEW count_tracks_artists_albums AS
+CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(nome) AS cancoes,
 (SELECT COUNT(nome) FROM artistas) AS artistas,
 (SELECT COUNT(nome) FROM albums) AS albuns
