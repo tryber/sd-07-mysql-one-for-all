bc64b8ab35dd3d5616096c995cef96c616cca35a
Melissa Gomes <mel.ge171201@gmail.com>
2021-03-08 17:41:19 -0300

    req9

--- desafio9.sql
+++ desafio9.sql
@@ -6,10 +6,10 @@ BEGIN
 SELECT 
 a.artista_name AS `artista`,
 al.album_name AS `album`
-FROM SpotifyClone.Artistas AS `a`
-INNER JOIN SpotifyClone.Albunss AS `al`
+FROM SpotifyClone.Artista AS `a`
+INNER JOIN SpotifyClone.Albuns AS `al`
 ON a.artista_id = al.artista_id
-HAVING a.artista_name = artistaName
+HAVING `artista` = artistaName
 ORDER BY `album`;
 END $$
 
