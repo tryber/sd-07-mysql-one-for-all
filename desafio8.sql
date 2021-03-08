af9f90b646f7226a172c9b81c68de32fb0d73734
Anderson Santana <anderslash@gmail.com>
2021-03-08 16:07:47 -0300

    Desafio 08 concluído

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER `trigger_usuario_delete`
 BEFORE DELETE ON SpotifyClone.usuarios
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.historico AS H WHERE H.usuario_id = OLD.usuario_id;
-DELETE FROM SpotifyClone.seguidores AS S WHERE S.usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.historico WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.seguidores WHERE usuario_id = OLD.usuario_id;
 END $$
 delimiter ;
