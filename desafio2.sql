5cf885e3dc4da47d0753d68472a9ffbd471554f1
Anderson Santana <anderslash@gmail.com>
2021-03-08 15:04:06 -0300

    Desafio 02 concluído

---
+++ desafio2.sql
@@ -0,0 +1,4 @@
+CREATE VIEW estatisticas_musicais AS SELECT
+(SELECT COUNT(CAN.cancao_id) FROM SpotifyClone.cancoes AS CAN) AS cancoes,
+(SELECT COUNT(ART.artista_id) FROM SpotifyClone.artistas AS ART) AS artistas,
+(SELECT COUNT(ALB.album_id) FROM  SpotifyClone.albuns AS ALB) AS albuns;
