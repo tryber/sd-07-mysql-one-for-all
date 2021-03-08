b671ff12d5751a7d2ce8e472d8f42314c0bf420c
Carol Bezerra <carol.arbe@gmail.com>
2021-03-07 22:26:00 -0300

    voltando ao meu Desafio 1 original tentativa 2

--- desafio11.sql
+++ desafio11.sql
@@ -3,5 +3,5 @@ SELECT c.nome AS nome, COUNT(uoc.cancao_id) AS reproducoes FROM SpotifyClone.can
 INNER JOIN SpotifyClone.usuario_ouve_cancao AS uoc ON c.id = uoc.cancao_id
 LEFT JOIN SpotifyClone.usuarios AS u ON uoc.usuario_id = u.id
 LEFT JOIN SpotifyClone.planos AS p ON u.plano_id = p.id
-WHERE p.tipo IN ('familiar', 'universitário')
+WHERE p.tipo IN ('familiar', 'universitario')
 GROUP BY nome ORDER BY nome;
