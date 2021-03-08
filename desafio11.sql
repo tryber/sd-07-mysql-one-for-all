43fe3a2fff39ee40c6b9c5f42b0c2817a91a660b
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-03-07 22:11:42 -0300

    Exercise 11 - done-2

--- desafio11.sql
+++ desafio11.sql
@@ -9,6 +9,6 @@ INNER JOIN SpotifyClone.usuario AS `use`
 ON his.usuario_id = `use`.usuario_id
 INNER JOIN SpotifyClone.plano AS pla
 ON `use`.plano_id = pla.plano_id
-WHERE pla.nome_plano = "familiar" OR pla.nome_plano = "universitario"
+WHERE pla.nome_plano IN ("familiar", "universitario")
 GROUP BY mus.nome_musica
 ORDER BY nome;
