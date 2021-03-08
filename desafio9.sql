f74a5c0db71bc6a8fd0fd7f2577772c4bed7bbb1
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-03-08 10:26:17 -0300

    deasafio 9.

---
+++ desafio9.sql
@@ -0,0 +1,11 @@
+USE SpotifyClone;
+DELIMITER //;
+CREATE PROCEDURE albuns_do_artista(IN nome_do_artista VARCHAR(45))
+BEGIN
+SELECT a.nome AS artista, al.nome AS album
+FROM artistas AS a
+INNER JOIN albums AS al
+ON a.id = al.artista_id
+HAVING artista = nome_do_artista;
+END //;
+DELIMITER ;
