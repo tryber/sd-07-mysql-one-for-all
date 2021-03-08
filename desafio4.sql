c6d08db45082a446f96cd3a319fb54943577d6bc
Anderson Santana <anderslash@gmail.com>
2021-03-08 15:13:34 -0300

    Desafio 04 concluído

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+CREATE VIEW top_3_artistas AS SELECT
+A.artista AS `artista`,
+COUNT(S.artista_id) AS `seguidores`
+FROM SpotifyClone.artistas as A
+INNER JOIN SpotifyClone.seguidores As S ON S.artista_id = A.artista_id
+GROUP BY `artista`
+ORDER BY `seguidores` DESC, `artista` ASC
+LIMIT 3;
