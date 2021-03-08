06df340d62731c3b66b1dd9c7c2dc9f8426c05c0
Melissa Gomes <mel.ge171201@gmail.com>
2021-03-08 17:00:42 -0300

    req 9

---
+++ desafio9.sql
@@ -0,0 +1,16 @@
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(IN artistaName VARCHAR(100))
+BEGIN
+SELECT 
+a.artista_name AS artista,
+al.album_name AS album
+FROM SpotifyClone.artistas AS `a`
+INNER JOIN SpotifyClone.albuns AS `al`
+ON a.artista_id = al.artista_id
+HAVING artista_name = artistaName
+ORDER BY album;
+END $$
+
+DELIMITER ;
