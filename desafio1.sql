c4cb39b89c4e1445399ca0798db4009c3a7c7685
Strongreen <strongreen@live.com>
2021-03-09 00:10:14 -0300

    reformulaçao do banco

--- desafio1.sql
+++ desafio1.sql
@@ -3,20 +3,20 @@ CREATE DATABASE IF NOT EXISTS SpotifyClone;
 
 USE SpotifyClone;
 
-DROP TABLE IF EXISTS Plano;
+DROP TABLE IF EXISTS `Plano`;
 CREATE TABLE IF NOT EXISTS Plano (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     tipo VARCHAR(40) NOT NULL,
     valor DECIMAL(5 , 2 ) NOT NULL
 )  ENGINE=INNODB;
 
-DROP TABLE IF EXISTS Artista;
+DROP TABLE IF EXISTS `Artista`;
 CREATE TABLE IF NOT EXISTS Artista (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     nome VARCHAR(50) NOT NULL
 )  ENGINE=INNODB;
 
-DROP TABLE IF EXISTS Album;
+DROP TABLE IF EXISTS `Album`;
 CREATE TABLE IF NOT EXISTS Album (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     nome VARCHAR(50) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS Album (
         REFERENCES SpotifyClone.Artista (id)
 )  ENGINE=INNODB;
 
-DROP TABLE IF EXISTS Usuario;
+DROP TABLE IF EXISTS `Usuario`;
 CREATE TABLE IF NOT EXISTS Usuario (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     nome VARCHAR(50) NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS Usuario (
         REFERENCES SpotifyClone.Plano (id)
 )  ENGINE=INNODB;
 
-DROP TABLE IF EXISTS Seguindo;
+DROP TABLE IF EXISTS `Seguindo`;
 CREATE TABLE IF NOT EXISTS Seguindo (
     usuario_id INT,
     artista_id INT,
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS Seguindo (
         REFERENCES SpotifyClone.Artista (id)
 )  ENGINE=INNODB;
 
-DROP TABLE IF EXISTS Cancao;
+DROP TABLE IF EXISTS `Cancao`;
 CREATE TABLE IF NOT EXISTS Cancao (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     nome VARCHAR(50) NOT NULL,
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS Cancao (
         REFERENCES SpotifyClone.Album (id)
 )  ENGINE=INNODB;
 
-DROP TABLE IF EXISTS Historico;
+DROP TABLE IF EXISTS `Historico`;
 CREATE TABLE IF NOT EXISTS Historico (
     usuario_id INT,
     cancao_id INT,
@@ -67,22 +67,22 @@ CREATE TABLE IF NOT EXISTS Historico (
 )  ENGINE=INNODB;
 
 INSERT INTO Plano(tipo, valor)
-VALUES("gratuito", 0), ("familiar", 7.99), ("universitário", 5.99);
+VALUES('gratuito', 0), ('familiar', 7.99), ('universitário', 5.99);
 
 INSERT INTO Artista(nome)
-VALUES("Walter Phoenix"), ("Peter Strong"), ("Lancy Day"), ("Freedie Shannon");
+VALUES('Walter Phoenix'), ('Peter Strong'), ('Lancy Day'), ('Freedie Shannon');
 
 INSERT INTO Album(nome, artista_id)
-VALUES("Envious", 1), ("Exuberant", 2), ("Hallowed Steam", 3), ("Incandescent", 4), ("Temporary Culture", 5);
+VALUES('Envious', 1), ('Exuberant', 2), ('Hallowed Steam', 3), ('Incandescent', 4), ('Temporary Culture', 5);
     
 INSERT INTO Usuario(nome, idade, plano_id )
-VALUES("Thati", 23, 1 ), ("Cintia", 35, 2), ("Bill", 20, 3), ("Roger", 45, 1);
+VALUES('Thati', 23, 1 ), ('Cintia', 35, 2), ('Bill', 20, 3), ('Roger', 45, 1);
 
 INSERT INTO Seguindo(usuario_id, artista_id )
 VALUES(1, 1), (1, 3), (1, 4), (2, 1), (2, 3), (3, 1), (3, 2), (4, 4);
     
 INSERT INTO Cancao(nome, album_id)
-VALUES("Soul For Us", 1 ), ("Reflections Of Magic", 1), ("Dance With Her Own", 1), ("Troubles Of My Inner Fire", 2), ("Time Fireworks", 2), ("Magic Circus", 3), ("Honey, So Do I", 3), ("Sweetie, Let's Go Wild", 3), ("She Knows", 3), ("Fantasy For Me", 4), ("Celebration Of More", 4), ("Rock His Everything", 4), ("Home Forever", 4), ("Diamond Power", 4), ("Honey, Let's Be Silly", 4), ("Thang Of Thunder", 5), ("Words Of Her Life", 5), ("Without My Streets", 5);
+VALUES('Soul For Us', 1 ), ('Reflections Of Magic', 1), ('Dance With Her Own', 1), ('Troubles Of My Inner Fire', 2), ('Time Fireworks', 2), ('Magic Circus', 3), ('Honey, So Do I', 3), ('Sweetie, Let's Go Wild', 3), ('She Knows', 3), ('Fantasy For Me', 4), ('Celebration Of More', 4), ('Rock His Everything', 4), ('Home Forever', 4), ('Diamond Power', 4), ('Honey, Let's Be Silly', 4), ('Thang Of Thunder', 5), ('Words Of Her Life', 5), ('Without My Streets', 5);
         
 INSERT INTO Historico(usuario_id, cancao_id)
 VALUES(1, 1), (1, 6), (1, 14), (1, 16), (2, 13), (2, 17), (2, 2), (2, 15), (3, 4), (3, 16), (3, 6), (4, 3), (4, 18), (4, 11);
