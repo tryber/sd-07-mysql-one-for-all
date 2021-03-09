26be24171aa6d66b4a662f85ef961799fac795a0
CleberFontinele <cleberfontinele2387@gmail.com>
2021-03-09 08:50:26 -0300

    mudando para MAIUSCULA

--- desafio2.sql
+++ desafio2.sql
@@ -3,7 +3,7 @@ SELECT
 COUNT(DISTINCT C.cancao) AS 'cancoes',
 COUNT(DISTINCT A.artista) AS 'artistas',
 COUNT(DISTINCT AL.album) AS 'albuns'  
-from SpotifyClone.albuns as AL
+FROM SpotifyClone.albuns as AL
 INNER JOIN SpotifyClone.cancoes AS C
 ON C.album_id = AL.album_id
 INNER JOIN SpotifyClone.artistas AS A
