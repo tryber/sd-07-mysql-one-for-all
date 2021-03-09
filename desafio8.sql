46ee966551ef475bc460a0049a9a4dcd55cb741e
Strongreen <strongreen@live.com>
2021-03-09 06:30:43 -0300

    Exercicio 8

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,9 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON Usuario
 FOR EACH ROW
 BEGIN
-   DELETE FROM SpotifyClone.Historico  WHERE usuario_id = old.id;
+DELETE FROM SpotifyClone.Historico 
+WHERE 
+	usuario_id = old.id;
 DELETE FROM SpotifyClone.Seguindo 
 WHERE
     usuario_id = old.id;
