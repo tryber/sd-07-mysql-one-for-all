dcb9b283e2922d2bd95eeb92f1e3984fc540e3c2
Strongreen <strongreen@live.com>
2021-03-09 03:17:32 -0300

    Exercicicio 3

---
+++ desafio3.sql
@@ -0,0 +1,18 @@
+USE SpotifyClone;
+DROP VIEW IF EXISTS historico_reproducao_usuarios;
+CREATE VIEW historico_reproducao_usuarios AS
+    SELECT 
+        users.nome AS usuario, cancao.nome AS nome
+    FROM
+        SpotifyClone.Historico AS historico
+            INNER JOIN
+        SpotifyClone.Cancao AS cancao ON historico.cancao_id = cancao.id
+            INNER JOIN
+        SpotifyClone.Usuario AS users ON historico.usuario_id = users.id
+    ORDER BY usuario , nome;
+                
+                
+SELECT 
+    *
+FROM
+    historico_reproducao_usuarios;
