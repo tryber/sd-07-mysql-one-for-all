9bcf3bc6e7a241da0681e204f755866e6c335b97
Strongreen <strongreen@live.com>
2021-03-09 04:09:38 -0300

    Exercicicio 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,12 @@
 CREATE VIEW top_3_artistas AS
     SELECT 
-        artista.nome AS artista, COUNT(usuario.id) AS seguidores
+        art.nome AS artista, COUNT(usuario.id) AS seguidores
     FROM
         SpotifyClone.Seguindo AS seguindo
             INNER JOIN
         SpotifyClone.Usuario AS usuario ON seguindo.usuario_id = usuario.id
             INNER JOIN
-        SpotifyClone.Artista AS artista ON seguindo.artista_id = artista.id
+        SpotifyClone.Artista AS art ON seguindo.artista_id = artista.id
     GROUP BY artista
     ORDER BY seguidores DESC , artista
     LIMIT 3;
