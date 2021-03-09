522eead4655a4de33bfc3cc84b2a60f5ecb56501
Strongreen <strongreen@live.com>
2021-03-09 08:17:01 -0300

    Exercicio 8

--- desafio8.sql
+++ desafio8.sql
@@ -5,9 +5,9 @@ FOR EACH ROW
 BEGIN
 DELETE FROM SpotifyClone.Historico 
 WHERE
-    SpotifyClone.Historico.usuario_id = OLD.id;
+    usuario_id = OLD.id;
 DELETE FROM SpotifyClone.Seguindo 
 WHERE
-    SpotifyClone.Seguindo.usuario_id = OLD.id;
+    usuario_id = OLD.id;
 END $$
 DELIMITER ;
