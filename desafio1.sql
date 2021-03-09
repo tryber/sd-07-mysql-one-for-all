35364dbed95512aef9e26cb0647476f1c9d850eb
Strongreen <strongreen@live.com>
2021-03-08 23:09:21 -0300

    reformulaçao da estrutura do banco

--- desafio1.sql
+++ desafio1.sql
@@ -17,7 +17,10 @@ CREATE TABLE Artista (
 
 CREATE TABLE Album (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    nome VARCHAR(50) NOT NULL
+    nome VARCHAR(50) NOT NULL,
+    artista_id INT NOT NULL,
+    FOREIGN KEY (artista_id)
+        REFERENCES Artista (id)
 );
 
 CREATE TABLE Usuario (
@@ -44,10 +47,7 @@ CREATE TABLE Cancao (
     nome VARCHAR(50) NOT NULL,
     album_id INT NOT NULL,
     FOREIGN KEY (album_id)
-        REFERENCES Album (id),
-    artista_id INT NOT NULL,
-    FOREIGN KEY (artista_id)
-        REFERENCES Artista (id)
+        REFERENCES Album (id)
 );
 
 CREATE TABLE Historico (
@@ -66,8 +66,8 @@ VALUES("gratuito", 0), ("familiar", 7.99), ("universitário", 5.99);
 INSERT INTO Artista(nome)
 VALUES("Walter Phoenix"), ("Peter Strong"), ("Lancy Day"), ("Freedie Shannon");
 
-INSERT INTO Album(nome)
-VALUES("Envious"), ("Exuberant"), ("Hallowed Steam"), ("Incandescent"), ("Temporary Culture");
+INSERT INTO Album(nome, artista_id)
+VALUES("Envious", 1), ("Exuberant", 2), ("Hallowed Steam", 3), ("Incandescent", 4), ("Temporary Culture", 5);
     
 INSERT INTO Usuario(nome, idade, plano_id )
 VALUES("Thati", 23, 1 ), ("Cintia", 35, 2), ("Bill", 20, 3), ("Roger", 45, 1);
@@ -75,9 +75,9 @@ VALUES("Thati", 23, 1 ), ("Cintia", 35, 2), ("Bill", 20, 3), ("Roger", 45, 1);
 INSERT INTO Seguindo(usuario_id, artista_id )
 VALUES(1, 1), (1, 3), (1, 4), (2, 1), (2, 3), (3, 1), (3, 2), (4, 4);
     
-INSERT INTO Cancao(nome, album_id, artista_id )
-VALUES("Soul For Us", 1, 1 ), ("Reflections Of Magic", 1, 1), ("Dance With Her Own", 1, 1), ("Troubles Of My Inner Fire", 2, 1), ("Time Fireworks", 2, 1), ("Magic Circus", 3, 2), ("Honey, So Do I", 3, 2), ("Sweetie, Let's Go Wild", 3, 2), ("She Knows", 3, 2), ("Fantasy For Me", 4, 3), ("Celebration Of More", 4, 3), ("Rock His Everything", 4, 3), ("Home Forever", 4, 3), ("Diamond Power", 4, 3), ("Honey, Let's Be Silly", 4, 3), ("Thang Of Thunder", 5, 4), ("Words Of Her Life", 5, 4), ("Without My Streets", 5, 4);
+INSERT INTO Cancao(nome, album_id)
+VALUES("Soul For Us", 1 ), ("Reflections Of Magic", 1), ("Dance With Her Own", 1), ("Troubles Of My Inner Fire", 2), ("Time Fireworks", 2), ("Magic Circus", 3), ("Honey, So Do I", 3), ("Sweetie, Let's Go Wild", 3), ("She Knows", 3), ("Fantasy For Me", 4), ("Celebration Of More", 4), ("Rock His Everything", 4), ("Home Forever", 4), ("Diamond Power", 4), ("Honey, Let's Be Silly", 4), ("Thang Of Thunder", 5), ("Words Of Her Life", 5), ("Without My Streets", 5);
     
     
-INSERT INTO Historico(usuario_id, cancao_id )
+INSERT INTO Historico(usuario_id, cancao_id)
 VALUES(1, 1), (1, 6), (1, 14), (1, 16), (2, 13), (2, 17), (2, 2), (2, 15), (3, 4), (3, 16), (3, 6), (4, 3), (4, 18), (4, 11);
