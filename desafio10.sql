25d0c0c05fe2f8a6f5a8d057cf16854fbc957800
Jr3564 <rodolfojr94@gmail.com>
2021-03-09 11:47:31 -0300

    Requisito 10, puro erro de interpretação ;

--- desafio10.sql
+++ desafio10.sql
@@ -1,12 +1,11 @@
 DELIMITER $$
 
-CREATE FUNCTION uantidade_musicas_no_historico(in_use_name VARCHAR(100))
+CREATE FUNCTION quantidade_musicas_no_historico(in_use_id INT)
 RETURNS INT READS SQL DATA
 BEGIN
   DECLARE songs_total INT;
-  SELECT COUNT(*) FROM SpotifyClone.Historic AS his
-  INNER JOIN SpotifyClone.Users AS usr ON usr.use_id = his.use_id
-  WHERE usr.use_name = in_use_name INTO songs_total;
+  SELECT COUNT(*) FROM SpotifyClone.Historic
+  WHERE use_id = in_use_id INTO songs_total;
   RETURN songs_total;
 END $$
 
