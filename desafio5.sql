36b996ea1a88519b8993524a757940651fb6852e
Murilo Pereira <muriloedu006@gmail.com>
2021-03-08 22:53:42 -0300

    desafio7

--- desafio5.sql
+++ desafio5.sql
@@ -3,7 +3,7 @@ SELECT songs.name_songs AS "cancao",
 COUNT(rep.user_id) AS "reproducoes"
 FROM SpotifyClone.songs AS songs
 INNER JOIN SpotifyClone.reproductions AS rep
-ON songs.songs_id = rep.user_id
+ON songs.songs_id = rep.songs_id
 GROUP BY `cancao`
 ORDER BY `reproducoes` DESC, `cancao`
 LIMIT 2;
--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 CREATE VIEW faturamento_atual AS
 SELECT ROUND(MIN(price), 2) AS "faturamento_minimo",
 ROUND(MAX(plans.price), 2) AS "faturamento_maximo",
-ROUND(AVG(plans.price), 2) AS "faturamento_maximo",
-ROUND(SUM(plans.price), 2) AS "faturamento_maximo"
+ROUND(AVG(plans.price), 2) AS "faturamento_medio",
+ROUND(SUM(plans.price), 2) AS "faturamento_total"
 FROM SpotifyClone.users AS users
 INNER JOIN SpotifyClone.plans AS plans
 ON users.plans_id = plans.plans_id;
