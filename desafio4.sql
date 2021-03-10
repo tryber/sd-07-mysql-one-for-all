23f4a92c95009a6ff5684c2d533ce7827b0a5f4d
Macarena Rodriguez <macarenar697@gmail.com>
2021-03-10 19:13:39 -0300

    req4

--- desafio4.sql
+++ desafio4.sql
@@ -1,8 +1,8 @@
 create view top_3_artistas as
-select a.artista as artista, count(u.usuario_id) as seguidores
-from SpotifyClone.seguindo as s
+select a.artista as artista, count(a.artista) as seguidores
+from SpotifyClone.usuarios as u
+inner join SpotifyClone.seguindo as s on u.usuario_id = s.usuario_id
 inner join SpotifyClone.artistas as a on a.artista_id = s.artista_id
-inner join SpotifyClone.usuarios as u on u.usuario_id = s.usuario_id
 group by artista
 order by seguidores desc, artista
 limit 3;
