d8ca7b5f38d49aa37f0af5397a30d1e52b2663b4
Lucas <lafdesouza2002@outlook.com>
2021-03-10 16:50:27 -0300

    Ajustes

--- desafio7.sql
+++ desafio7.sql
@@ -1,3 +1,4 @@
+DROP VIEW IF EXISTS perfil_artistas;
 CREATE VIEW perfil_artistas AS SELECT t1.artist AS `artista`, t1.cover AS `album`, COUNT(t2.following) AS `seguidores`
 FROM covers AS t1
 INNER JOIN following as t2
