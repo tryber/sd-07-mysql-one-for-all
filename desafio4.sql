66e265f13aee7d5091bfad1f1c1cc96f6ece0495
Muniz <vinicius.muniz.marin@gmail.com>
2021-03-10 00:51:48 -0300

    desafio4

---
+++ desafio4.sql
@@ -0,0 +1,7 @@
+CREATE VIEW top_3_artistas AS
+    SELECT 
+        seguindo_artistas artista, COUNT(*) seguidores
+    FROM
+        seguindo
+    GROUP BY seguindo_artistas
+    ORDER BY COUNT(*) DESC , seguindo_artistas;
