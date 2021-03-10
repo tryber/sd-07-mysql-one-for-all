c6ddc01f38cd04c41274485e3e4c3e5581df0eae
samantabelow <samanta.below@gmail.com>
2021-03-09 23:19:09 -0300

    Desfaio3

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+CREATE VIEW SpotifyClone.historico_reproducao_usuarios AS
+SELECT usuario.usuario AS usuario,
+cancao.cancao AS nome
+FROM SpotifyClone.usuario AS usuario
+INNER JOIN SpotifyClone.historico_reproducoes AS hist
+ON usuario.usuario_id = hist.usuario_id
+INNER JOIN SpotifyClone.cancao AS cancao
+ON cancao.cancao_id = hist.cancao_id
