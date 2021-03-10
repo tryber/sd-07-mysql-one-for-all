0808bfa5ca4f156d7d999619d0a65a4bc180efe2
Lucas <lafdesouza2002@outlook.com>
2021-03-10 15:02:03 -0300

    Ajuste desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -3,4 +3,4 @@ SELECT t1.name AS `usuario`, t2.reproductions AS `nome`
 FROM users AS t1
 INNER JOIN history_reproductions AS t2
 WHERE t1.user_id = t2.user_id
-ORDER BY `usuario`;
+ORDER BY `usuario`, `nome`;
