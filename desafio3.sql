af6a1ffd9af631197dd2c28e2a7aabf0946d18e5
Zeca Costa <jccostaso@gmail.com>
2021-03-10 17:30:39 -0300

    refact desafio3.sql

--- desafio3.sql
+++ desafio3.sql
@@ -1,10 +1,6 @@
-CREATE VIEW historico_reproducao_usuarios AS
-SELECT
-U.nome AS usuario,
-C.nome AS nome
+CREATE VIEW historico_reproducao_usuarios
+AS SELECT U.nome as `usuario`, C.nome AS `nome`
 FROM SpotifyClone.usuarios_cancoes AS UC
-INNER JOIN SpotifyClone.cancoes AS C
-ON UC.cancao_id = C.cancao_id
-INNER JOIN SpotifyClone.usuarios AS U
-ON UC.usuario_id = U.usuario_id
-ORDER BY U.nome, C.nome;
+INNER JOIN SpotifyClone.cancoes AS C ON UC.cancao_id = C.cancao_id
+INNER JOIN SpotifyClone.usuarios AS U ON UC.usuario_id = U.usuario_id
+ORDER BY `usuario`, `nome`;
