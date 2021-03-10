4c9b093a1796e77d502f67b9150641aca41fd810
Elano Evaristo <elano3004@gmail.com>
2021-03-10 10:57:27 -0300

    adicionando o desafio 1 e 2

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+CREATE VIEW estatisticas_musicais
+AS SELECT COUNT(son_id) AS 'cancoes',
+(
+  SELECT COUNT(*) FROM SpotifyClone.Singers
+) AS 'artistas',
+(
+  SELECT COUNT(*) FROM SpotifyClone.Albums
+) AS 'albuns'
+FROM SpotifyClone.Songs;
