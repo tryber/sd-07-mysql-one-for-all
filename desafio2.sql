25bdd2904658e4e4a512156e4b2fb588b200bbcb
Zeca Costa <jccostaso@gmail.com>
2021-03-10 15:01:06 -0300

    desafio2.sql

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+USE SpotfyClone;
+CREATE VIEW estatisticas_musicais AS
+SELECT
+COUNT(*) AS cancoes,
+(SELECT COUNT(*) FROM SpotifyClone.artistas)
+AS  artistas,
+(SELECT COUNT(*) FROM SpotifyClone.albuns)
+AS  albuns
+FROM SpotifyClone.cancoes;
