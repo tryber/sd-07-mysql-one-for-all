734b80fe78d558ea446d55e4253bd9fd849bda76
Lucas <lafdesouza2002@outlook.com>
2021-03-10 16:02:43 -0300

    Ajustes

--- desafio9.sql
+++ desafio9.sql
@@ -3,10 +3,10 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS albuns_do_artista;
 CREATE PROCEDURE albuns_do_artista(IN name VARCHAR(50))
 BEGIN
-	SELECT artist AS `artista`, cover AS `album`
-	FROM covers
-	WHERE artist = name
-    ORDER BY `album`;
+SELECT artist AS `artista`, cover AS `album`
+FROM covers
+WHERE artist = name
+ORDER BY `album`;
 END $$
 
 DELIMITER ;
