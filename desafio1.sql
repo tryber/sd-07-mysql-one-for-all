923bc5b9a0a371162e0007f22268ecfc52cb9b1f
samantabelow <samanta.below@gmail.com>
2021-03-09 21:38:08 -0300

    codeclimate

--- desafio1.sql
+++ desafio1.sql
@@ -3,13 +3,13 @@ DROP DATABASE IF EXISTS SpotifyClone;
 CREATE DATABASE SpotifyClone;
 
 CREATE TABLE plano(
-	plano_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  plano_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   plano VARCHAR(100) NOT NULL,
   valor_plano INT NOT NULL
 ) ENGINE=InnoDB;
 
 CREATE TABLE usuario(
-	usuario_id INT PRIMARY KEY NOT NULL,
+  usuario_id INT PRIMARY KEY NOT NULL,
   usuario VARCHAR(100) NOT NULL,
   idade INT NOT NULL,
   plano_id INT NOT NULL,
@@ -17,26 +17,26 @@ CREATE TABLE usuario(
 ) ENGINE=InnoDB;
 
 CREATE TABLE artista(
-	artista_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  artista_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   artista VARCHAR(100) NOT NULL
 ) ENGINE=InnoDB;
 
 CREATE TABLE album(
-	album_id INT PRIMARY KEY NOT NULL,
+  album_id INT PRIMARY KEY NOT NULL,
   album VARCHAR(100) NOT NULL,
   artista_id INT NOT NULL,
   FOREIGN KEY (artista_id) REFERENCES artista(artista_id)
 ) ENGINE=InnoDB;
 
 CREATE TABLE cancao(
-	cancao_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  cancao_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   cancao VARCHAR(100) NOT NULL,
   album_id INT NOT NULL,
   FOREIGN KEY (album_id) REFERENCES album(album_id)
 ) ENGINE=InnoDB;
 
 CREATE TABLE seguidores(
-	seguidores_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  seguidores_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   artista_id INT NOT NULL,
   usuario_id INT NOT NULL,
   FOREIGN KEY (artista_id) REFERENCES artista(artista_id),
@@ -44,7 +44,7 @@ CREATE TABLE seguidores(
 ) ENGINE=InnoDB;
 
 CREATE TABLE historico_reproducoes(
-	historico_reproducoes_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  historico_reproducoes_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   usuario_id INT NOT NULL,
   cancao_id INT NOT NULL,
   FOREIGN KEY (usuario_id) REFERENCES usuario(usuario_id),
