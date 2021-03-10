01ae6689c2768a9cda6ccb23b493644f898b3678
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2021-03-10 17:57:47 -0300

    desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT U.nome AS usuario,
+C.titulo_cancao AS nome
+FROM SpotifyClone.usuarios AS U
+INNER JOIN SpotifyClone.historico_de_reproducoes AS H
+ON U.usuario_id = H.usuario_id
+INNER JOIN SpotifyClone.cancoes AS C
+ON C.cancao_id = H.cancao_id;
