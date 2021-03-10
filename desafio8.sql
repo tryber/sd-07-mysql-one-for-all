f8efbe6a2f1cb2b3b8e8b98352caeb1ad08602d5
Muniz <vinicius.muniz.marin@gmail.com>
2021-03-10 17:34:22 -0300

    desafio8 fix 2

--- desafio8.sql
+++ desafio8.sql
@@ -1,15 +1,13 @@
-DELIMITER //
-
-CREATE 
-    TRIGGER  trigger_usuario_delete
-BEFORE DELETE ON usuarios FOR EACH ROW 
-    BEGIN 
-    DELETE FROM historico_reproducao
-    WHERE 
-    OLD.usuario_id = usuario_id;
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON usuarios
+FOR EACH ROW 
+BEGIN 
+DELETE FROM historico_reproducao
+WHERE 
+OLD.usuario_id = usuario_id;
 DELETE FROM seguindo 
 WHERE
-    OLD.usuario_id = usuario_id;
-    END //
-
+OLD.usuario_id = usuario_id;
+END $$
 DELIMITER ;
