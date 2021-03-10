589c3600ecc87c500dcc58f5ea0d74b3ff417794
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2021-03-10 19:50:42 -0300

    desafio 008

--- desafio8.sql
+++ desafio8.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE
-ON SpotifyClone.usuario FOR EACH ROW
+ON SpotifyClone.usuarios FOR EACH ROW
 BEGIN
 DELETE FROM SpotifyClone.historico_de_reproducoes
 WHERE usuario_id = OLD.usuario_id;
