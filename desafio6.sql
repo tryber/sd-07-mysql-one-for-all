22bbfa7d3c75ca55c978f84be3dc4bc619005e05
Lucas <lafdesouza2002@outlook.com>
2021-03-10 16:24:57 -0300

    Ajustes

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,4 @@
-DROP VIEW IF EXISTS faturamento_atual;
-CREATE VIEW faturamento_atual AS 
-SELECT ROUND(MIN(t1.value), 2) AS `faturamento_minimo`, ROUND(MAX(t1.value), 2) AS `faturamento_maximo`, ROUND(AVG(t1.value), 2) AS `faturamento_medio`, ROUND(SUM(t1.value), 2) AS `faturamento_total`
+CREATE VIEW faturamento_atual AS SELECT ROUND(MIN(t1.value), 2) AS `faturamento_minimo`, ROUND(MAX(t1.value), 2) AS `faturamento_maximo`, ROUND(AVG(t1.value), 2) AS `faturamento_medio`, ROUND(SUM(t1.value), 2) AS `faturamento_total`
 FROM plans AS t1
 INNER JOIN users as t2
 WHERE t1.plan = t2.plan;
--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,4 @@
-DROP VIEW IF EXISTS perfil_artistas;
-CREATE VIEW perfil_artistas AS 
-SELECT t1.artist AS `artist`, t1.cover AS `album`, COUNT(t2.following) AS `seguidores`
+CREATE VIEW perfil_artistas AS SELECT t1.artist AS `artist`, t1.cover AS `album`, COUNT(t2.following) AS `seguidores`
 FROM covers AS t1
 INNER JOIN following as t2
 ON t1.artist = t2.following
