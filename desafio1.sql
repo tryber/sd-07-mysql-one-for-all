a9b3a16e970ef7df293b4c7c1b49f9421dbc1627
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2021-03-10 08:29:01 -0300

    desafio1 - colunas alteradas

--- desafio1.sql
+++ desafio1.sql
@@ -9,7 +9,7 @@ valor_plano DOUBLE,
 PRIMARY KEY(plano_id)
 );
 
-INSERT INTO plano 
+INSERT INTO plano(plano_id, plano, valor_plano) 
 VALUES (1, 'gratuito', 0), (2, 'universitário', 5.99), (3, 'familiar', 7.99);
 
 CREATE TABLE artistas(
@@ -18,7 +18,7 @@ artista VARCHAR(100) NOT NULL,
 PRIMARY KEY(artista_id)
 );
 
-INSERT INTO artistas
+INSERT INTO artistas(artista_id, artista)
 VALUES (1,'Freedie Shannon'), (2, 'Lance Day'), (3, 'Peter Strong'), (4, 'Walter Phoenix');
 
 CREATE TABLE albuns(
@@ -28,7 +28,7 @@ artista_id INT NOT NULL,
 FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
 );
 
-INSERT INTO albuns
+INSERT INTO albuns(album_id, titulo_album, artista_id)
 VALUES (1,'Envious', 4), (2, 'Exuberant', 4), (3, 'Hallowed Steam', 3), (4, 'Incandescent', 2), (5, 'Temporary Culture', 1);
 
 CREATE TABLE cancoes(
@@ -38,7 +38,7 @@ album_id INT NOT NULL,
 FOREIGN KEY (album_id) REFERENCES albuns(album_id)
 );
 
-INSERT INTO cancoes 
+INSERT INTO cancoes(cancao_id, titulo_cancao, album_id)
 VALUES (1, 'Soul For Us', 1),
 (2,'Reflections Of Magic', 1),
 (3,'Dance With Her Own', 1),
@@ -67,7 +67,7 @@ PRIMARY KEY(usuario_id),
 FOREIGN KEY (plano) REFERENCES plano(plano_id)
 );
 
-INSERT INTO usuarios
+INSERT INTO usuarios(usuario_id, nome, idade, plano)
 VALUES (1, 'Thati', 23, 1), (2, 'Cintia', 35, 3), (3, 'Bill', 20, 2), (4, 'Roger', 45, 1);
 
 CREATE TABLE historico_de_reproducoes(
@@ -78,7 +78,7 @@ FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
 FOREIGN KEY (cancao_id) REFERENCES cancoes(cancao_id)
 );
 
-INSERT INTO historico_de_reproducoes
+INSERT INTO historico_de_reproducoes(historico_id, usuario_id, cancao_id)
 VALUES (1, 1, 1), (2, 1, 6), (3, 1, 14), (4, 1, 16), (5, 2, 13), (6, 2, 17), (7, 2, 2), (8, 2, 15), (9, 3, 4), (10, 3, 16), (11, 3, 6), (12, 4, 3), (13, 4, 18), (14, 4, 11);
 
 CREATE TABLE seguindo_artistas(
@@ -89,5 +89,5 @@ FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
 FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
 );
 
-INSERT INTO seguindo_artistas
+INSERT INTO seguindo_artistas(seguindo_id, usuario_id, artista_id)
 VALUES (1, 1, 4), (2, 1, 1), (3, 1, 2), (4, 2, 4), (5, 2, 2), (6, 3, 3), (7, 3, 4), (8, 4, 1);
