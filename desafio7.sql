4b0bed34806f244090144a35402e293dd0daf988
samantabelow <samanta.below@gmail.com>
2021-03-10 14:43:12 -0300

    Desafio7 Group by modificado

--- desafio7.sql
+++ desafio7.sql
@@ -7,5 +7,5 @@ INNER JOIN SpotifyClone.album AS album
 ON artista.artista_id = album.artista_id
 INNER JOIN SpotifyClone.seguidores AS seguidores
 ON artista.artista_id = seguidores.artista_id
-GROUP BY artista.artista_id
+GROUP BY album.album_id
 ORDER BY 3 DESC, artista.artista, album.album;
