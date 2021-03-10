532dfd3d3fa196414ba1ad646a80d550ac2ceb13
Lucas <lafdesouza2002@outlook.com>
2021-03-10 19:20:49 -0300

    Ajustes

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,8 @@
-CREATE VIEW faturamento_atual AS SELECT CAST(ROUND(MIN(t1.value), 2) AS DECIMAL(5,2)) AS `faturamento_minimo`, CAST(ROUND(MAX(t1.value), 2) AS DECIMAL) AS `faturamento_maximo`, CAST(ROUND(AVG(t1.value), 2) AS DECIMAL) AS `faturamento_medio`, CAST(ROUND(SUM(t1.value), 2) AS DECIMAL(5,2)) AS `faturamento_total`
+CREATE VIEW faturamento_atual AS 
+SELECT CAST(ROUND(MIN(t1.value), 2) AS DECIMAL(5, 2)) AS `faturamento_minimo`, 
+CAST(ROUND(MAX(t1.value), 2) AS DECIMAL(5, 2)) AS `faturamento_maximo`,
+CAST(ROUND(AVG(t1.value), 2) AS DECIMAL(5, 2)) AS `faturamento_medio`,
+CAST(ROUND(SUM(t1.value), 2) AS DECIMAL(5,2)) AS `faturamento_total`
 FROM plans AS t1
 INNER JOIN users as t2
 WHERE t1.plan = t2.plan;
