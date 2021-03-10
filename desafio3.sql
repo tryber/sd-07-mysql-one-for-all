c7b3f4aafab8d3e3d41c4207707326cd5106e0e2
Lucas <lafdesouza2002@outlook.com>
2021-03-10 14:57:28 -0300

    Desafio 3, 4 e 5. Imbiquei

---
+++ desafio3.sql
@@ -0,0 +1,6 @@
+CREATE VIEW historico_reproducao_usuarios AS 
+SELECT t1.name AS `usuario`, t2.reproductions AS `nome`
+FROM users AS t1
+INNER JOIN history_reproductions AS t2
+WHERE t1.user_id = t2.user_id
+ORDER BY `usuario`;
