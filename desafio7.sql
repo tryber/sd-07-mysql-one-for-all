8e1dc361f2c4fa5216361c58265265fabdbbc0c2
samantabelow <samanta.below@gmail.com>
2021-03-10 15:54:43 -0300

    Desafio 7 nova tentativa

--- desafio7.sql
+++ desafio7.sql
@@ -1,11 +1,11 @@
 CREATE VIEW SpotifyClone.perfil_artistas AS
-SELECT artista.artista AS artista,
-album.album AS album,
-COUNT(album.album_id) AS seguidores
+SELECT artista.artista AS `artista`,
+album.album AS `album`,
+COUNT(seguidores.artista_id) AS `seguidores`
 FROM SpotifyClone.artista AS artista
 INNER JOIN SpotifyClone.album AS album
 ON artista.artista_id = album.artista_id
 INNER JOIN SpotifyClone.seguidores AS seguidores
 ON artista.artista_id = seguidores.artista_id
-GROUP BY album.album_id
-ORDER BY 3 DESC, artista.artista, album.album;
+GROUP BY artista, album
+ORDER BY seguidores DESC, artista, album;
