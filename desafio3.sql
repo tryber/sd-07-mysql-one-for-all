747171ccb688336f66d5b7ef77678e0207012345
Lorena Góes <goesloren@gmail.com>
2021-03-11 18:27:44 -0300

    update challenges 2 to 5

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
 CREATE VIEW historico_reproducao_usuarios AS
-  SELECT u.usuario AS 'usuario',
-  c.cancao AS 'nome'
+  SELECT u.usuario AS usuario,
+  c.cancao AS nome
   FROM SpotifyClone.usuarios u
   INNER JOIN SpotifyClone.historico hist ON hist.usuario_id = u.id
   INNER JOIN SpotifyClone.cancoes c ON c.id = hist.cancao_id
