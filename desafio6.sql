911a3fcd2c58ee2c9313c53f0bacb20afe76c0ea
Macarena Rodriguez <macarenar697@gmail.com>
2021-03-11 09:47:50 -0300

    req6

--- desafio6.sql
+++ desafio6.sql
@@ -1,3 +1,4 @@
+create view faturamento_atual as 
 select round(min(valor_plano), 2) as faturamento_minimo,
 round(max(valor_plano), 2) as faturamento_maximo,
 round(avg(valor_plano), 2) as faturamento_medio,
