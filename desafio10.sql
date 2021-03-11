9660a6c044e65b8f7965d58f11b0299668d7aa1a
Zeca Costa <jccostaso@gmail.com>
2021-03-11 18:54:10 -0300

    refact desafio10.sql

--- desafio10.sql
+++ desafio10.sql
@@ -5,9 +5,9 @@ RETURNS INT READS SQL DATA
 BEGIN
   DECLARE total_cancoes INT;
   SELECT COUNT(UC.cancao_id)
-	FROM SpotifyClone.usuarios AS U
-	INNER JOIN SpotifyClone.usuarios_cancoes UC ON U.usuario_id = UC.usuario_id
-	WHERE U.usuario_id = usuario_id
+	FROM SpotifyClone.usuarios AS US
+	INNER JOIN SpotifyClone.usuarios_cancoes UC ON US.usuario_id = UC.usuario_id
+	WHERE US.usuario_id = usuario_id
 	GROUP BY UC.usuario_id
     INTO total_cancoes;
     RETURN total_cancoes;
