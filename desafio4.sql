d721ae75198a3efc533d3c6104843c28164d55a7
Macarena Rodriguez <macarenar697@gmail.com>
2021-03-11 11:15:16 -0300

    req4 e 5 refatoracao

--- desafio4.sql
+++ desafio4.sql
@@ -1,7 +1,6 @@
 create view top_3_artistas as
 select a.artista as artista, count(a.artista) as seguidores
-from SpotifyClone.usuarios as u
-inner join SpotifyClone.seguindo as s on u.usuario_id = s.usuario_id
+from SpotifyClone.seguindo as s
 inner join SpotifyClone.artistas as a on a.artista_id = s.artista_id
 group by artista
 order by seguidores desc, artista
--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,6 @@
 create view top_2_hits_do_momento as
-select c.cancoe as cancao, count(u.usuario) as reproducoes
+select c.cancoe as cancao, count(c.cancoe) as reproducoes
 from SpotifyClone.reproducoes as r
-inner join SpotifyClone.usuarios as u on u.usuario_id = r.usuario_id
 inner join SpotifyClone.cancoes as c on c.cancoe_id = r.cancoe_id
 group by cancoe
 order by reproducoes desc, cancao
