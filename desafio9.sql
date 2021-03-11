69f5eb8eece2e02e018f2b3e8a532fb508e8d65c
Danwhat <daanwhant@gmail.com>
2021-03-10 21:20:58 -0300

    lint

--- desafio9.sql
+++ desafio9.sql
@@ -2,12 +2,12 @@ DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista(IN nameArtista VARCHAR(100))
 BEGIN
-    SELECT sin.sin_name AS 'artista',
-    abl.abl_name AS 'album'
-    FROM SpotifyClone.albums AS abl
-    INNER JOIN SpotifyClone.artistas AS sin ON sin.idartistas = abl.idartistas
-    WHERE sin.name_singer = nameArtista
-    ORDER BY album;
+SELECT sin.sin_name AS 'artista',
+abl.abl_name AS 'album'
+FROM SpotifyClone.albums AS abl
+INNER JOIN SpotifyClone.artistas AS sin ON sin.idartistas = abl.idartistas
+WHERE sin.name_singer = nameArtista
+ORDER BY album;
 END $$
 
 DELIMITER ;
