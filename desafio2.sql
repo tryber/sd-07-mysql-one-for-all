37e752f3ea214e1db0de6f7c8bdbba4465637bc5
Lorena Góes <goesloren@gmail.com>
2021-03-11 16:57:18 -0300

    challenges 2 and 3 - fixing syntax

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,7 @@
-USE SpotifyClone;
-
 CREATE VIEW estatisticas_musicais AS
   SELECT COUNT(c.cancao) AS 'cancoes',
   COUNT(DISTINCT art.artista) AS 'artistas',
   COUNT(DISTINCT alb.album) AS 'albuns'
-  FROM albuns alb
-  INNER JOIN cancoes c ON alb.id = c.album_id
-  INNER JOIN artistas art ON art.id = alb.artista_id;
+  FROM SpotifyClone.albuns alb
+  INNER JOIN SpotifyClone.cancoes c ON alb.id = c.album_id
+  INNER JOIN SpotifyClone.artistas art ON art.id = alb.artista_id;
--- desafio3.sql
+++ desafio3.sql
@@ -1,9 +1,7 @@
-USE SpotifyClone;
-
 CREATE VIEW historico_reproducao_usuarios AS
   SELECT u.usuario AS 'usuario',
   c.cancao AS 'nome'
-  FROM usuarios u
-  INNER JOIN historico hist ON hist.usuario_id = u.id
-  INNER JOIN cancoes c ON c.id = hist.cancao_id
+  FROM SpotifyClone.usuarios u
+  INNER JOIN SpotifyClone.historico hist ON hist.usuario_id = u.id
+  INNER JOIN SpotifyClone.cancoes c ON c.id = hist.cancao_id
   ORDER BY `usuario`, `nome`;
