3ae5e83af6dafab3bcddbd2a0aa61cee6b8d20f3
Macarena Rodriguez <macarenar697@gmail.com>
2021-03-11 10:09:44 -0300

    req6

--- desafio6.sql
+++ desafio6.sql
@@ -3,4 +3,5 @@ select round(min(valor_plano), 2) as faturamento_minimo,
 round(max(valor_plano), 2) as faturamento_maximo,
 round(avg(valor_plano), 2) as faturamento_medio,
 round(sum(valor_plano), 2) as faturamento_total
-from SpotifyClone.planos;
+from SpotifyClone.planos
+inner join SpotifyClone.usuarios as u on u.plano_id = planos.plano_id;
