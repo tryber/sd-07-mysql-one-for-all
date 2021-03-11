068453ff15b49d50f5fb22090e4458470c11ef9e
Zeca Costa <jccostaso@gmail.com>
2021-03-11 19:05:10 -0300

    refact3 desafio10.sql

--- desafio10.sql
+++ desafio10.sql
@@ -3,14 +3,14 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(usuario_id int)
 RETURNS INT READS SQL DATA
 BEGIN
-  DECLARE total_cancoes INT;
-  SELECT COUNT(UC.cancao_id)
-	FROM SpotifyClone.usuarios AS US
-	INNER JOIN SpotifyClone.usuarios_cancoes UC ON US.usuario_id = UC.usuario_id
-	WHERE US.usuario_id = usuario_id
-	GROUP BY UC.usuario_id
-  INTO total_cancoes;
-  RETURN total_cancoes;
+    DECLARE total_cancoes INT;
+    SELECT COUNT(UC.cancao_id)
+	  FROM SpotifyClone.usuarios AS US
+	  INNER JOIN SpotifyClone.usuarios_cancoes UC ON US.usuario_id = UC.usuario_id
+	  WHERE US.usuario_id = usuario_id
+	  GROUP BY UC.usuario_id
+    INTO total_cancoes;
+    RETURN total_cancoes;
 END $$
 
 DELIMITER ;
