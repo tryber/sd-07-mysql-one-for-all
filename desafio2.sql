c355e650c518b9c6d97153a8d791d6a461b93385
brendalima <brenda.c.lima@poli.ufrj.br>
2021-03-11 18:14:11 -0300

    query for challenge n2

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+USE SpotifyClone;
+
+CREATE VIEW estatisticas_musicais AS
+SELECT COUNT(*) AS cancoes, (
+SELECT COUNT(*)
+FROM artistas) AS artistas, (
+SELECT COUNT(*)
+FROM albuns) AS albuns
+FROM cancoes;
