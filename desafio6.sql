76c5d025348de53ae9098f4bd25d94a1da345a86
Macarena Rodriguez <macarenar697@gmail.com>
2021-03-11 09:33:09 -0300

    req6

---
+++ desafio6.sql
@@ -0,0 +1,5 @@
+select round(min(valor_plano), 2) as faturamento_minimo,
+round(max(valor_plano), 2) as faturamento_maximo,
+round(avg(valor_plano), 2) as faturamento_medio,
+round(sum(valor_plano), 2) as faturamento_total
+from SpotifyClone.planos;
