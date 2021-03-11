881438406e607bdb2e2937f8bd77d56d9eab2341
Fabio Camargos <fabiocmmcm@gmail.com>
2021-03-11 19:46:50 -0300

    Finale

--- tests/challenges.spec.js
+++ tests/challenges.spec.js
@@ -351,7 +351,7 @@ describe('Queries de seleção', () => {
     });
   });
 
-  describe.only('Crie uma `VIEW` chamada `cancoes_premium` que exiba o nome e a quantidade de vezes que cada canção foi tocada por pessoas usuárias do plano familiar ou universitário', () => {
+  describe('Crie uma `VIEW` chamada `cancoes_premium` que exiba o nome e a quantidade de vezes que cada canção foi tocada por pessoas usuárias do plano familiar ou universitário', () => {
     it('Verifica o desafio 11', async () => {
       const createViewQuery = readFileSync('desafio11.sql', 'utf8').trim();
 
