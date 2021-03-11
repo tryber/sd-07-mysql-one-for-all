7ac35a4b0b0dc49f930e9e495e6b1ee14e9d7e25
Danwhat <daanwhant@gmail.com>
2021-03-10 21:40:36 -0300

    desafio11

--- desafio11.sql
+++ desafio11.sql
@@ -5,6 +5,6 @@ COUNT(UM.usuario_idusuario) AS 'reproducoes'
 FROM SpotifyClone.usuario_musicas UM
 JOIN SpotifyClone.musicas M ON UM.musicas_idmusicas = M.idmusicas
 JOIN SpotifyClone.usuario U ON U.idusuario =  UM.usuario_idusuario
-WHERE U.planos_idplanos >= 2
+WHERE U.idplanos >= 2
 GROUP BY M.name_song
 ORDER BY M.name_song;
