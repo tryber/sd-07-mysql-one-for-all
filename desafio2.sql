aecb63d749e93f967886977d73282a82251b5888
Lorena Góes <goesloren@gmail.com>
2021-03-11 16:48:20 -0300

    fixing challenges 2 and 3

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,5 @@
 USE SpotifyClone;
 
-DROP VIEW estatisticas_musicais;
 CREATE VIEW estatisticas_musicais AS
   SELECT COUNT(c.cancao) AS 'cancoes',
   COUNT(DISTINCT art.artista) AS 'artistas',
@@ -8,5 +7,3 @@ CREATE VIEW estatisticas_musicais AS
   FROM albuns alb
   INNER JOIN cancoes c ON alb.id = c.album_id
   INNER JOIN artistas art ON art.id = alb.artista_id;
-  
-select * from estatisticas_musicais;
--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,5 @@
 USE SpotifyClone;
-select * from cancoes;
 
-DROP VIEW historico_reproducao_usuarios;
 CREATE VIEW historico_reproducao_usuarios AS
   SELECT u.usuario AS 'usuario',
   c.cancao AS 'nome'
@@ -9,5 +7,3 @@ CREATE VIEW historico_reproducao_usuarios AS
   INNER JOIN historico hist ON hist.usuario_id = u.id
   INNER JOIN cancoes c ON c.id = hist.cancao_id
   ORDER BY `usuario`, `nome`;
-
-select * from historico_reproducao_usuarios;
