61a0ba3d8a3b1b5184a199418e23ae0fed05e4e9
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-03-11 11:49:31 -0300

    desafio6

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,9 @@
 CREATE VIEW faturamento_atual AS
 SELECT MIN(P1.price) AS "faturamento_minimo" ,
 MAX(P1.price) AS "faturamento_maximo",
-ROUND(AVG(P1.price), 2)"faturamento_médio",
-SUM(P1.price)"faturamento_total"
-FROM SpotifyClone.plans AS P1;
+ROUND((SUM(P1.price)/
+(SELECT COUNT(users.id))),2) AS "faturamento_medio",
+SUM(P1.price) AS "faturamento_total"
+FROM SpotifyClone.plans AS P1
+INNER JOIN SpotifyClone.users AS users
+ON P1.id = users.plan_id;
