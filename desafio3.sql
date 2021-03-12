e11635786579d3181db5d5fc190802f289807b3b
Camila Valentim <camilaifpb@hotmail.com>
2021-03-12 13:39:21 -0300

    requisito 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,9 +1,9 @@
 CREATE VIEW historico_de_reproducoes AS
 SELECT u.usuario AS usuario,
 c.cancao AS nome
-FROM SpotifyClone.historico_de_reproducoes AS h
-INNER JOIN SpotifyClone.usuarios AS u
-ON h.usuario_id = u.usuario_id
+FROM SpotifyClone.usuarios AS u
+INNER JOIN SpotifyClone.historico_de_reproducoes AS h
+ON u.usuario_id = h.usuario_id
 INNER JOIN SpotifyClone.cancoes AS c
 ON c.cancao_id = h.cancao_id
 ORDER BY usuario, nome;
