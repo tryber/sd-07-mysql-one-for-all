7a57f3b3543523c1c75d55fdd3396c31e783571c
Lorena Góes <goesloren@gmail.com>
2021-03-12 17:22:16 -0300

    challenge 9 - fixing structure

--- desafio9.sql
+++ desafio9.sql
@@ -4,10 +4,11 @@ CREATE PROCEDURE albuns_do_artista(IN nome_artista VARCHAR(50))
 BEGIN
   SELECT 
     art.artista AS artista,
-    art.album AS album
+    alb.album AS album
   FROM SpotifyClone.artistas AS art
   INNER JOIN SpotifyClone.albuns AS alb
   ON art.id = alb.artista_id
+  WHERE nome_artista = art.artista
   ORDER BY album;
 END $$
 
