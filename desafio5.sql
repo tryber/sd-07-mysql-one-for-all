8f12f9100d0b3c5025537a168af06909f262387d
moraisgabri <gabriel8azevedo@gmail.com>
2021-03-12 03:21:15 -0300

    sera se

--- desafio5.sql
+++ desafio5.sql
@@ -1,10 +1,9 @@
+CREATE VIEW top_2_hits_do_momento AS
 SELECT
-c.cancao AS cancao,
-COUNT(*) AS reproducoes
-FROM
-SpotifyClone.cancoes AS c
-INNER JOIN SpotifyClone.historico_de_reproducoes AS hr
-ON c.id = hr.cancoes_id
-GROUP BY c.cancao, hr.cancoes_id
-ORDER BY 2 DESC, cancao
+m.music_name AS 'cancao',
+COUNT(mh.user_id) AS 'reproducoes'
+FROM SpotifyClone.musics AS m
+INNER JOIN SpotifyClone.music_history AS mh ON mh.music_id = m.id
+GROUP BY music_id
+ORDER BY `reproducoes` DESC, `cancao`
 LIMIT 2;
--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,8 @@
 CREATE VIEW faturamento_atual AS
 SELECT
-ROUND(MIN(p.valor_plano), 2) AS faturamento_minimo,
-ROUND(MAX(p.valor_plano), 2) AS faturamento_maximo,
-ROUND(AVG(p.valor_plano), 2) AS faturamento_medio,
-ROUND(SUM(p.valor_plano), 2) AS faturamento_total
-FROM
-SpotifyClone.planos AS p
-INNER JOIN SpotifyClone.usuarios AS u
-ON u.planos_id = p.id;
+  ROUND(MIN(plan_price), 2) AS 'faturamento_minimo',
+  ROUND(MAX(plan_price),2) AS 'faturamento_maximo',
+  ROUND(AVG(plan_price), 2) AS 'faturamento_medio',
+  ROUND(SUM(plan_price), 2) AS 'faturamento_total'
+FROM SpotifyClone.plans
+INNER JOIN SpotifyClone.users ON users.plan_id = plans.id;
