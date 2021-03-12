22ef130636b75931b9c6bd3a0c082455aa861175
Lorena Góes <goesloren@gmail.com>
2021-03-12 18:58:09 -0300

    fixing challenge 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,27 +1,11 @@
 CREATE VIEW cancoes_premium AS
-  SELECT
+ SELECT
     c.cancao AS nome,
     COUNT(*) AS reproducoes
   FROM SpotifyClone.usuarios u
   INNER JOIN SpotifyClone.planos p ON p.id = u.plano_id
   INNER JOIN SpotifyClone.historico h ON h.usuario_id = u.id
   INNER JOIN SpotifyClone.cancoes c ON c.id = h.cancao_id
+  WHERE p.plano = 'familiar' OR p.plano = 'universitário'
   GROUP BY nome
-  WHERE p.plano = 'familiar' OR p.plano = 'universitário'
-  ORDER BY nome;
-  
-SELECT * from SpotifyClone.historico;
-SELECT * from SpotifyClone.cancoes;
-SELECT * from SpotifyClone.planos;
-SELECT * from SpotifyClone.usuarios;
-
-SELECT
-    c.cancao AS nome,
-    COUNT(*) AS reproducoes
-  FROM SpotifyClone.usuarios u
-  INNER JOIN SpotifyClone.planos p ON p.id = u.plano_id
-  INNER JOIN SpotifyClone.historico h ON h.usuario_id = u.id
-  INNER JOIN SpotifyClone.cancoes c ON c.id = h.cancao_id
-  WHERE p.plano = 'familiar' OR p.plano = 'universitário'
-  GROUP BY `nome`
   ORDER BY nome;
