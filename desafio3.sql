4cb05d277b27d0487e97f14ec9ded0e6eae1ecbd
Camila Valentim <camilaifpb@hotmail.com>
2021-03-12 17:04:59 -0300

    requisito 3

--- desafio3.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW historico_de_reproducao_usuarios AS
-SELECT
-u.usuario AS usuario,
-c.cancao AS nome
-FROM SpotifyClone.historico_de_reproducao_usuarios AS h
-INNER JOIN SpotifyClone.usuarios AS u ON h.usuario_id = u.usuario_id
-INNER JOIN SpotifyClone.cancoes AS c ON h.cancao_id = c.musicas_id
-ORDER BY usuario, nome;
---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON usuario
+FOR EACH ROW
+BEGIN
+DELETE FROM seguindo_artistas 
+WHERE usuario_id = OLD.usuario_id;
+DELETE FROM historico_de_reproducoes 
+WHERE usuario_id = OLD.usuario_id;
+END $$
+DELIMITER ;
