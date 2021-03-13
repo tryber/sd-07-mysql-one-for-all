dfad0db1da17cea380772c6fa96bdddd6a777a90
laurods <itrafor@gmail.com>
2021-03-13 10:37:46 -0300

    Desafio3 corrigido

--- desafio3.sql
+++ desafio3.sql
@@ -1,9 +1,9 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT
 u.usuario AS usuario,
-m.musica AS musica
+m.musica AS nome
 FROM
 SpotifyClone.usuario AS u
 INNER JOIN SpotifyClone.historico AS h ON u.usuario_id = h.usuario_id
 INNER JOIN SpotifyClone.musica AS m ON h.musica_id = m.musica_id
-ORDER BY usuario, musica;
+ORDER BY usuario, nome;
