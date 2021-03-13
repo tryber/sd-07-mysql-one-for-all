62f8adb0f5e6d3ca5ddf682941764d40934f8f69
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-03-13 17:43:11 -0300

    7th requeriment

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT artistas.artista AS artista,
+albuns.album AS album,
+COUNT(seguidores.usuario_id) AS seguidores
+FROM SpotifyClone.artistas AS artistas
+INNER JOIN SpotifyClone.albuns AS albuns ON artistas.id = albuns.artista_id
+INNER JOIN SpotifyClone.seguidores AS seguidores ON seguidores.artista_id = artistas.id
+GROUP BY seguidores.artista_id, albuns.album
+ORDER BY `seguidores` DESC, `artista`, `album`;
