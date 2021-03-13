219bed4b425100812ab3aae2eba0d115b149d562
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-03-13 14:18:01 -0300

    letras em maiusculo

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,8 @@
 CREATE VIEW cancoes_premium AS
 SELECT musics.title AS "nome",
 COUNT(music_history.user_id) AS "reproducoes"
-FROM spotifyclone.musics AS musics
-INNER JOIN spotifyclone.music_history AS music_history
+FROM SpotifyClone.musics AS musics
+INNER JOIN SpotifyClone.music_history AS music_history
 ON musics.id = music_history.music_id
 GROUP BY musics.title
 ORDER BY musics.title;
