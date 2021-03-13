1c4890510179884de5406be77397356747e05445
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-03-13 16:09:41 -0300

    3rd requiment

---
+++ desafio3.sql
@@ -0,0 +1,6 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT usuarios.usuario AS usuario, cancoes.cancao AS nome
+FROM SpotifyClone.usuarios AS usuarios
+INNER JOIN SpotifyClone.historico AS historicos ON usuarios.id = historicos.usuario_id
+INNER JOIN SpotifyClone.cancoes AS cancoes ON cancoes.id = historicos.cancao_id
+ORDER BY `usuario`, `nome`;
