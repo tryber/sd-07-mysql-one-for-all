dec4d8a3004ab1f48ed9737fb66d5cf46a41e76c
Camila Valentim <camilaifpb@hotmail.com>
2021-03-13 10:00:39 -0300

    desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+/*Referencia ALexandre Faustino */
+CREATE VIEW perfil_artistas AS
+SELECT ar.artista AS "artista",
+album AS "album",
+COUNT(sa.usuario_id) AS "seguidores"
+FROM SpotifyClone.artistas AS ar
+INNER JOIN SpotifyClone.albuns AS al
+ON ar.artista_id = al.artista_id
+INNER JOIN SpotifyClone.seguindo_artistas AS sa
+ON ar.artista_id = sa.artista_id
+GROUP BY 2, 1
+ORDER BY 3 DESC, 1, 2;
