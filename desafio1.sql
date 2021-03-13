8b5784fcde5d59ca0998dc70e1e7bb475ac776c9
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-03-13 16:33:33 -0300

    modifing my table for others requeriments

--- desafio1.sql
+++ desafio1.sql
@@ -1,74 +1,74 @@
--- DROP DATABASE IF EXISTS SpotiyfyClone;-- 
 CREATE DATABASE SpotifyClone;
 USE SpotifyClone;
 
-CREATE TABLE IF NOT EXISTS planos (
-id INT PRIMARY KEY AUTO_INCREMENT,
-plano VARCHAR(50) NOT NULL,
-valor DECIMAL(5, 2) NOT NULL
+CREATE TABLE planos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    plano VARCHAR(50) NOT NULL,
+    valor DECIMAL (5, 2) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS artistas (
-id INT PRIMARY KEY AUTO_INCREMENT,
-artista VARCHAR(45) NOT NULL
+CREATE TABLE artistas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    artista VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS albuns (
-id INT PRIMARY KEY AUTO_INCREMENT,
-album VARCHAR(50) NOT NULL,
-artista_id INT NOT NULL,
-FOREIGN KEY (artista_id) REFERENCES SpotifyClone.artistas (id)
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    album VARCHAR(50) NOT NULL,
+    artista_id INT NOT NULL,
+    FOREIGN KEY (artista_id) REFERENCES SpotifyClone.artistas (id)
 );
 
 CREATE TABLE IF NOT EXISTS cancoes (
-id INT PRIMARY KEY AUTO_INCREMENT,
-cancao VARCHAR(50) NOT NULL,
-album_id INT NOT NULL,
-FOREIGN KEY (album_id) REFERENCES SpotifyClone.albuns (id)
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  cancao VARCHAR(50) NOT NULL,
+  album_id INT NOT NULL,
+  FOREIGN KEY (album_id) REFERENCES SpotifyClone.albuns (id)
 );
 
 CREATE TABLE IF NOT EXISTS usuarios (
-id INT PRIMARY KEY AUTO_INCREMENT,
-usuario VARCHAR(50) NOT NULL,
-idade INT NOT NULL,
-plano_id INT NOT NULL,
-FOREIGN KEY (plano_id) REFERENCES SpotifyClone.planos (id)
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  usuario VARCHAR(45) NOT NULL,
+  idade INT NOT NULL,
+  plano_id INT NOT NULL,
+  FOREIGN KEY (plano_id) REFERENCES SpotifyClone.planos (id)
 );
 
 CREATE TABLE IF NOT EXISTS seguidores (
-artista_id INT NOT NULL,
-usuario_id INT NOT NULL,
-PRIMARY KEY (artista_id, usuario_id),
-FOREIGN KEY (artista_id) REFERENCES SpotifyClone.artistas (id),
+  artista_id INT NOT NULL,
+  usuario_id INT NOT NULL,
+  PRIMARY KEY (artista_id, usuario_id),
+  FOREIGN KEY (artista_id) REFERENCES SpotifyClone.artistas (id),
+  FOREIGN KEY (usuario_id) REFERENCES SpotifyClone.usuarios (id)
+);
+
+CREATE TABLE IF NOT EXISTS historicos (
+  cancao_id INT NOT NULL,
+  usuario_id INT NOT NULL,
+  PRIMARY KEY (cancao_id, usuario_id),
+  FOREIGN KEY (cancao_id) REFERENCES SpotifyClone.cancoes (id),
 FOREIGN KEY (usuario_id) REFERENCES SpotifyClone.usuarios (id)
 );
 
-CREATE TABLE IF NOT EXISTS historico (
-cancao_id INT NOT NULL,
-usuario_id INT NOT NULL,
-PRIMARY KEY (cancao_id, usuario_id),
-FOREIGN KEY (cancao_id) REFERENCES SpotifyClone.cancoes (id),
-FOREIGN KEY (usuario_id) REFERENCES SpotifyClone.usuarios (id)
-);
 
 INSERT INTO planos (plano, valor)
 VALUES
     ('gratuito', 0),
-    ('universitário', 5.99),
-    ('familiar', 7.99);
+    ('familiar', 7.99),
+    ('universitário', 5.99);
 
 INSERT INTO artistas (artista)
 VALUES
-    ('Walter Phoenix'),
+    ('Walter Phoenix'), 
     ('Peter Strong'),
     ('Lance Day'),
     ('Freedie Shannon');
-    
+
 INSERT INTO albuns (album, artista_id)
 VALUES
     ('Envious', 1),
-    ('Hallowed Steam', 2),
     ('Exuberant', 1),
+    ('Hallowed Steam', 2),
     ('Incandescent', 3),
     ('Temporary Culture', 4);
 
@@ -80,17 +80,17 @@ VALUES
     ('Troubles Of My Inner Fire', 2),
     ('Time Fireworks', 2),
     ('Magic Circus', 3),
-    ('Sweetie, Let''s Go Wild', 3),
     ('Honey, So Do I', 3),
+    ('Sweetie, Let''s Go Wild', 3),
     ('She Knows', 3),
-    ('Celebration Of More', 4),
     ('Fantasy For Me', 4),
+    ('Celebration Of More', 4),
     ('Rock His Everything', 4),
     ('Home Forever', 4),
-    ('Honey, Let''s Be Silly', 4),
     ('Diamond Power', 4),
-    ('Words Of Her Life', 5),
+    ('Honey, Let''s Be Silly', 4),
     ('Thang Of Thunder', 5),
+    ('Words Of Her Life', 5),
     ('Without My Streets', 5);
 
 INSERT INTO usuarios (usuario, idade, plano_id)
@@ -99,7 +99,7 @@ VALUES
     ('Cintia', 35, 2),
     ('Bill', 20, 3),
     ('Roger', 45, 1);
-    
+
 INSERT INTO seguidores (usuario_id, artista_id)
 VALUES
     (1, 1),
@@ -111,7 +111,7 @@ VALUES
     (3, 1),
     (4, 4);
 
-INSERT INTO historico (usuario_id, cancao_id)
+INSERT INTO historicos (usuario_id, cancao_id)
 VALUES
     (1, 1),
     (1, 6),
@@ -121,9 +121,9 @@ VALUES
     (2, 17),
     (2, 2),
     (2, 15),
-    (4, 3),
     (3, 4),
     (3, 16),
     (3, 6),
+    (4, 3),
     (4,18),
     (4, 11);
