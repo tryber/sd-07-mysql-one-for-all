9c584346ebafed741bf73f88f3e1fb7cd3a485b5
lisvalver <lisvalver.atte@gmail.com>
2021-03-14 12:29:12 -0300

    Desafio 10 - Alterando dados de entrada para ID

--- desafio10.sql
+++ desafio10.sql
@@ -1,5 +1,5 @@
 DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historico(nome VARCHAR(100))
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE mus_hist INT;
@@ -7,7 +7,7 @@ SELECT COUNT(h.user_id)
 FROM SpotifyClone.reproduction_history AS h
 INNER JOIN SpotifyClone.user AS u
 ON u.user_id = h.user_id 
-WHERE u.user_name = nome
+WHERE u.user_id = id
 INTO mus_hist;
 RETURN mus_hist;
 END $$
