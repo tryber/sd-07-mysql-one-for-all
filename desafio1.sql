2d387dd517eda7c06d337eedd06cf67e9c39d112
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2021-03-14 18:51:46 -0300

    NIcholas - Iniciando projeto. VQV

---
+++ desafio1.sql
@@ -0,0 +1,122 @@
+DROP SCHEMA IF EXISTS SpotifyClone;
+CREATE SCHEMA IF NOT EXISTS SpotifyClone;
+USE SpotifyClone;
+
+CREATE TABLE IF NOT EXISTS planos (
+	id_plano INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome_plano VARCHAR(25) NOT NULL UNIQUE,
+    valor DECIMAL(5,2) NOT NULL
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS artistas (
+	id_artista INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome_artista VARCHAR(100) NOT NULL
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS albuns (
+	id_album INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome_album VARCHAR(50) NOT NULL,
+    id_artista INT NOT NULL,
+    FOREIGN KEY (id_artista) REFERENCES SpotifyClone.artistas(id_artista)
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+	id_usuario INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome_usuario VARCHAR(100) NOT NULL,
+    idade INT DEFAULT NULL,
+    id_plano INT NOT NULL,
+    FOREIGN KEY (id_plano) REFERENCES SpotifyClone.planos(id_plano)
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS cancoes (
+	id_cancao INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome_cancao VARCHAR(100) NOT NULL,
+    id_album INT NOT NULL,
+    FOREIGN KEY (id_album) REFERENCES SpotifyClone.albuns(id_album)
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS historico_reproducao (
+	id_usuario INT NOT NULL,
+    id_cancao INT NOT NULL,
+    PRIMARY KEY (id_cancao, id_usuario),
+    FOREIGN KEY (id_cancao) REFERENCES SpotifyClone.cancoes(id_cancao),
+    FOREIGN KEY (id_usuario) REFERENCES SpotifyClone.usuarios(id_usuario)
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS seguidores (
+	id_usuario INT NOT NULL,
+    id_artista INT NOT NULL,
+    PRIMARY KEY (id_artista, id_usuario),
+    FOREIGN KEY (id_artista) REFERENCES SpotifyClone.artistas(id_artista),
+    FOREIGN KEY (id_usuario) REFERENCES SpotifyClone.usuarios(id_usuario)
+)ENGINE=InnoDB;
+
+INSERT INTO planos (nome_plano, valor) VALUES
+('gratuito', 0),
+('familiar', 7.99),
+('universitário', 5.99);
+
+INSERT INTO usuarios (nome_usuario, idade, id_plano) VALUES
+('Thati', 23, 1),
+('Cintia', 35, 2),
+('Bill', 20, 3),
+('Roger', 45, 1);
+
+INSERT INTO artistas (nome_artista) VALUES
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
+
+INSERT INTO albuns (nome_album, id_artista) VALUES
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
+
+INSERT INTO cancoes (nome_cancao, id_album) VALUES
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+('Sweetie, Let\'s Go Wild', 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+('Honey, Let\'s Be Silly', 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
+
+INSERT INTO seguidores (id_usuario, id_artista) VALUES
+(1, 1),
+(1, 4),
+(1, 3),
+(2, 1),
+(2, 3),
+(3, 2),
+(3, 1),
+(4, 4);
+
+INSERT INTO historico_reproducao (id_usuario, id_cancao) VALUES
+(1, 1),
+(1, 6),
+(1, 14),
+(1, 16),
+(2, 13),
+(2, 17),
+(2, 2),
+(2, 15),
+(3, 4),
+(3, 16),
+(3, 6),
+(4, 3),
+(4, 18),
+(4, 11);
