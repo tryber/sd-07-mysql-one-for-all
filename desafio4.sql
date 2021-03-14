4039ff4268572a6a80aa5ce5a9fcc9f6c5a0f757
julianafatsil <julianafatsil@gmail.com>
2021-03-14 16:57:50 -0300

    Desfio 06

--- desafio4.sql
+++ desafio4.sql
@@ -2,9 +2,9 @@ CREATE VIEW top_3_artistas AS
     SELECT 
         a.artista_nome AS artista, COUNT(s.usuario_id) AS seguidores
     FROM
-        spotifyclone.seguindo_artista AS s
+        SpotifyClone.seguindo_artista AS s
             JOIN
-        spotifyclone.artista AS a ON s.artista_id = a.artista_id
+        SpotifyClone.artista AS a ON s.artista_id = a.artista_id
     GROUP BY artista
     ORDER BY seguidores DESC , artista
     LIMIT 3;
--- desafio5.sql
+++ desafio5.sql
@@ -2,9 +2,9 @@ CREATE VIEW top_2_hits_do_momento AS
     SELECT 
         c.cancao_nome AS cancao, COUNT(h.usuario_id) AS reproducoes
     FROM
-        spotifyclone.historico_de_reproducoes AS h
+        SpotifyClone.historico_de_reproducoes AS h
             JOIN
-        spotifyclone.cancao AS c ON h.cancao_id = c.cancao_id
+        SpotifyClone.cancao AS c ON h.cancao_id = c.cancao_id
     GROUP BY cancao
     ORDER BY reproducoes DESC , cancao
     LIMIT 2;
