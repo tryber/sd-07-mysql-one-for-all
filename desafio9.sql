6a4960ec691812164ea796055fa50e37d8b5d527
laurods <itrafor@gmail.com>
2021-03-14 19:42:24 -0300

    correcores af

--- desafio9.sql
+++ desafio9.sql
@@ -2,8 +2,8 @@ DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(IN nome_artista VARCHAR(100))
 BEGIN
 SELECT
-a.artista AS artista,
-al.album AS album
+a.nome AS artista,
+al.nome AS album
 FROM
 SpotifyClone.artista AS a
 INNER JOIN SpotifyClone.album AS al ON a.artista_id = al.artista_id
