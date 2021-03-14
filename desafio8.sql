ea042dfa3d14485c611f321ed758229bb5af88aa
BrunoNogueiraBruno <nugobruno@gmail.com>
2021-03-14 19:22:21 -0300

    Desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON users
+FOR EACH ROW
+
+BEGIN
+-- Referência do DECLARE: https://github.com/tryber/sd-07-mysql-one-for-all/blob/cosmo-mysql-one-for-all/desafio8.sql
+DECLARE USER_ID INT;
+SET USER_ID = OLD.user_id;
+
+DELETE FROM SpotifyClone.users_history AS HISTORY
+WHERE HISTORY.user_history_id = USER_ID;
+
+DELETE FROM SpotifyClone.users_following AS FOLLOW
+WHERE FOLLOW.user_following_id = USER_ID;
+
+END $$
+DELIMITER ;
