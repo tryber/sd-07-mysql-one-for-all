bb55abbbdfc12f1c562d256959ec37684088b22c
julianafatsil <julianafatsil@gmail.com>
2021-03-14 15:14:04 -0300

    Desafio 04

---
+++ desafio4.sql
@@ -0,0 +1,12 @@
+CREATE VIEW top_3_artistas AS
+    SELECT 
+        a.artista_nome AS artista, COUNT(s.usuario_id) AS seguidores
+    FROM
+        spotifyclone.seguindo_artista AS s
+            JOIN
+        spotifyclone.artista AS a ON s.artista_id = a.artista_id
+            JOIN
+        spotifyclone.usuario AS u ON s.usuario_id = u.usuario_id
+    GROUP BY artista
+    ORDER BY seguidores DESC , artista
+    LIMIT 3;
