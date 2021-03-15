e061e6699e76d329d52e6c1b49c6adae9e1ef95d
julianafatsil <julianafatsil@gmail.com>
2021-03-15 12:30:49 -0300

    Revision Issue

--- desafio9.sql
+++ desafio9.sql
@@ -4,7 +4,7 @@ DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista(IN nome_artista VARCHAR(100))
 BEGIN
-	SELECT a.artista_nome AS artista, al.album_nome AS album
+  SELECT a.artista_nome AS artista, al.album_nome AS album
     FROM SpotifyClone.artista AS a
     JOIN SpotifyClone.album AS al ON a.artista_id = al.artista_id
     WHERE a.artista_nome = nome_artista
