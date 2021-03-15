1c05b22ccec5bc133c7d85bb0a0068326b85a8e1
moacyrrln <moacyrrln@gmail.com>
2021-03-15 03:02:22 -0300

    desafio6

---
+++ desafio6.sql
@@ -0,0 +1,22 @@
+create view faturamento_atual as
+SELECT (
+SELECT
+min(SpotifyClone.planos.valor)
+FROM SpotifyClone.usuario
+inner join SpotifyClone.planos
+on SpotifyClone.usuario.plano_id = SpotifyClone.planos.id) as "faturamento_minimo",
+(SELECT
+max(SpotifyClone.planos.valor)
+FROM SpotifyClone.usuario
+inner join SpotifyClone.planos
+on SpotifyClone.usuario.plano_id = SpotifyClone.planos.id) as "faturamento_maximo",
+(SELECT
+round(avg(SpotifyClone.planos.valor), 2)
+FROM SpotifyClone.usuario
+inner join SpotifyClone.planos
+on SpotifyClone.usuario.plano_id = SpotifyClone.planos.id) as "faturamento_medio",
+(SELECT
+round(sum(SpotifyClone.planos.valor), 2)
+FROM SpotifyClone.usuario
+inner join SpotifyClone.planos
+on SpotifyClone.usuario.plano_id = SpotifyClone.planos.id) as "faturamento_total"
