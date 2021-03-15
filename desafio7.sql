c7ed1b6de83d1842ca5417a161d74b2ac0d8edd3
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-03-15 14:27:52 -0300

    correct req7

--- desafio7.sql
+++ desafio7.sql
@@ -7,4 +7,5 @@ INNER JOIN SpotifyClone.albuns AS alb
 ON alb.artista_id = art.artista_id
 INNER JOIN SpotifyClone.seguindo AS seg
 ON seg.artista_id = art.artista_id
-GROUP BY alb.album_id;
+GROUP BY alb.album_id
+ORDER BY 3 DESC , 1 , 2;
