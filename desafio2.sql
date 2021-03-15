126ced59c3345434c958cf6b90b12a628e4a03e3
w27M <marcos.1998226@gmail.com>
2021-03-14 22:35:36 -0300

    desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,8 @@
+CREATE VIEW estatisticas_musicais AS 
+	SELECT COUNT(distinct A.id) AS 'cancoes', 
+	COUNT(distinct C.id) AS 'artistas',
+	COUNT(distinct B.id) AS 'albuns' FROM SpotifyClone.song AS A
+	INNER JOIN SpotifyClone.album AS B
+	ON A.album_id = B.id
+	INNER JOIN SpotifyClone.artist AS C
+	ON B.artist_id = C.id;
