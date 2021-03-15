e8d925315cf9f66aebc09559698eaf3830366602
w27M <marcos.1998226@gmail.com>
2021-03-14 21:46:13 -0300

    vai logo pow

--- desafio1.sql
+++ desafio1.sql
@@ -17,6 +17,7 @@ CREATE TABLE IF NOT EXISTS song(
 id INT PRIMARY KEY AUTO_INCREMENT, 
 album_id INT NOT NULL,
 song_title VARCHAR(100) NOT NULL,
+song_length Varchar(4) NOT NULL,
 FOREIGN KEY (album_id) REFERENCES album(id)) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS plan(
@@ -38,14 +39,14 @@ user_id INT NOT NULL,
 song_id INT NOT NULL,
 PRIMARY KEY (user_id, song_id),
 FOREIGN KEY (user_id) REFERENCES users(id),
-FOREIGN KEY (song_id) REFERENCES song(ID)) ENGINE=InnoDB;
+FOREIGN KEY (song_id) REFERENCES song(id)) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS follewed_artist(
-artist_id INT NOT NULL,
 user_id INT NOT NULL,
-PRIMARY KEY (artist_id, user_id),
-FOREIGN KEY (artist_id) REFERENCES artist(id),
-FOREIGN KEY (user_id) REFERENCES users(id)) ENGINE=InnoDB;
+artist_id INT NOT NULL,
+PRIMARY KEY (user_id, artist_id),
+FOREIGN KEY (user_id) REFERENCES users(id), 
+FOREIGN KEY (artist_id) REFERENCES artist(id)) ENGINE=InnoDB;
 
 INSERT INTO plan (plan, price, activated)
 VALUES('gratuito', 0, true),
@@ -65,25 +66,25 @@ VALUES(1,'Envious', '1994-4-21'),
 (3,'Incandescent', '1998-6-8'),
 (4,'Temporary Culture', '2000-1-10');
 
-INSERT INTO song (album_id, song_title)
-VALUES(1, 'Soul For Us'),
-(1,'Reflections Of Magic'),
+INSERT INTO song (album_id, song_title, song_length)
+VALUES(1, 'Soul For Us','2:45'),
+(1,'Reflections Of Magic','2:45'),
 (1,'Dance With Her Own','2:45'),
-(2,'Troubles Of My Inner Fire'),
-(2,'Time Fireworks'),
-(3,'Magic Circus'),
-(3,'Honey, So Do I'),
-(3,'Sweetie, Lets Go Wild'),
-(3,'She Knows'),
-(4,'Fantasy For Me'),
-(4,'Celebration Of More'),
-(5,'Rock His Everything'),
-(4,'Home Forever'),
-(4,'Diamond Power'),
-(4,'Honey, Lets Be Silly'),
-(5,'Thang Of Thunder'),
-(5,'Words Of Her Life'),
-(5,'Without My Streets');
+(2,'Troubles Of My Inner Fire','2:45'),
+(2,'Time Fireworks','2:45'),
+(3,'Magic Circus','2:45'),
+(3,'Honey, So Do I','2:45'),
+(3,'Sweetie, Lets Go Wild','2:45'),
+(3,'She Knows','2:45'),
+(4,'Fantasy For Me','2:45'),
+(4,'Celebration Of More','2:45'),
+(5,'Rock His Everything','2:45'),
+(4,'Home Forever','2:45'),
+(4,'Diamond Power','2:45'),
+(4,'Honey, Lets Be Silly','2:45'),
+(5,'Thang Of Thunder','2:45'),
+(5,'Words Of Her Life','2:45'),
+(5,'Without My Streets','2:45');
 
 INSERT INTO users (plan_id, first_name, last_name, age)
 VALUES(1, 'Thati','Sales', 23),
@@ -107,7 +108,7 @@ VALUES(1, 1),
 (4, 18),
 (4, 11);
 
-INSERT INTO follewed_artist(artist_id, user_id)
+INSERT INTO follewed_artist(user_id, artist_id)
 VALUES(1, 1),
 (1, 4),
 (1, 3),
