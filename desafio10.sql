b11d50793d119fde233fbe4a169659ddc7a11ad9
Lets <letsferreiratx@gmail.com>
2021-03-15 11:32:32 -0300

    agora vai 10

--- desafio10.sql
+++ desafio10.sql
@@ -6,7 +6,7 @@ DECLARE quantidade_cancoes INT;
 SELECT
 COUNT(historico.cancao_id)
 FROM SpotifyClone.historico_de_reproducoes AS historico
-WHERE historico.usario_id = usuario_id
+WHERE historico.usuario_id = usuario_id
 INTO quantidade_cancoes ;
 RETURN quantidade_cancoes;
 END $$
