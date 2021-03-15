7f6948d401098e1d39f01eac1cba83236f24ee57
w27M <marcos.1998226@gmail.com>
2021-03-14 22:54:32 -0300

    desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,10 @@
+CREATE VIEW historico_reproducao_usuarios AS 
+SELECT 
+usuario.first_name AS 'usuario', 
+musica.song_title AS 'nome' 
+FROM SpotifyClone.users AS usuario
+INNER JOIN SpotifyClone.history_reproduction AS lista
+ON lista.user_id = usuario.id
+INNER JOIN SpotifyClone.song AS musica
+ON lista.song_id = musica.id
+ORDER BY usuario.first_name, musica.song_title;
