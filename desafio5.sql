593f1feff3c702d8684d5ac3c7802a8c011584ff
Raphael Caputo <faelelf2@gmail.com>
2021-03-14 21:15:00 -0300

    Desafio 5 nova tentativa

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,9 @@
 CREATE VIEW top_2_hits_do_momento AS (
-  SELECT S.music_name AS cancao, COUNT(*) AS reproducoes
-  FROM SpotifyClone.musics AS S
-    JOIN SpotifyClone.music_history AS mhistory
-      ON S.id = mhistory.music_id
-  GROUP BY music_id
-  ORDER BY 'reproducoes' DESC, 'cancao'
+  SELECT S.music_name AS 'cancao', COUNT(mhistory.user_id) AS 'reproducoes'
+    FROM SpotifyClone.musics AS S
+    INNER JOIN SpotifyClone.music_history AS mhistory
+    ON mhistory.music_id = S.id
+    GROUP BY music_id
+    ORDER BY 'reproducoes' DESC, 'cancao'
   LIMIT 2
 );
