95d0e7c247091a280182c47140eb08bf7e123f20
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-03-15 15:07:44 -0300

    req09

---
+++ desafio9.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(IN nomeArtista TEXT)
+BEGIN
+    SELECT art.artista_nome AS "artista"
+    FROM SpotifyClone.artistas AS art
+    INNER JOIN SpotifyClone.albuns AS alb
+    ON alb.album_id = art.album_id
+    WHERE nomeArtista = art.artista_nome
+    ORDER BY 2;
+END $$
+
+DELIMITER ;
