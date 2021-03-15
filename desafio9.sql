1a8452fe7431ef510b597575ac64586620339aac
w27M <marcos.1998226@gmail.com>
2021-03-15 01:48:38 -0300

    desafio 9

--- desafio9.sql
+++ desafio9.sql
@@ -2,13 +2,13 @@ DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista(IN nomeArtista VARCHAR(300))
 BEGIN
-    SELECT ARTISTA.artist_name AS 'artista', 
-    ALBUM.album_title AS 'album'
-   FROM SpotifyClone.artist AS ARTISTA
-   INNER JOIN SpotifyClone.album AS ALBUM
-   WHERE ALBUM.artist_id = ARTISTA.id
-   AND ARTISTA.artist_name = nomeArtista
-   ORDER BY ALBUM.album_title;
+SELECT ARTISTA.artist_name AS 'artista', 
+ALBUM.album_title AS 'album'
+FROM SpotifyClone.artist AS ARTISTA
+INNER JOIN SpotifyClone.album AS ALBUM
+WHERE ALBUM.artist_id = ARTISTA.id
+AND ARTISTA.artist_name = nomeArtista
+ORDER BY ALBUM.album_title;
 END $$
 
 DELIMITER ;
