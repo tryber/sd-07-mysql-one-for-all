e9f28d8ecb13ee18ab238a8b1042251ff31f7a13
moacyrrln <moacyrrln@gmail.com>
2021-03-15 01:32:48 -0300

    desafio2

---
+++ desafio2.sql
@@ -0,0 +1,4 @@
+CREATE VIEW `estatisticas_musicais` AS
+SELECT (select count(SpotifyClone.cancoes.id) from SpotifyClone.cancoes) as cancoes,
+(select count(SpotifyClone.artista.id) from SpotifyClone.artista) as artitstas,
+(select count(SpotifyClone.album.id) from SpotifyClone.album) as albuns;
