e1c30d5b40c1fbb8c9087b950bddd03970524a45
Raphael Caputo <faelelf2@gmail.com>
2021-03-14 21:06:14 -0300

    Mais refatoracoes

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,9 @@
 CREATE VIEW top_2_hits_do_momento AS (
   SELECT S.music_name AS cancao, COUNT(*) AS reproducoes
   FROM SpotifyClone.musics AS S
-    LEFT JOIN SpotifyClone.music_history AS mhistory
-      ON S.id = mhistory.song_id
+    JOIN SpotifyClone.music_history AS mhistory
+      ON S.id = mhistory.music_id
   GROUP BY music_id
-  ORDER BY 'reproducoes' DESC, S.name ASC
+  ORDER BY 'reproducoes' DESC, 'cancao'
   LIMIT 2
 );
--- desafio9.sql
+++ desafio9.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista(IN nome VARCHAR(255))
 BEGIN
-  SELECT a.artist_name AS artista, alb.album_name AS album
+  SELECT a.artist_name AS 'artista', al.album_name AS 'album'
   FROM SpotifyClone.artists AS a
     JOIN SpotifyClone.albums AS al
     on al.artist_id = a.id
