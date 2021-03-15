7807655c68bdfcb125072cae6dff8ce54965a0fa
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-03-15 14:14:34 -0300

    req7

--- desafio7.sql
+++ desafio7.sql
@@ -1,10 +1,10 @@
 CREATE VIEW SpotifyClone.perfil_artistas AS 
 SELECT art.artista_nome AS "artista",
 alb.album_nome AS "album",
-COUNT(seg.usuario_id) AS "seguidores"
+COUNT(alb.album_id) AS "seguidores"
 FROM SpotifyClone.artistas AS art
 INNER JOIN SpotifyClone.albuns AS alb
 ON alb.artista_id = art.artista_id
 INNER JOIN SpotifyClone.seguindo AS seg
 ON seg.artista_id = art.artista_id
-GROUP BY art.artista_nome;
+GROUP BY alb.album_id;
