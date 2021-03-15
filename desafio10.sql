c7daea9ac5028fad85e4adc9f53436f7f651770f
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-03-15 15:20:58 -0300

    req10

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(usuarioId TEXT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE music_qnt INT;
+SELECT COUNT(*) FROM SpotifyClone.reproducoes AS rep
+WHERE usuarioId = rep.usuario_id INTO music_qnt;
+RETURN music_qnt;
+END $$
+
+DELIMITER ;
