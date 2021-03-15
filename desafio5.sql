cab83d217e21ff0ca1a32fce72d4bdebbdb86193
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-03-15 13:32:44 -0300

    req5

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+CREATE VIEW SpotifyClone.top_2_hits_do_momento AS 
+SELECT mus.musica_nome AS "cancao",
+COUNT(rep.musica_id) AS "seguidores"
+FROM SpotifyClone.musicas AS mus
+INNER JOIN SpotifyClone.reproducoes AS rep
+ON rep.musica_id = mus.musica_id
+GROUP BY mus.musica_id
+ORDER BY 2 DESC, 1
+LIMIT 2;
