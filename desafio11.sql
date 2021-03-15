90634e67ae64cb78c3a66dfe9a740b5901549b54
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-03-15 19:54:11 -0300

    Github me ajuda!

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,11 @@
 CREATE VIEW SpotifyClone.cancoes_premium AS 
 SELECT mus.musica_nome AS "nome",
 COUNT(*) AS "reproducoes"
-FROM SpotifyClone.reproducoes AS re
+FROM SpotifyClone.reproducoes AS rep
 INNER JOIN SpotifyClone.musicas AS mus
-ON mus.musica_id = re.musica_id
+ON mus.musica_id = rep.musica_id
 INNER JOIN SpotifyClone.usuario AS usu
-ON usu.usuario_id = re.usuario_id
+ON usu.usuario_id = rep.usuario_id
 WHERE usu.plano_id <> 1
 GROUP BY mus.musica_nome
 ORDER BY 1;
