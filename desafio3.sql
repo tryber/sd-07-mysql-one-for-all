a56a49690630028aeac8001bb8ae671954624621
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-03-16 17:01:09 -0300

    refat

--- desafio3.sql
+++ desafio3.sql
@@ -1,12 +1,12 @@
 CREATE VIEW historico_reproducao_usuarios as
 select
 (
-select user_name from SpotifyClone.users where user_id = p.user_id
+select user_name from users where user_id = p.user_id
 ) as 'usuario',
 (
-select song_title from SpotifyClone.songs where song_id = p.song_id
+select song_title from songs where song_id = p.song_id
 ) as 'nome'
-from SpotifyClone.plays p
+from plays p
 order by
 1 ASC,
 2 ASC;
