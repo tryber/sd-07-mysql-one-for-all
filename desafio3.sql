b8a985fb9e142ce53eeaec0617e520156c1628c2
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-03-16 14:01:03 -0300

    req 3

---
+++ desafio3.sql
@@ -0,0 +1,10 @@
+CREATE VIEW historico_reproducao_usuarios as
+select
+u.user_name as 'usuario',
+s.song_title as 'nome'
+from SpotifyClone.plays p
+inner join SpotifyClone.users u on u.user_id = p.user_id
+inner join SpotifyClone.songs s on s.song_id = p.song_id
+order by
+1 ASC,
+2 ASC;
