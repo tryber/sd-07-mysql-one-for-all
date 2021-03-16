fb04b87cf4a9fb70853455211bdd0597646e50a3
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-03-16 20:49:24 -0300

    refat

--- desafio9.sql
+++ desafio9.sql
@@ -4,11 +4,11 @@ DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(IN artistin VARCHAR(100))
 BEGIN
     SELECT
-	a.name AS artista,
-	ab.album_name AS album
-	FROM artists a
-	INNER JOIN albums ab ON ab.artist_id = a.artist_id
-	WHERE name LIKE CONCAT('%', artistin, '%');
+a.name AS artista,
+ab.album_name AS album
+FROM artists a
+INNER JOIN albums ab ON ab.artist_id = a.artist_id
+WHERE name LIKE CONCAT('%', artistin, '%');
 END $$
 
 DELIMITER ;
