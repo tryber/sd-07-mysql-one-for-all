ef962a0c7b05140cdb630852d099b14fb75a54de
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-03-17 17:21:50 -0300

    corrige arquivo desafio1.json e inclui desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,6 @@
+SELECT 
+(SELECT COUNT(*) FROM SpotifyClone.songs) AS cancoes,
+(SELECT COUNT(*) FROM SpotifyClone.artists) AS artistas,
+(SELECT COUNT(*) FROM SpotifyClone.albuns) AS albuns;
+
+/* feito com ajuda do código do @rafaelmguimaraes */
