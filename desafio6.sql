d397c0b3bfb46158a6e9294f7250bf02966e1c8b
Thays Costa <thayscosta3@gmail.com>
2021-03-18 18:39:18 -0300

    Adicionando arredondamento em todas as operaçoes

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 CREATE VIEW SpotifyClone.faturamento_atual AS
 SELECT MIN(sub.price) AS faturamento_minimo,
-MAX(sub.price) AS faturamento_maximo,
+ROUND(MAX(sub.price), 2) AS faturamento_maximo,
 ROUND(AVG(sub.price), 2) AS faturamento_medio,
-SUM(sub.price) AS faturamento_total
+ROUND(SUM(sub.price), 2) AS faturamento_total
 FROM SpotifyClone.subscriptions AS sub
 INNER JOIN SpotifyClone.user AS user
 ON sub.id = user.plan_id;
