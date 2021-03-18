8d9cbc201b0526d20d4309b9c4ef24f141902eef
Thays Costa <thayscosta3@gmail.com>
2021-03-18 20:45:13 -0300

    Voltando a query passada - req. 7

--- desafio7.sql
+++ desafio7.sql
@@ -3,9 +3,9 @@ SELECT artists.artist AS artista,
 albums.name AS album, 
 COUNT(*) AS seguidores
 FROM SpotifyClone.artists AS artists
-INNER JOIN SpotifyClone.albums AS albums
-ON followers.artist_id = artists.id
 INNER JOIN SpotifyClone.followed_artists AS followers
-ON artists.id = followers.artist_id
+ON followers.artist_id = artists.id
+INNER JOIN SpotifyClone.albums AS albums
+ON artists.id = albums.artist_id
 GROUP BY albums.id
-ORDER BY 3 DESC, 1;
+ORDER BY 3 DESC, 1, 2;
