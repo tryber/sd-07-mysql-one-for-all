7a99d50ebbbd3915e734116a70e62d3a9b1f3a6e
Thays Costa <thayscosta3@gmail.com>
2021-03-18 23:39:02 -0300

    Requisito 8 - com USE e nome da tabela entre crase e sem tab

--- desafio8.sql
+++ desafio8.sql
@@ -5,9 +5,9 @@ AFTER DELETE
 ON `user`
 FOR EACH ROW
 BEGIN
-	DELETE FROM rep_history
-	WHERE user_id = OLD.user_id;
-	DELETE FROM SpotifyClone.followed_artists
-	WHERE user_id = OLD.user_id;
+DELETE FROM rep_history
+WHERE user_id = OLD.user_id;
+DELETE FROM SpotifyClone.followed_artists
+WHERE user_id = OLD.user_id;
 END
 DELIMITER $$ ;
