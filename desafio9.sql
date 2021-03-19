9edd6f52451fc9b6a993ac8b1507951c17054a97
Thays Costa <thayscosta3@gmail.com>
2021-03-19 19:32:45 -0300

    Req. 9 de novo

--- desafio9.sql
+++ desafio9.sql
@@ -6,8 +6,8 @@ SELECT art.artist AS artista,
 albums.name AS album
 FROM SpotifyClone.artists AS art
 INNER JOIN SpotifyClone.albums AS albums
-ON art.id = album.artist_id
-WHERE artistName = art.artists
+ON art.id = albums.artist_id
+WHERE artistName = art.artist
 ORDER BY album;
 END $$
 DELIMITER ;
