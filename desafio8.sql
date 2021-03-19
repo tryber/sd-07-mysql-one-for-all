6d75e4fbc870e65b8dd44c6be8d409a2f03af42a
Thays Costa <thayscosta3@gmail.com>
2021-03-18 23:36:53 -0300

    Requisito 8 - com USE e nome da tabela entre crase

--- desafio8.sql
+++ desafio8.sql
@@ -1,12 +1,13 @@
+USE SpotifyClone;
 DELIMITER $$
-CREATE TRIGGER trigger_usuario_delete BEFORE DELETE
-ON SpotifyClone.user
+CREATE TRIGGER trigger_usuario_delete
+AFTER DELETE
+ON `user`
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.rep_history
-WHERE user_id = OLD.user_id;
-DELETE FROM SpotifyClone.followed_artists
-WHERE user_id = OLD.user_id;
+	DELETE FROM rep_history
+	WHERE user_id = OLD.user_id;
+	DELETE FROM SpotifyClone.followed_artists
+	WHERE user_id = OLD.user_id;
 END
-$$
-DELIMITER ;
+DELIMITER $$ ;
