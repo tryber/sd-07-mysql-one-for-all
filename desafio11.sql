91b377b65f7649d4d41abdc3b35d70d1842342af
Thays Costa <thayscosta3@gmail.com>
2021-03-19 23:40:01 -0300

    Requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+USE SpotifyClone;
+CREATE VIEW cancoes_premium AS
+SELECT songs.name AS nome,
+COUNT(*) AS reproducoes
+FROM songs
+INNER JOIN rep_history AS hist
+ON songs.id = hist.song_id
+JOIN user
+ON user.id = hist.user_id
+WHERE user.plan_id IN (2, 3)
+GROUP BY nome
+ORDER BY 1;
