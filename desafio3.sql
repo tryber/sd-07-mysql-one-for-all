f6035b137b48ca9de0480d11fb23c5dcec46234a
lucasvr39 <lucasveras1994@gmail.com>
2021-03-20 17:45:35 -0300

    fix challenge 3

--- desafio3.sql
+++ desafio3.sql
@@ -5,4 +5,5 @@ FROM SpotifyClone.users as db_users
 INNER JOIN historico_de_reproducoes as user_history
 ON user_history.usuario_id = db_users.usuario_id
 INNER JOIN songs
-ON user_history.cancoes_id = songs.cancoes_id;
+ON user_history.cancoes_id = songs.cancoes_id
+ORDER BY usuario, nome ASC;
