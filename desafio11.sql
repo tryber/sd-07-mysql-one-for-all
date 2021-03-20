b44fb7184c9f4bde89bdb71193f4ea6fd7da5874
Thays Costa <thayscosta3@gmail.com>
2021-03-20 17:41:04 -0300

    Requisito 11 - inserindo left join e tirando o 'use'

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,10 @@
-USE SpotifyClone;
 CREATE VIEW cancoes_premium AS
 SELECT songs.name AS nome,
 COUNT(*) AS reproducoes
-FROM songs
-INNER JOIN rep_history AS hist
+FROM SpotifyClone.songs
+INNER JOIN SpotifyClone.rep_history AS hist
 ON songs.id = hist.song_id
-INNER JOIN user
+LEFT JOIN SpotifyClone.user
 ON user.id = hist.user_id
 WHERE user.plan_id IN (2, 3)
 GROUP BY nome
