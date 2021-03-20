507a180dbb29242ac1c8be76b288bf94b977f482
lucasvr39 <lucasveras1994@gmail.com>
2021-03-20 17:28:52 -0300

    implement challenge 2

---
+++ desafio2.sql
@@ -0,0 +1,13 @@
+CREATE VIEW [estatisticas_musicais] AS
+SELECT (
+  SELECT count(cancoes_id) as 'cancoes'
+  FROM SpotifyClone.songs  
+) as cancoes, 
+(
+  SELECT count(artista_id) as 'artistas'
+  FROM SpotifyClone.artists
+) as artistas, 
+(
+  SELECT count(album_id) as 'albuns'
+  FROM SpotifyClone.albums
+) as albuns;
