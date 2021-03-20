a89174fb3c66781f2c6aabe3e07bccfc5e2b4b21
Thays Costa <thayscosta3@gmail.com>
2021-03-19 23:50:11 -0300

    Requisito 11 - inserindo 'inner'

--- desafio11.sql
+++ desafio11.sql
@@ -5,7 +5,7 @@ COUNT(*) AS reproducoes
 FROM songs
 INNER JOIN rep_history AS hist
 ON songs.id = hist.song_id
-JOIN user
+INNER JOIN user
 ON user.id = hist.user_id
 WHERE user.plan_id IN (2, 3)
 GROUP BY nome
