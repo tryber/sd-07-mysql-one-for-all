79bb0bb3a5a3662bf16c71452b838383a0fa9acf
lucasvr39 <lucasveras1994@gmail.com>
2021-03-20 17:41:41 -0300

    implement challenge 3

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT db_users.usuario AS 'usuario',
+songs.cancao as 'nome'
+FROM SpotifyClone.users as db_users
+INNER JOIN historico_de_reproducoes as user_history
+ON user_history.usuario_id = db_users.usuario_id
+INNER JOIN songs
+ON user_history.cancoes_id = songs.cancoes_id;
