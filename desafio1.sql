80d7c6c33d101f12b3a6f0ab9bf0ba691b1081a6
vladimirrchagas <vladimirrchagas@gmail.com>
2021-03-20 18:05:17 -0300

    checking challenge 1

---
+++ desafio1.sql
@@ -0,0 +1,176 @@
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE IF NOT EXISTS SpotifyClone;
+
+USE SpotifyClone;
+
+CREATE TABLE songs(
+	song_id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50)
+) ENGINE = InnoDB;
+
+CREATE TABLE artist(
+	artist_id INT PRIMARY KEY AUTO_INCREMENT,
+	first_name VARCHAR(20),
+    last_name VARCHAR(30)
+) ENGINE = InnoDB;
+
+CREATE TABLE album(
+	album_id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(50)
+) ENGINE = InnoDB;
+
+CREATE TABLE plan(
+	plan_id INT PRIMARY KEY AUTO_INCREMENT,
+	`type` VARCHAR(30),
+    cost REAL
+) ENGINE = InnoDB;
+
+CREATE TABLE `user`(
+	user_id INT PRIMARY KEY AUTO_INCREMENT,
+	`name` VARCHAR(20),
+    age INT
+) ENGINE = InnoDB;
+
+CREATE TABLE user_artist (
+	user_id INT NOT NULL,
+    artist_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES `user`(user_id),
+    FOREIGN KEY (artist_id) REFERENCES artist(artist_id),
+    CONSTRAINT PK_user_artist PRIMARY KEY (user_id, artist_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE album_artist (
+	album_id INT NOT NULL,
+    artist_id INT NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES album(album_id),
+    FOREIGN KEY (artist_id) REFERENCES artist(artist_id),
+    CONSTRAINT PK_album_artist PRIMARY KEY (album_id, artist_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE album_song (
+	album_id INT NOT NULL,
+    song_id INT NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES album(album_id),
+    FOREIGN KEY (song_id) REFERENCES songs(song_id),
+    CONSTRAINT PK_album_song PRIMARY KEY (album_id, song_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE user_plan (
+	user_id INT NOT NULL,
+    plan_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES `user`(user_id),
+    FOREIGN KEY (plan_id) REFERENCES plan(plan_id),
+    CONSTRAINT PK_user_plan PRIMARY KEY (user_id, plan_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE user_songs_reproduction (
+	user_id INT NOT NULL,
+    song_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES `user`(user_id),
+    FOREIGN KEY (song_id) REFERENCES songs(song_id),
+    CONSTRAINT PK_user_song PRIMARY KEY (user_id, song_id)
+) ENGINE = InnoDB;
+
+INSERT INTO songs(song_id, `name`) VALUES
+("Soul For Us"),
+("Reflections Of Magic"),
+("Dance With Her Own"),
+("Troubles Of My Inner Fire"),
+("Time Fireworks"),
+("Magic Circus"),
+("Honey, So Do I"),
+("Sweetie, Let's Go Wild"),
+("She Knows"),
+("Fantasy For Me"),
+("Celebration Of More"),
+("Rock His Everything"),
+("Home Forever"),
+("Diamond Power"),
+("Honey, Let's Be Silly"),
+("Thang Of Thunder"),
+("Words Of Her Life"),
+("Without My Streets");
+
+INSERT INTO artist(first_name, last_name) VALUES
+("Walter","Phoenix"),
+("Peter","Strong"),
+("Lance","Day"),
+("Freedie","Shannon");
+
+INSERT INTO album(`name`) VALUES
+("Envious"),
+("Exuberant"),
+("Hallowed Steam"),
+("Incandescent"),
+("Temporary Culture");
+
+INSERT INTO plan(`type`, cost) VALUES
+("gratuito",0.00),
+("familiar",7.99),
+("universitário",5.99);
+
+INSERT INTO `user`(`name`,age) VALUES
+("Thati",23),
+("Cintia",35),
+("Bill",20),
+("Roger",45);
+
+INSERT INTO user_artist(user_id, artist_id) VALUES
+(1,1),
+(1,3),
+(1,4),
+(2,1),
+(2,3),
+(3,1),
+(3,2),
+(4,4);
+
+INSERT INTO album_artist(album_id, artist_id) VALUES
+(1,1),
+(2,1),
+(3,2),
+(4,3),
+(5,4);
+
+INSERT INTO album_song(album_id, song_id) VALUES
+(1,1),
+(1,2),
+(1,3),
+(2,4),
+(2,5),
+(3,6),
+(3,7),
+(3,8),
+(3,9),
+(4,10),
+(4,11),
+(4,12),
+(4,13),
+(4,14),
+(4,15),
+(5,16),
+(5,17),
+(5,18);
+
+INSERT INTO user_plan(user_id, plan_id) VALUES
+(1,1),
+(2,2),
+(3,3),
+(4,1);
+
+INSERT INTO user_songs_reproduction(user_id, song_id) VALUES
+(1,1),
+(1,6),
+(1,14),
+(1,16),
+(2,13),
+(2,17),
+(2,2),
+(2,15),
+(3,4),
+(3,16),
+(3,6),
+(4,3),
+(4,18),
+(4,11);
