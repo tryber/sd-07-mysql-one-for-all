8f9f574f8fb4f1b075cf82ba75669f4e229b518e
spawnred <bruno.spawnhell@gmail.com>
2021-03-20 07:06:30 -0300

    item 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS `SpotifyClone` ;
-USE `SpotifyClone`;
+CREATE DATABASE IF NOT EXISTS "SpotifyClone" ;
+USE "SpotifyClone";
 
 CREATE TABLE planos(
     plano_id INT primary key auto_increment,
@@ -54,52 +54,52 @@ CREATE TABLE seguindo(
 
 INSERT INTO planos (plano, valor_plano)
 VALUES
-  ('gratuito', 0),
-  ('familiar', 7.99),
-  ('universitário', 5.99);
+  ("gratuito", 0),
+  ("familiar", 7.99),
+  ("universitário", 5.99);
   
 INSERT INTO users (usuario, idade, plano_id)
 VALUES
-  ('Thati', 23, 1),
-  ('Cintia', 35, 2),
-  ('Bill', 20, 3),
-  ('Roger', 45, 1); 
+  ("Thati", 23, 1),
+  ("Cintia", 35, 2),
+  ("Bill", 20, 3),
+  ("Roger", 45, 1); 
   
   INSERT INTO artistas (artista)
 VALUES
-  ('Walter Phoenix'),
-  ('Peter Strong'),
-  ('Lance Day'),
-  ('Freedie Shannon');
+  ("Walter Phoenix"),
+  ("Peter Strong"),
+  ("Lance Day"),
+  ("Freedie Shannon");
 
 INSERT INTO albuns (album, artista_id)
 VALUES
-  ('Envious', 1),
-  ('Exuberant', 1),
-  ('Hallowed Steam', 2),
-  ('Incandescent', 3),
-  ('Temporary Culture', 4);
+  ("Envious", 1),
+  ("Exuberant", 1),
+  ("Hallowed Steam", 2),
+  ("Incandescent", 3),
+  ("Temporary Culture", 4);
   
 INSERT INTO songs (cancoes, artista_id, album_id)
 VALUES
-  ('Soul For Us', 1, 1),
-  ('Reflections Of Magic', 1, 1),
-  ('Dance With Her Own', 1, 1),
-  ('Troubles Of My Inner Fire', 1, 2),
-  ('Time Fireworks', 1, 2),
-  ('Magic Circus', 2, 3),
-  ('Honey, So Do I', 2, 3),
+  ("Soul For Us", 1, 1),
+  ("Reflections Of Magic", 1, 1),
+  ("Dance With Her Own", 1, 1),
+  ("Troubles Of My Inner Fire", 1, 2),
+  ("Time Fireworks", 1, 2),
+  ("Magic Circus", 2, 3),
+  ("Honey, So Do I", 2, 3),
   ("Sweetie, Let's go Wild", 2, 3),
-  ('She Knows', 2, 3),
-  ('Fantasy For Me', 3, 4),
-  ('Celebration Of More', 3, 4),
-  ('Rock His Everything', 3, 4),
-  ('Home Forever', 3, 4),
-  ('Diamond Power', 3, 4),
+  ("She Knows", 2, 3),
+  ("Fantasy For Me", 3, 4),
+  ("Celebration Of More", 3, 4),
+  ("Rock His Everything", 3, 4),
+  ("Home Forever", 3, 4),
+  ("Diamond Power", 3, 4),
   ("Honey, Let's Be Silly", 3, 4),
-  ('Thang Of Thunder', 4, 5),
-  ('Words Of Her Life', 4, 5),
-  ('Without My Streets', 4, 5);
+  ("Thang Of Thunder", 4, 5),
+  ("Words Of Her Life", 4, 5),
+  ("Without My Streets", 4, 5);
 
 INSERT INTO historico (usuario_id, cancoes_id)
 VALUES
