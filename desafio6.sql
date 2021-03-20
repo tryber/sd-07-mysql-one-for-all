8787c320918d6ba78ec548217ebf0f856be1dd03
lucasvr39 <lucasveras1994@gmail.com>
2021-03-20 19:18:25 -0300

    try to fix challenge 6 again

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 CREATE VIEW faturamento_atual AS
 SELECT MIN(dbs.valor_plano) as faturamento_minimo,
-MAX(dbs.valor_plano) as faturamento_maximo,
+FORMAT(MAX(dbs.valor_plano), 2) as faturamento_maximo,
 FORMAT(ROUND(SUM(dbs.valor_plano) / COUNT(dbu.plano_id), 2), 2) as faturamento_medio,
-SUM(dbs.valor_plano) as faturamento_total 
+FORMAT(SUM(dbs.valor_plano), 2) as faturamento_total 
 FROM SpotifyClone.subscriptions as dbs
 INNER JOIN SpotifyClone.users as dbu
 ON dbu.plano_id = dbs.plano_id;
