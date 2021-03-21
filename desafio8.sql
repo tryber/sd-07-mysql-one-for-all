534bbb7260cbe1f5f546aa166cc2eb5f4fdc4cfe
vladimirrchagas <vladimirrchagas@gmail.com>
2021-03-21 13:42:25 -0300

    adjustment challenge 9

--- desafio8.sql
+++ desafio8.sql
@@ -3,11 +3,11 @@ DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista(IN artist_name VARCHAR(300))
 BEGIN
-    SELECT CONCAT(art.first_name,' ',art.last_name) AS 'artista',
-    alb.`name` AS 'album'
-    FROM artist art
-    INNER JOIN album alb ON art.artist_id = alb.artist_id AND CONCAT(art.first_name,' ',art.last_name) = artist_name
-    ORDER BY 2;
+SELECT CONCAT(art.first_name,' ',art.last_name) AS 'artista',
+alb.`name` AS 'album'
+FROM artist art
+INNER JOIN album alb ON art.artist_id = alb.artist_id AND CONCAT(art.first_name,' ',art.last_name) = artist_name
+ORDER BY 2;
 END $$
 
 DELIMITER ;
