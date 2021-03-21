41ed1f42f4cb2a1e0cd5739b5ebbbda76330e745
lucasvr39 <lucasveras1994@gmail.com>
2021-03-20 23:45:25 -0300

    try to fix challenge 8

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+USE SpotifyClone
+
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete 
+BEFORE DELETE ON SpotifyClone.users
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.seguindo_artistas
+WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.historico_de_reproducoes 
+WHERE usuario_id = OLD.usuario_id;
+END $$
+
+DELIMITER ;
