acc9973780e85f5463e5b34b9cb077106b71749c
vladimirrchagas <vladimirrchagas@gmail.com>
2021-03-21 13:18:38 -0300

    adjustment challenge 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,21 +1,9 @@
-USE SpotifyClone;
-
-DELIMITER $$
-CREATE FUNCTION qty_user_artist(artist_idr INT)
-RETURNS INT READS SQL DATA
-BEGIN
-DECLARE qty_follows INT;
-SELECT COUNT(ua.artist_id) FROM SpotifyClone.user_artist ua
-WHERE ua.artist_id = artist_idr INTO qty_follows;
-RETURN qty_follows;
-END $$
-
-DELIMITER ;
-
-CREATE VIEW perfil_artistas AS
+CREATE VIEW perfil_artistas AS;
 SELECT
 CONCAT(art.first_name,' ',art.last_name) AS 'artista', alb.`name` AS 'album',
-(SELECT qty_user_artist(art.artist_id)) AS 'seguidores'
+COUNT(ua.artist_id) AS 'seguidores'
 FROM artist art
 INNER JOIN album alb ON alb.artist_id = art.artist_id
+INNER JOIN user_artist ua ON ua.artist_id = art.artist_id
+GROUP BY 2, 1
 ORDER BY 3 DESC, 1, 2;
