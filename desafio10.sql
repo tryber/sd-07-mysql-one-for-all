5ab193e63e392c7203c980471d073e1c3d076911
vladimirrchagas <vladimirrchagas@gmail.com>
2021-03-21 15:52:28 -0300

    adjustment challenge 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,10 +1,10 @@
 USE SpotifyClone;
 DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historico(user_name VARCHAR(200))
+CREATE FUNCTION quantidade_musicas_no_historico(user_id_m INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE qty_reproductions INT;
-SELECT COUNT(user_id) FROM user_songs_reproduction WHERE user_id = (SELECT user_id FROM `user` WHERE `name` = user_name) INTO qty_reproductions;
+SELECT COUNT(user_id) FROM user_songs_reproduction WHERE user_id = user_id_m INTO qty_reproductions;
 RETURN qty_reproductions;
 END $$
 DELIMITER ;
