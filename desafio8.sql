6243c705e0274bc7e53a03def6debc6581452109
vladimirrchagas <vladimirrchagas@gmail.com>
2021-03-21 14:34:51 -0300

    checking challenge 8

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+    BEFORE DELETE ON `user`
+    FOR EACH ROW
+BEGIN
+    DELETE FROM user_artist ua WHERE ua.user_id = OLD.user_id;
+    DELETE FROM user_songs_reproduction usr WHERE usr.user_id = OLD.user_id;
+END $$
+DELIMITER ;
