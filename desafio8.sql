e3980e10114345d8c9ca94c01da7c90e54c96dd0
vladimirrchagas <vladimirrchagas@gmail.com>
2021-03-21 14:42:05 -0300

    adjustment challenge 8

--- desafio8.sql
+++ desafio8.sql
@@ -4,7 +4,7 @@ CREATE TRIGGER trigger_usuario_delete
     BEFORE DELETE ON `user`
     FOR EACH ROW
 BEGIN
-    DELETE FROM user_artist ua WHERE ua.user_id = OLD.user_id;
-    DELETE FROM user_songs_reproduction usr WHERE usr.user_id = OLD.user_id;
+    DELETE FROM user_artist WHERE user_id = OLD.user_id;
+    DELETE FROM user_songs_reproduction WHERE user_id = OLD.user_id;
 END $$
 DELIMITER ;
