3701b743d8f1eddcb174313e2ff703eb6f9ee37d
Anderson Meira <contatodeanderson@gmail.com>
2021-04-05 10:00:58 -0300

    desafios 6, 7, 8, 9 e 10 - fix

--- desafio8.sql
+++ desafio8.sql
@@ -4,8 +4,8 @@ BEFORE DELETE ON SpotifyClone.user
 FOR EACH ROW
 BEGIN
 DELETE FROM SpotifyClone.songs_heard
- WHERE user_id = OLD.user_id;
+  WHERE user_id = OLD.user_id;
 DELETE FROM SpotifyClone.user_artist
- WHERE user_id = OLD.user_id;
+  WHERE user_id = OLD.user_id;
 END $$
 DELIMITER ;
