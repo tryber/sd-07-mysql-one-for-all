145d8234cbc1ae75976264966f06a6af0a073cf8
André Ferreira <andreferreirap1@gmail.com>
2021-04-13 16:28:52 -0300

    Desafio 4 - VIEW top_3_artistas

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+CREATE VIEW top_3_artistas AS
+SELECT
+artists.artist_name 'artista', COUNT(favorites.artist_id) 'seguidores'
+FROM SpotifyClone.favorites favorites
+LEFT JOIN SpotifyClone.artists artists
+ON artists.artist_id = favorites.artist_id
+GROUP BY favorites.artist_id
+ORDER BY `seguidores` DESC, `artista`;
