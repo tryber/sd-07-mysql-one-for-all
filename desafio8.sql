1d212a96aa958d4790bb390ee220db5ee719e521
André Ferreira <andreferreirap1@gmail.com>
2021-04-14 03:20:15 -0300

    Desafio 8 - TRIGGER trigger_usuario_delete

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+DELIMITER &&
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.users
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.favorites WHERE user_id = OLD.user_id;
+DELETE FROM SpotifyClone.play_history WHERE user_id = OLD.user_id;
+END &&
+
+DELIMITER ;
