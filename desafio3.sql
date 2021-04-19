325a66550288e937c30428750e11a84fac7a25f7
Eriquy Jhordan <eriquy@gmail.com>
2021-04-19 11:18:36 -0300

    desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+CREATE VIEW SpotifyClone.historico_reproducao_usuarios AS
+SELECT
+t1.nome AS usuario,
+t3.title AS nome
+FROM SpotifyClone.Usuario AS t1
+INNER JOIN SpotifyClone.HistoricoReproducao AS t2 ON t1.id = t2.usuario_id
+INNER JOIN SpotifyClone.Cancao AS t3 ON t2.cancao_id = t3.id
+ORDER BY t1.nome, t3.title;
