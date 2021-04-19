a1624b1c77a6b75f082f848ad91bc3c58e6369f0
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-04-19 12:33:49 -0300

    desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,6 @@
+USE SpotifyClone;
+CREATE VIEW estatisticas_musicais AS
+SELECT
+(SELECT COUNT(cancao_id) FROM SpotifyClone.cancao) as cancoes,
+(SELECT COUNT(artista_id) FROM SpotifyClone.artista) as artistas,
+(SELECT COUNT(album_id) FROM SpotifyClone.album) as albuns
