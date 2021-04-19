69e0dbc792a088d2431d2a6595b350d3b030a01f
Eriquy Jhordan <eriquy@gmail.com>
2021-04-19 11:30:58 -0300

    correcao desafio 10 e 11

--- desafio10.sql
+++ desafio10.sql
@@ -1,14 +1,12 @@
 DELIMITER $$
-
-CREATE FUNCTION quantidade_musicas_no_historico(idUsuario INT)
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
-DECLARE retorneNumero INT;
-SELECT COUNT(uoc.cancao_id) AS quantidade_musicas_no_historico
-FROM SpotifyClone.usuario_ouve_cancao AS uoc
-INNER JOIN SpotifyClone.usuarios AS u ON uoc.usuario_id = u.id
-WHERE u.id = idUsuario INTO retorneNumero;
-RETURN retorneNumero;
+DECLARE quantidade INT;
+SELECT COUNT(cancao_id)
+FROM SpotifyClone.HistoricoReproducao
+WHERE HistoricoReproducao.usuario_id = id
+INTO quantidade;
+RETURN quantidade;
 END $$
-
 DELIMITER ;
--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,11 @@
 CREATE VIEW SpotifyClone.cancoes_premium AS
-SELECT c.nome AS nome, COUNT(uoc.cancao_id) AS reproducoes FROM SpotifyClone.cancoes AS c
-INNER JOIN SpotifyClone.usuario_ouve_cancao AS uoc ON c.id = uoc.cancao_id
-LEFT JOIN SpotifyClone.usuarios AS u ON uoc.usuario_id = u.id
-LEFT JOIN SpotifyClone.planos AS p ON u.plano_id = p.id
-WHERE p.tipo IN ('familiar', 'universitario')
-GROUP BY nome ORDER BY nome;
+SELECT
+t1.title AS nome,
+COUNT(t2.cancao_id) AS reproducoes
+FROM SpotifyClone.Cancao AS t1
+INNER JOIN SpotifyClone.HistoricoReproducao AS t2 ON t2.cancao_id = t1.id
+INNER JOIN SpotifyClone.Usuario AS t3 ON t2.usuario_id = t3.id
+INNER JOIN SpotifyClone.Plano AS t4 ON t4.id = t3.plano_id
+WHERE t4.descricao IN ('familiar', 'universitário')
+GROUP BY t1.title
+ORDER BY nome;
