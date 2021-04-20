a181fd7048d14aa6d0400952391fb3585168edce
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-04-20 17:27:45 -0300

    desafio 6 pra ver se passa

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 CREATE VIEW faturamento_atual AS
 SELECT 
-ROUND(MIN(valor_do_plano), 2) as faturamento_minimo,
+MIN(valor_do_plano) as faturamento_minimo,
 ROUND(MAX(valor_do_plano), 2) as faturamento_maximo,
 ROUND(AVG(valor_do_plano), 2) as faturamento_medio,
 ROUND(SUM(valor_do_plano), 2) as faturamento_total
