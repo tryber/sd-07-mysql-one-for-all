b39f30ed5078ff6e0697fbe754f3d6e572eeb082
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-04-20 17:22:24 -0300

    desafio 6 pra ver se passa

--- desafio6.sql
+++ desafio6.sql
@@ -4,4 +4,6 @@ ROUND(MIN(valor_do_plano), 2) as faturamento_minimo,
 ROUND(MAX(valor_do_plano), 2) as faturamento_maximo,
 ROUND(AVG(valor_do_plano), 2) as faturamento_medio,
 ROUND(SUM(valor_do_plano), 2) as faturamento_total
-FROM SpotifyClone.plano;
+FROM SpotifyClone.plano p
+INNER JOIN SpotifyClone.usuario u
+ON p.plano_id = u.plano_id;
