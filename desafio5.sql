4ce440dafb4935b4264cff7181215aeb83b5f674
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-04-20 16:12:39 -0300

    desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+SELECT c.nome_da_cancao AS cancao, COUNT(hr.cancao_id) as reproducoes
+FROM SpotifyClone.cancao c
+INNER JOIN SpotifyClone.historico_de_reproducao hr
+ON c.cancao_id = hr.cancao_id 
+GROUP BY 1
+ORDER BY 2 DESC, 1
+LIMIT 2;
