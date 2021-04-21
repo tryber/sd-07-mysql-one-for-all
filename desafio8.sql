047953e04c9d4be5477cc6d43c1650a2d2fe7deb
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2021-04-21 02:01:21 -0300

    desafio 8 e 9 CC1

--- desafio8.sql
+++ desafio8.sql
@@ -4,8 +4,8 @@ CREATE TRIGGER trigger_usuario_delete
 before DELETE ON SpotifyClone.users
 FOR EACH ROW
 BEGIN
-	DELETE FROM SpotifyClone.`history` WHERE user_id = old.user_id;
-	DELETE FROM SpotifyClone.artists_followed WHERE user_id = old.user_id;
+DELETE FROM SpotifyClone.`history` WHERE user_id = old.user_id;
+DELETE FROM SpotifyClone.artists_followed WHERE user_id = old.user_id;
 END
 $$
 DELIMITER ;
--- desafio9.sql
+++ desafio9.sql
@@ -3,13 +3,13 @@ DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista(in selectedArtist varchar(100))
 BEGIN
-	select a.artist as `artista`,
-    al.album as `album`
-    from SpotifyClone.artists a
-    inner join SpotifyClone.albums al
-    on a.artist_id = al.artist_id
-    where a.artist = selectedArtist
-    order by 2;
+select a.artist as `artista`,
+  al.album as `album`
+  from SpotifyClone.artists a
+  inner join SpotifyClone.albums al
+  on a.artist_id = al.artist_id
+  where a.artist = selectedArtist
+  order by 2;
 END $$
 
 DELIMITER ;
