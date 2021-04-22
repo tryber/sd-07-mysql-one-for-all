372cdb2b6b1be94298fa2b2f3cff17deac40c92f
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-04-22 13:58:22 -0300

    requisito 10 certo

--- desafio10.sql
+++ desafio10.sql
@@ -4,15 +4,14 @@ CREATE FUNCTION quantidade_musicas_no_historico(codigoUsuarioHistorico INT)
 RETURNS INT READS SQL DATA
 BEGIN
 	DECLARE qntdMusicasHistorico INT;
-    SELECT COUNT(hr.cancao_id)
+    SELECT COUNT(cancao_id)
     FROM SpotifyClone.historico_de_reproducao hr
-    INNER JOIN SpotifyClone.usuario u
-    WHERE codigoUsuarioHistorico = u.usuario_id
-    GROUP BY u.usuario_id
+    WHERE codigoUsuarioHistorico = hr.usuario_id
+    GROUP BY usuario_id
     INTO qntdMusicasHistorico;
     RETURN qntdMusicasHistorico;
 END $$
 
 DELIMITER ;
 
-SELECT quantidade_musicas_no_historico(3)
+SELECT quantidade_musicas_no_historico(3);
