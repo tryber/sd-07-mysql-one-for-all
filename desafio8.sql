d2fd1d67da9e02719a0aeccef4f855d75a8a70dc
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-04-22 12:01:32 -0300

    requisito 8 codeClimate

--- desafio8.sql
+++ desafio8.sql
@@ -4,10 +4,10 @@ CREATE TRIGGER trigger_usuario_delete
 AFTER DELETE ON SpotifyClone.usuario
 FOR EACH ROW
 BEGIN
- DELETE FROM SpotifyClone.historico_de_reproducao
- WHERE usuario_id = OLD.usuario_id;
- DELETE FROM SpotifyClone.seguidores
- WHERE usuario_id = OLD.usuario_id;
+  DELETE FROM SpotifyClone.historico_de_reproducao
+  WHERE usuario_id = OLD.usuario_id;
+  DELETE FROM SpotifyClone.seguidores
+  WHERE usuario_id = OLD.usuario_id;
 END $$
 
 DELIMITER ;
