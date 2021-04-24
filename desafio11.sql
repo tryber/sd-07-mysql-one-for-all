c8eea2ccdd73cacd6473d50d62a01dee8cb11214
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2021-04-24 13:49:03 -0300

    requisito 11 arrumando

--- desafio11.sql
+++ desafio11.sql
@@ -3,6 +3,10 @@ SELECT c.nome_da_cancao AS nome, COUNT(hr.cancao_id) AS reproducoes
 FROM SpotifyClone.cancao c
 INNER JOIN SpotifyClone.historico_de_reproducao hr
 ON c.cancao_id = hr.cancao_id
-WHERE c.cancao_id = hr.cancao_id
+INNER JOIN SpotifyClone.usuario u
+ON hr.usuario_id = u.usuario_id
+INNER JOIN SpotifyClone.plano p
+ON u.plano_id = p.plano_id
+WHERE u.usuario_id = hr.usuario_id AND u.plano_id = 2 OR u.plano_id = 3
 GROUP BY c.nome_da_cancao
 ORDER BY 1;
